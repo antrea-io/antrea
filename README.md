39c92066b4ef17cf14fed377d046aebd7d3313ee
Quan Tian <qtian@vmware.com>
2019-08-19 20:51:00 +0800

    Add docs about installation and configuration
    Added below documentations:
    * `Getting started` section in README.md
    * manual-installation.md
    * configuration.md

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # OKN
 
+## Getting started
+
+OKN components can be installed as Kubernetes DaemonSet and Deployment using Kubernetes manifests.
+
+### Requirements
+
+* A kubernetes cluster is created. See [Creating a cluster with kubeadm](
+https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/) for details.
+
+* `kube-controller-manager` is configured to enable `NodeIPAMController` by setting the following flags:
+  - `--cluster-cidr=<CIDR Range for Pods>`
+  - `--allocate-node-cidrs=true`
+
+  If `kubeadm` is used, passing `--pod-network-cidr=<CIDR Range for Pods>` to `kubeadm init` will set the two flags.
+
+* `kubelet` is configured to use `cni` network plugin. If `kubeadm` is used, `--network-plugin=cni` is set by default.
+
+* Open vSwitch >= 2.8.0 kernel module is installed and loaded on all worker nodes, See [Installing Open vSwitch](
+https://docs.openvswitch.org/en/latest/intro/install/#installation-from-packages) for details.
+
+### Installation
+
+To deploy OKN in your Kubernetes cluster, run the following command:
+```
+kubectl apply -f build/yamls/okn.yml
+```
+
+OKN components can also be run manually for development purpose. See [Manual Installation](docs/manual-installation.md)
+for details.
+
 ## Building and testing
 
 The OKN project uses the [Go modules
--- cmd/okn-agent/config.go
+++ cmd/okn-agent/config.go
@@ -7,11 +7,20 @@ import (
 type AgentConfig struct {
 	CNISocket string `yaml:"cniSocket,omitempty"`
 	// clientConnection specifies the kubeconfig file and client connection settings for the agent
-	// to use when communicating with the apiserver.
+	// to communicate with the apiserver.
 	ClientConnection componentbaseconfig.ClientConnectionConfiguration `yaml:"clientConnection"`
-	OVSBridge        string                                            `yaml:"ovsBridge,omitempty"`
-	HostGateway      string                                            `yaml:"hostGateway,omitempty"`
-	TunnelType       string                                            `yaml:"tunnelType,omitempty"`
+	// Name of the OpenVSwitch bridge okn-agent will create and use.
+	// Make sure it doesn't conflict with your existing OpenVSwitch bridges.
+	// Defaults to br-int.
+	OVSBridge string `yaml:"ovsBridge,omitempty"`
+	// Name of the interface okn-agent will create and use for host <--> pod communication.
+	// Make sure it doesn't conflict with your existing interfaces.
+	// Defaults to gw0.
+	HostGateway string `yaml:"hostGateway,omitempty"`
+	// Encapsulation mode for communication between Pods across Nodes, supported values:
+	// - vxlan (default)
+	// - geneve
+	TunnelType string `yaml:"tunnelType,omitempty"`
 	// Mount location of the /proc directory. The default is "/host", which is appropriate when
 	// okn-agent is run as part of the OKN DaemonSet (and the host's /proc directory is mounted
 	// as /host/proc in the okn-agent container). When running okn-agent as a process,
