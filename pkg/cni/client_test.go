aa3db5b5d8d104d2251b53e3d3c919842a4f2874
Quan Tian <qtian@vmware.com>
2019-10-10 19:04:00 +0800

    Minor updates in code style and comments of package cni
    * Merge and indent error handling flow to improve the readability.
    * Add comments to explain the usage of the function variable and the
      Request method.

--- pkg/cni/client_test.go
+++ pkg/cni/client_test.go
@@ -2,13 +2,14 @@ package cni
 
 import (
 	"context"
-	cnipb "okn/pkg/apis/cni"
 	"testing"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	cnipb "okn/pkg/apis/cni"
 )
 
 type testClient struct {
