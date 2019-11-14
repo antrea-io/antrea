bd10c3701b654161cc98c0a874857efe0e752f33
Quan Tian <qtian@vmware.com>
2019-11-13 23:47:17 -0800

    Implement agent side Network Policy controller (#53)
    This patch adds a Network Policy controller which watching Antrea
    AddressGroups, AppliedToGroups, and NetworkPolicies, feeding them to
    ruleCache, getting dirty rules from ruleCache, invoking reconciler to
    reconcile them with Openflow entries.
    
    This patch implements ruleCache that maintains the desired state of
    NetworkPolicy rules and uses a noop reconciler implementation
    temporarily.

---
+++ pkg/agent/client.go
@@ -0,0 +1,81 @@
+package agent
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/cert"
+	"k8s.io/component-base/config"
+	"k8s.io/klog"
+
+	"github.com/vmware-tanzu/antrea/pkg/client/clientset/versioned"
+)
+
+// CreateAntreaClient creates an Antrea client from the given config.
+func CreateAntreaClient(config config.ClientConnectionConfiguration) (versioned.Interface, error) {
+	var kubeConfig *rest.Config
+	var err error
+
+	if len(config.Kubeconfig) == 0 {
+		klog.Info("No kubeconfig file was specified. Falling back to in-cluster config")
+		kubeConfig, err = rest.InClusterConfig()
+	} else {
+		kubeConfig, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			&clientcmd.ClientConfigLoadingRules{ExplicitPath: config.Kubeconfig},
+			&clientcmd.ConfigOverrides{}).ClientConfig()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	kubeConfig.AcceptContentTypes = config.AcceptContentTypes
+	kubeConfig.ContentType = config.ContentType
+	kubeConfig.QPS = config.QPS
+	kubeConfig.Burst = int(config.Burst)
+
+	client, err := versioned.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// inClusterConfig returns a config object which uses the service account
+// kubernetes gives to pods. It's intended for clients that expect to be
+// running inside a pod running on kubernetes. It will return error
+// if called from a process not running in a kubernetes environment.
+func inClusterConfig() (*rest.Config, error) {
+	const (
+		tokenFile  = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+		rootCAFile = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+	)
+	host, port := os.Getenv("ANTREA_SERVICE_HOST"), os.Getenv("ANTREA_SERVICE_PORT")
+	if len(host) == 0 || len(port) == 0 {
+		return nil, fmt.Errorf("unable to load in-cluster configuration, ANTREA_SERVICE_HOST and ANTREA_SERVICE_PORT must be defined")
+	}
+
+	token, err := ioutil.ReadFile(tokenFile)
+	if err != nil {
+		return nil, err
+	}
+
+	tlsClientConfig := rest.TLSClientConfig{}
+
+	if _, err := cert.NewPool(rootCAFile); err != nil {
+		klog.Errorf("Expected to load root CA config from %s, but got err: %v", rootCAFile, err)
+	} else {
+		tlsClientConfig.CAFile = rootCAFile
+	}
+
+	return &rest.Config{
+		Host:            "https://" + net.JoinHostPort(host, port),
+		TLSClientConfig: tlsClientConfig,
+		BearerToken:     string(token),
+		BearerTokenFile: tokenFile,
+	}, nil
+}
---
+++ pkg/agent/controller/networkpolicy/networkpolicy_controller.go
@@ -0,0 +1,315 @@
+// Copyright 2019 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package networkpolicy
+
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog"
+
+	"github.com/vmware-tanzu/antrea/pkg/agent"
+	"github.com/vmware-tanzu/antrea/pkg/agent/openflow"
+	"github.com/vmware-tanzu/antrea/pkg/apis/networkpolicy/v1beta1"
+	"github.com/vmware-tanzu/antrea/pkg/client/clientset/versioned"
+)
+
+const (
+	// How long to wait before retrying the processing of a network policy change.
+	minRetryDelay = 5 * time.Second
+	maxRetryDelay = 300 * time.Second
+	// Default number of workers processing a rule change.
+	defaultWorkers = 4
+)
+
+// Controller is responsible for watching Antrea AddressGroups, AppliedToGroups,
+// and NetworkPolicies, feeding them to ruleCache, getting dirty rules from
+// ruleCache, invoking reconciler to reconcile them.
+//
+//          a.Feed AddressGroups,AppliedToGroups
+//               and NetworkPolicies
+//  |-----------|    <--------    |----------- |  c. Reconcile dirty rules |----------- |
+//  | ruleCache |                 | Controller |     ------------>         | reconciler |
+//  | ----------|    -------->    |----------- |                           |----------- |
+//              b. Notify dirty rules
+//
+type Controller struct {
+	// nodeName is the name of this node, which is used to filter resources
+	// when watching resources.
+	nodeName string
+	// antreaClient provides interfaces to watch Antrea AddressGroups,
+	// AppliedToGroups, and NetworkPolicies.
+	antreaClient versioned.Interface
+	// queue maintains the NetworkPolicy ruleIDs that need to be synced.
+	queue workqueue.RateLimitingInterface
+	// ruleCache maintains the desired state of NetworkPolicy rules.
+	ruleCache *ruleCache
+	// reconciler provides interfaces to reconcile the desired state of
+	// NetworkPolicy rules with the actual state of Openflow entries.
+	reconciler Reconciler
+}
+
+// NewNetworkPolicyController returns a new *Controller.
+func NewNetworkPolicyController(antreaClient versioned.Interface, ofClient openflow.Client, ifaceStore agent.InterfaceStore, nodeName string) *Controller {
+	c := &Controller{
+		antreaClient: antreaClient,
+		nodeName:     nodeName,
+		queue:        workqueue.NewNamedRateLimitingQueue(workqueue.NewItemExponentialFailureRateLimiter(minRetryDelay, maxRetryDelay), "networkpolicyrule"),
+		reconciler:   &NoopReconciler{},
+	}
+	c.ruleCache = newRuleCache(c.enqueueRule)
+	return c
+}
+
+// Run begins watching and processing Antrea AddressGroups, AppliedToGroups
+// and NetworkPolicies, and spawns workers that reconciles NetworkPolicy rules.
+// Run will not return until stopCh is closed.
+func (c *Controller) Run(stopCh <-chan struct{}) error {
+	go wait.Until(c.watchAppliedToGroups, 5*time.Second, stopCh)
+	go wait.Until(c.watchAddressGroups, 5*time.Second, stopCh)
+	go wait.Until(c.watchNetworkPolicies, 5*time.Second, stopCh)
+
+	for i := 0; i < defaultWorkers; i++ {
+		go wait.Until(c.worker, time.Second, stopCh)
+	}
+
+	<-stopCh
+	return nil
+}
+
+func (c *Controller) enqueueRule(ruleID string) {
+	c.queue.Add(ruleID)
+}
+
+// worker runs a worker thread that just dequeues items, processes them, and
+// marks them done. You may run as many of these in parallel as you wish; the
+// workqueue guarantees that they will not end up processing the same rule at
+// the same time.
+func (c *Controller) worker() {
+	for c.processNextWorkItem() {
+	}
+}
+
+func (c *Controller) processNextWorkItem() bool {
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	defer c.queue.Done(key)
+
+	err := c.syncRule(key.(string))
+	c.handleErr(err, key)
+
+	return true
+}
+
+func (c *Controller) syncRule(key string) error {
+	startTime := time.Now()
+	defer func() {
+		klog.V(4).Infof("Finished syncing rule %q. (%v)", key, time.Since(startTime))
+	}()
+
+	rule, exists, completed := c.ruleCache.GetCompletedRule(key)
+	if !exists {
+		klog.V(2).Infof("Rule %v had been deleted, removing its flows", key)
+		if err := c.reconciler.Forget(key); err != nil {
+			return err
+		}
+		return nil
+	}
+	// If the rule is not complete, we can simply skip it as it will be marked as dirty
+	// and queued again when we receive the missing group it missed.
+	if !completed {
+		klog.V(2).Infof("Rule %v was not complete, skipping", key)
+		return nil
+	}
+	if err := c.reconciler.Reconcile(rule); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Controller) handleErr(err error, key interface{}) {
+	if err == nil {
+		c.queue.Forget(key)
+		return
+	}
+
+	klog.Errorf("Error syncing rule %q, retrying. Error: %v", key, err)
+	c.queue.AddRateLimited(key)
+}
+
+func (c *Controller) nodeScopedListOptions() metav1.ListOptions {
+	options := metav1.ListOptions{}
+	options.FieldSelector = fields.OneTermEqualSelector("nodeName", c.nodeName).String()
+	return options
+}
+
+func (c *Controller) watchAppliedToGroups() {
+	// TODO: Cleanup AppliedToGroups that are removed during reconnection.
+	klog.Info("Start watching AppliedToGroups")
+	options := c.nodeScopedListOptions()
+	w, err := c.antreaClient.NetworkpolicyV1beta1().AppliedToGroups().Watch(options)
+	if err != nil {
+		klog.Errorf("Failed to watch AppliedToGroups: %v", err)
+		return
+	}
+
+	eventCount := 0
+	defer func() {
+		klog.Infof("Stop watching AppliedToGroups, total %v items received", eventCount)
+		w.Stop()
+	}()
+
+	for {
+		select {
+		case event, ok := <-w.ResultChan():
+			if !ok {
+				return
+			}
+			switch event.Type {
+			case watch.Added:
+				group, ok := event.Object.(*v1beta1.AppliedToGroup)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.AppliedToGroup: %v", event.Object)
+					return
+				}
+				klog.V(2).Infof("Added AppliedToGroup (%#v)", event.Object)
+				c.ruleCache.AddAppliedToGroup(group)
+			case watch.Modified:
+				patch, ok := event.Object.(*v1beta1.AppliedToGroupPatch)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.AppliedToGroupPatch: %v", event.Object)
+					return
+				}
+				klog.V(2).Infof("Patched AppliedToGroup (%#v)", event.Object)
+				c.ruleCache.PatchAppliedToGroup(patch)
+			case watch.Deleted:
+				group, ok := event.Object.(*v1beta1.AppliedToGroup)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.AppliedToGroup: %v", event.Object)
+					return
+				}
+				klog.V(2).Infof("Removed AppliedToGroup (%#v)", event.Object)
+				c.ruleCache.DeleteAppliedToGroup(group)
+			}
+			eventCount++
+		}
+	}
+}
+
+func (c *Controller) watchAddressGroups() {
+	// TODO: Cleanup AddressGroups that are removed during reconnection.
+	klog.Info("Start watching AddressGroups")
+	options := c.nodeScopedListOptions()
+	w, err := c.antreaClient.NetworkpolicyV1beta1().AddressGroups().Watch(options)
+	if err != nil {
+		klog.Errorf("Failed to watch AddressGroups: %v", err)
+		return
+	}
+
+	eventCount := 0
+	defer func() {
+		klog.Infof("Stop watching AddressGroups, total %v items received", eventCount)
+		w.Stop()
+	}()
+
+	for {
+		select {
+		case event, ok := <-w.ResultChan():
+			if !ok {
+				return
+			}
+			switch event.Type {
+			case watch.Added:
+				group, ok := event.Object.(*v1beta1.AddressGroup)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.AddressGroup: %v", event.Object)
+					return
+				}
+				c.ruleCache.AddAddressGroup(group)
+			case watch.Modified:
+				patch, ok := event.Object.(*v1beta1.AddressGroupPatch)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.AddressGroupPatch: %v", event.Object)
+					return
+				}
+				c.ruleCache.PatchAddressGroup(patch)
+			case watch.Deleted:
+				group, ok := event.Object.(*v1beta1.AddressGroup)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.AddressGroup: %v", event.Object)
+					return
+				}
+				c.ruleCache.DeleteAddressGroup(group)
+			}
+			eventCount++
+		}
+	}
+}
+
+func (c *Controller) watchNetworkPolicies() {
+	// TODO: Cleanup NetworkPolicies that are removed during reconnection.
+	klog.Info("Start watching NetworkPolicies")
+	options := c.nodeScopedListOptions()
+	w, err := c.antreaClient.NetworkpolicyV1beta1().NetworkPolicies("").Watch(options)
+	if err != nil {
+		klog.Errorf("Failed to watch NetworkPolicies: %v", err)
+		return
+	}
+
+	eventCount := 0
+	defer func() {
+		klog.Infof("Stop watching NetworkPolicies, total %v items received", eventCount)
+		w.Stop()
+	}()
+
+	for {
+		select {
+		case event, ok := <-w.ResultChan():
+			if !ok {
+				return
+			}
+			switch event.Type {
+			case watch.Added:
+				policy, ok := event.Object.(*v1beta1.NetworkPolicy)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.NetworkPolicy: %v", event.Object)
+					return
+				}
+				c.ruleCache.AddNetworkPolicy(policy)
+			case watch.Modified:
+				policy, ok := event.Object.(*v1beta1.NetworkPolicy)
+				if !ok {
+					klog.Errorf("Cannot convert to *v1beta1.NetworkPolicy: %v", event.Object)
+					return
+				}
+				c.ruleCache.UpdateNetworkPolicy(policy)
+			case watch.Deleted:
+				policy, ok := event.Object.(*v1beta1.NetworkPolicy)
+				if !ok {
+					klog.Errorf("cannot convert to *v1beta1.NetworkPolicy: %v", event.Object)
+					return
+				}
+				c.ruleCache.DeleteNetworkPolicy(policy)
+			}
+			eventCount++
+		}
+	}
+}
