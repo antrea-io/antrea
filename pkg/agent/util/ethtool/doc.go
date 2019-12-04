19143ec06ccc3f3a634fec2fe049ea2aa38817b0
Weiqiang Tang <weiqiangt@vmware.com>
2019-12-04 08:44:26 +0800

    Update pkg/agent/util/ethtool/doc.go
    Co-Authored-By: Antonin Bas <antonin.bas@gmail.com>

--- pkg/agent/util/ethtool/doc.go
+++ pkg/agent/util/ethtool/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package ethtool provides functions that make antrea to run on Kind.
+// Package ethtool provides Go wrappers for ioctl ethtool system calls on Linux
 package ethtool
