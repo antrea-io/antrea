2ac48ff201fa955c404962ac81a1987aeb0ebc35
Kobi Samoray <ksamoray@vmware.com>
2020-04-21 10:18:56 +0300

    Apply authentication to agent apiserver endpoint (#622)
    Antrea agent uses apiserver to interact with antctl. However, unlike the
    controller, the API endpoint is exposed only locally which limits antctl
    to local execution.
    Additionally, we would like to reuse the listener for Prometheus metrics,
    which would require external access to the API endpoint's metrics path.
    
    Having authentication in a similar way to the controller's implementation
    could help here as it will allow external exposure of the API endpoint.

--- pkg/agent/apiserver/apiserver.go
+++ pkg/agent/apiserver/apiserver.go
@@ -77,27 +77,36 @@ func New(aq agentquerier.AgentQuerier, npq querier.AgentNetworkPolicyInfoQuerier
 
 func newConfig(bindPort int) (*genericapiserver.CompletedConfig, error) {
 	secureServing := genericoptions.NewSecureServingOptions().WithLoopback()
+	authentication := genericoptions.NewDelegatingAuthenticationOptions()
+	authorization := genericoptions.NewDelegatingAuthorizationOptions()
+
 	// Set the PairName but leave certificate directory blank to generate in-memory by default.
 	secureServing.ServerCert.CertDirectory = ""
 	secureServing.ServerCert.PairName = Name
-	secureServing.BindAddress = net.ParseIP("127.0.0.1")
 	secureServing.BindPort = bindPort
+
 	if err := secureServing.MaybeDefaultWithSelfSignedCerts("localhost", nil, []net.IP{net.ParseIP("127.0.0.1")}); err != nil {
 		return nil, fmt.Errorf("error creating self-signed certificates: %v", err)
 	}
+	serverConfig := genericapiserver.NewConfig(codecs)
+	if err := secureServing.ApplyTo(&serverConfig.SecureServing, &serverConfig.LoopbackClientConfig); err != nil {
+		return nil, err
+	}
+	if err := authentication.ApplyTo(&serverConfig.Authentication, serverConfig.SecureServing, nil); err != nil {
+		return nil, err
+	}
+	if err := authorization.ApplyTo(&serverConfig.Authorization); err != nil {
+		return nil, err
+	}
 	v := antreaversion.GetVersion()
-	serverCfg := genericapiserver.NewConfig(codecs)
-	serverCfg.Version = &k8sversion.Info{
+	serverConfig.Version = &k8sversion.Info{
 		Major:        fmt.Sprint(v.Major),
 		Minor:        fmt.Sprint(v.Minor),
 		GitVersion:   v.String(),
 		GitTreeState: antreaversion.GitTreeState,
 		GitCommit:    antreaversion.GetGitSHA(),
 	}
-	if err := secureServing.ApplyTo(&serverCfg.SecureServing, &serverCfg.LoopbackClientConfig); err != nil {
-		return nil, err
-	}
 
-	completedServerCfg := serverCfg.Complete(nil)
+	completedServerCfg := serverConfig.Complete(nil)
 	return &completedServerCfg, nil
 }
