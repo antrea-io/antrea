e0df6b7b80c2ed3540575a586a65cb867a63d183
Kobi Samoray <ksamoray@vmware.com>
2020-04-29 03:38:08 +0300

    Antrea Prometheus integration (#321)
    Integrate with Prometheus monitoring solution.
    Integration of the Prometheus client into Antrea controller and agent
    allows the exposure of various metrics to Prometheus server.
    In addition to Antrea's own set of metrics, Prometheus client will also
    expose metrics which are defined by various components which are part of
    the Antrea ecosystem, e.g golang, Prometheus itself etc.
    
    See #236

---
+++ pkg/util/env/env.go
@@ -0,0 +1,54 @@
+// Copyright 2020 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package env
+
+import (
+	"os"
+
+	"k8s.io/klog"
+)
+
+// nodeNameEnvKey is environment variable.
+const (
+	nodeNameEnvKey = "NODE_NAME"
+	podNameEnvKey  = "POD_NAME"
+)
+
+// GetNodeName returns the node's name used in Kubernetes, based on the priority:
+// - Environment variable NODE_NAME, which should be set by Downward API
+// - OS's hostname
+func GetNodeName() (string, error) {
+	nodeName := os.Getenv(nodeNameEnvKey)
+	if nodeName != "" {
+		return nodeName, nil
+	}
+	klog.Infof("Environment variable %s not found, using hostname instead", nodeNameEnvKey)
+	var err error
+	nodeName, err = os.Hostname()
+	if err != nil {
+		klog.Errorf("Failed to get local hostname: %v", err)
+		return "", err
+	}
+	return nodeName, nil
+}
+
+// GetPodName returns the pod name where the code executes
+func GetPodName() string {
+	podName := os.Getenv(podNameEnvKey)
+	if podName == "" {
+		klog.Warningf("Environment variable %s not found", podNameEnvKey)
+	}
+	return podName
+}
