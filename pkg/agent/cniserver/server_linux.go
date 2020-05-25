13e9e046f390e12e6b25c81b5778f61dbc8dbcce
Rui Cao <rcao@vmware.com>
2020-05-25 08:53:09 +0800

    [Windows]Fix Pod IP changes issue during creation
    On windows platform, each container in a Pod will generate a CNI
    request. In current implementation every container have chance to
    be allocated with a IP in specific time sequence. Which may cause
    the Pod IP change during the creation.
    
    This patch fixes the issue by calling IPAM plugin only when handling
    CNI request from infrastructure container.
    
    Signed-off-by: Rui Cao <rcao@vmware.com>

--- pkg/agent/cniserver/server_linux.go
+++ pkg/agent/cniserver/server_linux.go
@@ -20,3 +20,19 @@ import "github.com/containernetworking/cni/pkg/types/current"
 func updateResultDNSConfig(result *current.Result, cniConfig *CNIConfig) {
 	result.DNS = cniConfig.DNS
 }
+
+// When running in a container, the host's /proc directory is mounted under s.hostProcPathPrefix, so
+// we need to prepend s.hostProcPathPrefix to the network namespace path provided by the cni. When
+// running as a simple process, s.hostProcPathPrefix will be empty.
+func (s *CNIServer) hostNetNsPath(netNS string) string {
+	if netNS == "" {
+		return ""
+	}
+	return s.hostProcPathPrefix + netNS
+}
+
+// isInfraContainer return if a container is infra container according to the network namespace path.
+// Always return true on Linux platform, because kubelet only call CNI request for infra container.
+func isInfraContainer(netNS string) bool {
+	return true
+}
