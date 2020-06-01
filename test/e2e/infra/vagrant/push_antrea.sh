d1918258aa2a88cac98930be857f97063f2e23bc
Antonin Bas <abas@vmware.com>
2020-06-01 10:13:06 -0700

    Fix Jenkins job YAML specification for e2e tests (#767)
    The `--prometheus` flag needs to come after the package name, or the job
    fails with the following error:
    "build .: cannot find module for path ."
    
    Also fix test/e2e/infra/vagrant/push_antrea.sh script for macOS.

--- test/e2e/infra/vagrant/push_antrea.sh
+++ test/e2e/infra/vagrant/push_antrea.sh
@@ -39,7 +39,7 @@ cp "${ANTREA_BASE_YML}" "${ANTREA_YML}"
 
 if [ "$RUN_PROMETHEUS" == "true" ]; then
     # Prepare Antrea yamls
-    sed -i 's|#enablePrometheusMetrics: false|enablePrometheusMetrics: true|g' "${ANTREA_YML}"
+    sed -i.bak -E 's|#enablePrometheusMetrics: false|enablePrometheusMetrics: true|g' "${ANTREA_YML}"
     echo "---" >> "${ANTREA_YML}"
     cat "${ANTREA_PROMETHEUS_YML}" >> "${ANTREA_YML}"
 fi
