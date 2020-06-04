b8ddd43d9886e2f88a6073630e081abdddadcdf4
Weiqiang TANG <weiqiangt@vmware.com>
2020-06-04 14:23:17 +0800

    Add service bandwidth benchmark
    - Service bandwidth of intra-node traffic
    - Service bandwidth of across nodes traffic
    - Enhance service creation functions in e2e framework
    
    Signed-off-by: Weiqiang TANG <weiqiangt@vmware.com>

--- test/e2e/bandwidth_test.go
+++ test/e2e/bandwidth_test.go
@@ -16,41 +16,85 @@ package e2e
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 	"testing"
+
+	v1 "k8s.io/api/core/v1"
 )
 
-// BenchmarkBandwidthIntraNode runs the benchmark of bandwidth between Pods on same node.
-func BenchmarkBandwidthIntraNode(b *testing.B) {
-	withPerfTestSetup(func(data *TestData) {
-		b.StartTimer()
+const iperfPort = 5201
 
-		podDefA := createPerfTestPodDefinition("perftest-a", perftoolContainerName, perftoolImage)
-		if _, err := data.clientset.CoreV1().Pods(testNamespace).Create(podDefA); err != nil {
-			b.Fatalf("Error when creating the first perftest Pod: %v", err)
-		}
-		podDefB := createPerfTestPodDefinition("perftest-b", perftoolContainerName, perftoolImage)
-		if _, err := data.clientset.CoreV1().Pods(testNamespace).Create(podDefB); err != nil {
-			b.Fatalf("Error when creating the second perftest Pod: %v", err)
-		}
-		podBIP, err := data.podWaitForIP(defaultTimeout, podDefB.Name, testNamespace)
-		if err != nil {
-			b.Fatalf("Error when getting perftest Pod IP: %v", err)
-		}
-		stdout, _, err := data.runCommandFromPod(testNamespace, "perftest-a", perftoolContainerName, []string{"bash", "-c", fmt.Sprintf("iperf3 -c %s|grep sender|awk '{print $7,$8}'", podBIP)})
-		if err != nil {
-			b.Fatalf("Error when running iperf3 client: %v", err)
-		}
-		stdout = strings.TrimSpace(stdout)
-		results := strings.Split(stdout, " ")
-		if len(results) != 2 {
-			b.Fatalf("Error when parsing iperf result: cannot parse output `%s`", stdout)
-		}
-		// Disable default output.
-		b.ReportMetric(0, "ns/op")
-		bandwidthNum, _ := strconv.ParseFloat(results[0], 64)
-		bandwidthUnit := strings.TrimSpace(results[1])
-		b.ReportMetric(bandwidthNum, bandwidthUnit)
-	}, b)
+// TestBenchmarkBandwidthIntraNode runs the bandwidth benchmark between Pods on same node.
+func TestBenchmarkBandwidthIntraNode(t *testing.T) {
+	skipIfNotBenchmarkTest(t)
+	data, err := setupTest(t)
+	if err != nil {
+		t.Fatalf("Error when setting up test: %v", err)
+	}
+	defer teardownTest(t, data)
+	if err := data.createPodOnNode("perftest-a", masterNodeName(), perftoolImage, nil, nil, nil, nil); err != nil {
+		t.Fatalf("Error when creating the perftest client Pod: %v", err)
+	}
+	if err := data.podWaitForRunning(defaultTimeout, "perftest-a", testNamespace); err != nil {
+		t.Fatalf("Error when waiting for the perftest client Pod: %v", err)
+	}
+	if err := data.createPodOnNode("perftest-b", masterNodeName(), perftoolImage, nil, nil, nil, []v1.ContainerPort{{Protocol: v1.ProtocolTCP, ContainerPort: iperfPort}}); err != nil {
+		t.Fatalf("Error when creating the perftest server Pod: %v", err)
+	}
+	podBIP, err := data.podWaitForIP(defaultTimeout, "perftest-b", testNamespace)
+	if err != nil {
+		t.Fatalf("Error when getting the perftest server Pod's IP: %v", err)
+	}
+	stdout, _, err := data.runCommandFromPod(testNamespace, "perftest-a", "perftool", []string{"bash", "-c", fmt.Sprintf("iperf3 -c %s|grep sender|awk '{print $7,$8}'", podBIP)})
+	if err != nil {
+		t.Fatalf("Error when running iperf3 client: %v", err)
+	}
+	stdout = strings.TrimSpace(stdout)
+	t.Logf("Bandwidth: %s", stdout)
+}
+
+func benchmarkBandwidthService(t *testing.T, endpointNode, clientNode string) {
+	data, err := setupTest(t)
+	if err != nil {
+		t.Fatalf("Error when setting up test: %v", err)
+	}
+	defer teardownTest(t, data)
+
+	svc, err := data.createService("perftest-b", iperfPort, iperfPort, map[string]string{"antrea-e2e": "perftest-b"})
+	if err != nil {
+		t.Fatalf("Error when creating perftest service: %v", err)
+	}
+	if err := data.createPodOnNode("perftest-a", clientNode, perftoolImage, nil, nil, nil, nil); err != nil {
+		t.Fatalf("Error when creating the perftest client Pod: %v", err)
+	}
+	if err := data.podWaitForRunning(defaultTimeout, "perftest-a", testNamespace); err != nil {
+		t.Fatalf("Error when waiting for the perftest client Pod: %v", err)
+	}
+	if err := data.createPodOnNode("perftest-b", endpointNode, perftoolImage, nil, nil, nil, []v1.ContainerPort{{Protocol: v1.ProtocolTCP, ContainerPort: iperfPort}}); err != nil {
+		t.Fatalf("Error when creating the perftest server Pod: %v", err)
+	}
+	if err := data.podWaitForRunning(defaultTimeout, "perftest-b", testNamespace); err != nil {
+		t.Fatalf("Error when getting the perftest server Pod's IP: %v", err)
+	}
+	stdout, stderr, err := data.runCommandFromPod(testNamespace, "perftest-a", perftoolContainerName, []string{"bash", "-c", fmt.Sprintf("iperf3 -c %s|grep sender|awk '{print $7,$8}'", svc.Spec.ClusterIP)})
+	if err != nil {
+		t.Fatalf("Error when running iperf3 client: %v, stderr: %s", err, stderr)
+	}
+	stdout = strings.TrimSpace(stdout)
+	t.Logf("Bandwidth: %s", stdout)
+}
+
+// TestBenchmarkBandwidthServiceLocalAccess runs the bandwidth benchmark of service
+// traffic between a Pod and an Endpoint on same Node.
+func TestBenchmarkBandwidthServiceLocalAccess(t *testing.T) {
+	skipIfNotBenchmarkTest(t)
+	benchmarkBandwidthService(t, masterNodeName(), masterNodeName())
+}
+
+// TestBenchmarkBandwidthServiceRemoteAccess runs the bandwidth benchmark of service
+// traffic between a Pod and an Endpoint on different Node.
+func TestBenchmarkBandwidthServiceRemoteAccess(t *testing.T) {
+	skipIfNotBenchmarkTest(t)
+	skipIfNumNodesLessThan(t, 2)
+	benchmarkBandwidthService(t, masterNodeName(), workerNodeName(1))
 }
