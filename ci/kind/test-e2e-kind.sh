e1e9d2cf4ae30fe3aab313c1c9e1858d9ceb473c
Antonin Bas <abas@vmware.com>
2020-07-02 15:58:08 -0700

    Fix NetworkPolicy failures in Kind clusters
    When running NetworkPolicy tests in Kind, we "force" the tunnel type to
    VXLAN as we observe some failures with Geneve (see #897).
    
    We also change the Github CI jobs so that CNP tests are run in their own
    job. This is to address the issue above, and is also a partial
    implementation of #893.

--- ci/kind/test-e2e-kind.sh
+++ ci/kind/test-e2e-kind.sh
@@ -18,31 +18,82 @@
 
 set -eo pipefail
 
+function echoerr {
+    >&2 echo "$@"
+}
+
+_usage="Usage: $0 [--encap-mode <mode>] [--proxy] [--np] [--help|-h]
+        --encap-mode                  Traffic encapsulation mode. (default is 'encap')
+        --proxy                       Enables Antrea proxy.
+        --np                          Enables Namespaced Antrea NetworkPolicy CRDs and ClusterNetworkPolicy related CRDs.
+        --help, -h                    Print this message and exit
+"
+
+function print_usage {
+    echoerr "$_usage"
+}
+
+
 TESTBED_CMD=$(dirname $0)"/kind-setup.sh"
 YML_CMD=$(dirname $0)"/../../hack/generate-manifest.sh"
 COMMON_IMAGES="busybox nginx antrea/antrea-ubuntu:latest"
 
 function quit {
   if [[ $? != 0 ]]; then
-    echo " Test failed cleaning testbed"
+    echoerr " Test failed cleaning testbed"
     $TESTBED_CMD destroy kind
   fi
 }
 trap "quit" INT EXIT
 
-function run_test {
-  mode=$1
-  proxy=$2
-  args=$3
-  if [[ $proxy != "--proxy" ]]; then
-    proxy=""
-    args=$2
-  fi
+mode=""
+proxy=false
+np=false
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-  echo "create test bed with args $args"
+case $key in
+    --proxy)
+    proxy=true
+    shift
+    ;;
+    --np)
+    np=true
+    shift
+    ;;
+    --encap-mode)
+    mode="$2"
+    shift 2
+    ;;
+    -h|--help)
+    print_usage
+    exit 0
+    ;;
+    *)    # unknown option
+    echoerr "Unknown option $1"
+    exit 1
+    ;;
+esac
+done
+
+manifest_args=""
+if $proxy; then
+    manifest_args="$manifest_args --proxy"
+fi
+if $np; then
+    # See https://github.com/vmware-tanzu/antrea/issues/897
+    manifest_args="$manifest_args --np --tun vxlan"
+fi
+
+function run_test {
+  current_mode=$1
+  args=$2
+
+  echo "creating test bed with args $args"
   eval "timeout 600 $TESTBED_CMD create kind --antrea-cni false $args"
 
-  $YML_CMD --kind --encap-mode $mode $proxy | docker exec -i kind-control-plane dd of=/root/antrea.yml
+  $YML_CMD --kind --encap-mode $current_mode $manifest_args | docker exec -i kind-control-plane dd of=/root/antrea.yml
   sleep 1
   go test -v -timeout=30m github.com/vmware-tanzu/antrea/test/e2e -provider=kind
   $TESTBED_CMD destroy kind
@@ -51,17 +102,17 @@ function run_test {
 docker pull busybox
 docker pull nginx
 
-if [[ $# == 0 ]] || [[ $1 == "encap" ]]; then
+if [[ "$mode" == "" ]] || [[ "$mode" == "encap" ]]; then
   echo "======== Test encap mode =========="
-  run_test encap $2 "--images \"$COMMON_IMAGES\""
+  run_test encap "--images \"$COMMON_IMAGES\""
 fi
-if [[ $# == 0 ]] || [[ $1 == "noEncap" ]]; then
+if [[ "$mode" == "" ]] || [[ "$mode" == "noEncap" ]]; then
   echo "======== Test noencap mode =========="
-  run_test noEncap $2 "--images \"$COMMON_IMAGES\""
+  run_test noEncap "--images \"$COMMON_IMAGES\""
 fi
-if [[ $# == 0 ]] || [[ $1 == "hybrid" ]]; then
+if [[ "$mode" == "" ]] || [[ "$mode" == "hybrid" ]]; then
   echo "======== Test hybrid mode =========="
-  run_test hybrid $2 "--subnets \"20.20.20.0/24\" --images \"$COMMON_IMAGES\""
+  run_test hybrid "--subnets \"20.20.20.0/24\" --images \"$COMMON_IMAGES\""
 fi
 exit 0
 
