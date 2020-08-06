98e8abcf7825d3ae06b330a63be4e13b732fb1e4
Antonin Bas <abas@vmware.com>
2020-08-05 18:57:24 -0700

    Use Ubuntu 20.04 as base distribution in Antrea Docker image
    The main reason for this update is picking up a more recent version of
    glibc, as the one that ships with Ubuntu 18.04 can cause OVS to deadlock
    (See #1022).
    
    In this PR, we only update the distribution for the "main" Antrea Docker
    image; other images, such as the ones we use for testing or for
    deploying the Antrea Octant plugin, can be updated later if needed.
    
    This is also a good opportunity to upgrade OVS daemons from 2.13.0 to
    2.13.1, since the Docker build had to be updated anyway. For the sake of
    simplicity, from now on we will only support building the base
    openvswitch Docker image for OVS >= 2.13.0.
    
    Fixes #1022

--- build/images/ovs/build_and_push.sh
+++ build/images/ovs/build_and_push.sh
@@ -24,7 +24,7 @@ function echoerr {
 }
 
 if [ -z "$OVS_VERSION" ]; then
-    echoerr "The OVS_VERSION env variable must be set to a valid value (e.g. 2.13.0)"
+    echoerr "The OVS_VERSION env variable must be set to a valid value (e.g. 2.13.1)"
     exit 1
 fi
 
@@ -39,7 +39,7 @@ pushd $THIS_DIR > /dev/null
 # locally.
 # See https://github.com/moby/moby/issues/34715.
 
-docker pull ubuntu:18.04
+docker pull ubuntu:20.04
 
 docker build --target ovs-debs \
        --cache-from antrea/openvswitch-debs:$OVS_VERSION \
