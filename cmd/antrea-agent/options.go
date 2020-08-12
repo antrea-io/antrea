b285cde06bada70e586550947d0555fbd1532662
Jianjun Shen <shenj@vmware.com>
2020-08-11 21:21:49 -0700

    Check AntreaProxy must be enabled for noEncap, hybrid, policyOnly modes (#1062)
    These modes work with AntreaProxy only and do not support kube-proxy
    any more.

--- cmd/antrea-agent/options.go
+++ cmd/antrea-agent/options.go
@@ -100,8 +100,13 @@ func (o *Options) validate(args []string) error {
 	if !ok {
 		return fmt.Errorf("TrafficEncapMode %s is unknown", o.config.TrafficEncapMode)
 	}
-	if encapMode.SupportsNoEncap() && o.config.EnableIPSecTunnel {
-		return fmt.Errorf("IPSec tunnel may only be enabled on %s mode", config.TrafficEncapModeEncap)
+	if encapMode.SupportsNoEncap() {
+		if !features.DefaultFeatureGate.Enabled(features.AntreaProxy) {
+			return fmt.Errorf("Mode %s requires AntreaProxy to be enabled", o.config.TrafficEncapMode)
+		}
+		if o.config.EnableIPSecTunnel {
+			return fmt.Errorf("IPSec tunnel may only be enabled on %s mode", config.TrafficEncapModeEncap)
+		}
 	}
 	if err := o.validateFlowExporterConfig(); err != nil {
 		return fmt.Errorf("Failed to validate flow exporter config: %v", err)
