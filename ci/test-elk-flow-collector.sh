797cfbcf2223fc064b06c39628fb5f6790ee5ba4
zyiou <zyiou@vmware.com>
2020-08-20 11:25:07 -0700

    Modified CI script and added doc

--- ci/test-elk-flow-collector.sh
+++ ci/test-elk-flow-collector.sh
@@ -4,18 +4,65 @@ LOGSTASH_PORT=4739
 LOGSTASH_IP="0.0.0.0"
 LOGSTASH_PROTOCOL="udp"
 FAILURE=false
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+GIT_CHECKOUT_DIR=${THIS_DIR}/..
+
+_usage="Usage: $0 [--kubeconfig <KubeconfigSavePath>]
+
+Setup Elastic stack (elk) flow collector and Antrea Agent flow exporter to validate elk-flow-collector.yml.
+
+    --kubeconfig    Path to kubeconfig.
+
+"
+
+echoerr() {
+  >&2 echo "$@"
+}
+
+print_usage() {
+  echoerr "$_usage"
+}
+
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --kubeconfig)
+    export KUBECONFIG="$2"
+    echo "kube"
+    shift 2
+    ;;
+    -h|--help)
+    print_usage
+    exit 0
+    ;;
+    *)
+    echoerr "Unknown option $1"
+    exit 1
+    ;;
+esac
+done
+
+start_antrea() {
+  echo "=== Starting Antrea ==="
+  kubectl apply -f ${GIT_CHECKOUT_DIR}/build/yamls/antrea.yml
+  kubectl rollout status --timeout=5m deployment/coredns -n kube-system
+  kubectl rollout status --timeout=5m deployment.apps/antrea-controller -n kube-system
+  kubectl rollout status --timeout=5m daemonset/antrea-agent -n kube-system
+}
 
 setup_flow_collector() {
   echo "=== Starting Flow Collector ==="
-  kubectl create namespace antrea-flow-collector
-  kubectl create configmap logstash-configmap -n antrea-flow-collector --from-file=${GIT_CHECKOUT_DIR}/build/yamls/flow-collector/logstash/
-  kubectl apply -f ${GIT_CHECKOUT_DIR}/build/yamls/flow-collector/flow-collector.yml -n antrea-flow-collector
+  kubectl create namespace elk-flow-collector
+  kubectl create configmap logstash-configmap -n elk-flow-collector --from-file=${GIT_CHECKOUT_DIR}/build/yamls/elk-flow-collector/logstash/
+  kubectl apply -f ${GIT_CHECKOUT_DIR}/build/yamls/elk-flow-collector/elk-flow-collector.yml -n elk-flow-collector
   echo "=== Waiting for Elastic Stack to be ready ==="
-  kubectl wait --for=condition=ready pod -l app=elasticsearch -n antrea-flow-collector --timeout=600s
-  kubectl wait --for=condition=ready pod -l app=kibana -n antrea-flow-collector
-  kubectl wait --for=condition=ready pod -l app=logstash -n antrea-flow-collector --timeout=600s
+  kubectl wait --for=condition=ready pod -l app=kibana -n elk-flow-collector --timeout=600s
+  kubectl wait --for=condition=ready pod -l app=logstash -n elk-flow-collector --timeout=600s
+  kubectl wait --for=condition=ready pod -l app=elasticsearch -n elk-flow-collector --timeout=600s
   # get cluster-ip of logstash
-  LOGSTASH_IP=$(kubectl get svc logstash -n antrea-flow-collector -o jsonpath='{.spec.clusterIP}')
+  LOGSTASH_IP=$(kubectl get svc logstash -n elk-flow-collector -o jsonpath='{.spec.clusterIP}')
   if [ ${LOGSTASH_PROTOCOL} = "udp" ]; then
     nc -zvu ${LOGSTASH_IP} ${LOGSTASH_PORT}
   fi
@@ -29,40 +76,35 @@ config_antrea() {
   sed -i -e "s/#flowCollectorAddr.*/flowCollectorAddr: \"${LOGSTASH_IP}:${LOGSTASH_PORT}:${LOGSTASH_PROTOCOL}\"/g" ${GIT_CHECKOUT_DIR}/build/yamls/antrea.yml
   sed -i -e "s/#  FlowExporter: false/  FlowExporter: true/g" ${GIT_CHECKOUT_DIR}/build/yamls/antrea.yml
   sed -i -e "s/#  AntreaProxy: false/  AntreaProxy: true/g" ${GIT_CHECKOUT_DIR}/build/yamls/antrea.yml
-  echo "=== Restarting Antrea ==="
-  kubectl apply -f ${GIT_CHECKOUT_DIR}/build/yamls/antrea.yml
-  sleep 20
-  kubectl wait --for=condition=ready pod -l app=antrea -n kube-system --timeout=300s
-  echo "=== Antrea Agent flow exporter starts to send data ==="
 }
 
 # Antrea agent flow exporter starts to send CoreDNS flow records.
 # It will check if flow records with one of desired fields (soursePodName) are received correctly.
 check_record() {
-  echo "=== Wait for 5 minutes to receive data ==="
-  sleep 5m
-   # if the records are received in logstash and processed correctly, the logstash logs should show the formatted data, which have 'sourcePodName' field
-  LOGSTASH_LOGS=$(kubectl logs -n antrea-flow-collector $(kubectl -n antrea-flow-collector get pod -l app=logstash -o jsonpath="{.items[0].metadata.name}"))
-  if ( echo ${LOGSTASH_LOGS} | grep -q 'sourcePodName' ); then
-    echo "=== Record is received correctly ==="
-  else
-    echo "=== Record is NOT received correctly ==="
-    FAILURE=true
-  fi
+  echo "=== Wait for up to 5 minutes to receive data ==="
+  for i in `seq 5`
+  do
+    sleep 1m
+    echo "=== Get flow record (try for 1m) ==="
+    # if the records are received in logstash and processed correctly, the logstash logs should show the formatted data, which have 'sourcePodName' field
+    LOGSTASH_LOGS=$(kubectl logs -n elk-flow-collector $(kubectl -n elk-flow-collector get pod -l app=logstash -o jsonpath="{.items[0].metadata.name}"))
+    if ( echo ${LOGSTASH_LOGS} | grep -q 'sourcePodName' ); then
+      echo "=== Record is received correctly ==="
+      break
+    elif [ $i == 5 ]; then
+      echo "=== Record is NOT received correctly ==="
+      FAILURE=true
+    fi
+  done
 }
 
-cleanup() {
-  echo "=== Removing Elastic Stack ==="
-  kubectl delete -f ${GIT_CHECKOUT_DIR}/build/yamls/flow-collector/flow-collector.yml  -n antrea-flow-collector
-  kubectl delete configmap logstash-configmap -n antrea-flow-collector
-  kubectl delete namespace antrea-flow-collector
-  echo "=== Elastic Stack has been removed ==="
-}
-
+start_antrea
 setup_flow_collector
 config_antrea
+start_antrea
 check_record
-cleanup
+
 if ( ${FAILURE} == true ); then
-  exit 1
+  echo "=== TEST FAILURE !! ==="
+  touch TEST_FAILURE
 fi
