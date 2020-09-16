410f137682507377e2c9e1681bb69a3d56b56a6e
Antonin Bas <abas@vmware.com>
2020-09-15 17:17:32 -0700

    Download all artifacts from https://downloads.antrea.io
    Fixes #1254
    
    The SHA256 checksum verification for the OVS Windows binaries is no
    longer required for security reasons, but it may be a good idea to keep
    it (there is no real maintenance burden, we just need to update it
    whenever the link is updated). It can help ensure that there is no issue
    on our side and that the file that was uploaded to S3 by us is the
    correct one. There is no burden on users. We can also remove that code
    altogether.

--- hack/windows/Install-OVS.ps1
+++ hack/windows/Install-OVS.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = "Stop"
 # TODO: set up HTTPS so that the archive can be downloaded securely. In the
 # meantime, we use a SHA256 hash to ensure that the downloaded archive is
 # correct.
-$OVSDownloadURL = "http://downloads.antrea.io/ovs/ovs-2.14.0-antrea.1-win64.zip"
+$OVSDownloadURL = "https://downloads.antrea.io/ovs/ovs-2.14.0-antrea.1-win64.zip"
 $OVSPublishedHash = 'E81800A6B8E157C948BAE548E5AFB425B2AD98CE18BC8C6148AB5B7F81E76B7D'
 $OVSDownloadDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 $InstallLog = "$OVSDownloadDir\install.log"
