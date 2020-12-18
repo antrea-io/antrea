5fa4e72551be82f389ce3f8dc7db88d34f2b44bc
Zhecheng Li <lzhecheng@vmware.com>
2020-12-18 09:10:37 +0800

    [CI] Use K8s v1.19.1 in VMC Jenkins (#1666)

--- ci/jenkins/test-vmc.sh
+++ ci/jenkins/test-vmc.sh
@@ -162,7 +162,7 @@ function saveLogs() {
 function setup_cluster() {
     export KUBECONFIG=$KUBECONFIG_PATH
     if [ -z $K8S_VERSION ]; then
-      export K8S_VERSION=v1.18.2
+      export K8S_VERSION=v1.19.1
     fi
     if [ -z $TEST_OS ]; then
       export TEST_OS=ubuntu-1804
