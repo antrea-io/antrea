696775808ea42384fb096769e725553e6043576a
Antonin Bas <abas@vmware.com>
2020-12-23 18:25:58 -0800

    Use default K8s version supported by AKS for cloud CI (#1701)

--- ci/test-conformance-aks.sh
+++ ci/test-conformance-aks.sh
@@ -40,7 +40,7 @@ Setup a AKS cluster to run K8s e2e community tests (Conformance & Network Policy
 
         --cluster-name           The cluster name to be used for the generated AKS cluster. Must be specified if not run in Jenkins environment.
         --kubeconfig             Path to save kubeconfig of generated AKS cluster.
-        --k8s-version            AKS K8s cluster version. Defaults to first supported version in the Azure region.
+        --k8s-version            AKS K8s cluster version. Defaults to the default K8s version for AKS in the Azure region.
         --azure-app-id           Azure Service Principal Application ID.
         --azure-tenant-id        Azure Service Principal Tenant ID.
         --azure-password         Azure Service Principal Password.
@@ -142,7 +142,7 @@ function setup_aks() {
     az group create --name ${RESOURCE_GROUP} --location $REGION
 
     if [[ -z ${K8S_VERSION+x} ]]; then
-        K8S_VERSION=$(az aks get-versions -l ${REGION} | grep "orchestratorVersion" | head -n1 | cut -d'"' -f4)
+        K8S_VERSION=$(az aks get-versions -l ${REGION} -o json | jq -r '.orchestrators[] | select(.default==true).orchestratorVersion')
     fi
 
     echo '=== Creating a cluster in AKS ==='
