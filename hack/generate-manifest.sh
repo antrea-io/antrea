a68f78185566c6ddea08423204aa0b8eace3bee8
Bin Liu <biliu@vmware.com>
2021-01-12 12:18:38 +0800

    Implement agent simulator as an independent bin (#1493)
    This patch implements agent simulator as add an independent bin, it runs with kubemark
    which simulates kubelet, and mainly watches NetworkPolicies, AddressGroups and
    AppliedToGroups from antrea controller and prints the events of these resources to log.
    With agent simulator, we do not need to lauch large cluster for scale test.
    
    The agent simulator uses labels and nodeaffinity to disable antrea-agent/antrea-controller
    running on the simulated nodes, and we can add some taints to not allow other pods to run on
    simulated nodes.
    
    To use agent simulator, please refer to docs/antrea-agent-simulator.md.

--- hack/generate-manifest.sh
+++ hack/generate-manifest.sh
@@ -38,6 +38,7 @@ Generate a YAML manifest for Antrea using Kustomize and print it to stdout.
         --on-delete                   Generate a manifest with antrea-agent's update strategy set to OnDelete.
                                       This option will work only for Kind clusters (when using '--kind').
         --coverage                    Generates a manifest which supports measuring code coverage of Antrea binaries.
+        --simulator                   Generates a manifest with antrea-agent simulator included
         --help, -h                    Print this message and exit
 
 In 'release' mode, environment variables IMG_NAME and IMG_TAG must be set.
@@ -70,6 +71,7 @@ VERBOSE_LOG=false
 ON_DELETE=false
 COVERAGE=false
 K8S_115=false
+SIMULATOR=false
 
 while [[ $# -gt 0 ]]
 do
@@ -130,6 +132,10 @@ case $key in
     ;;
     --coverage)
     COVERAGE=true
+    shift
+    ;;
+    --simulator)
+    SIMULATOR=true
     shift
     ;;
     -h|--help)
@@ -318,6 +324,18 @@ if [[ $CLOUD == "EKS" ]]; then
     $KUSTOMIZE edit add base $BASE
     $KUSTOMIZE edit add patch --path eksEnv.yml
     BASE=../eks
+    cd ..
+fi
+
+if $SIMULATOR; then
+    mkdir simulator && cd simulator
+    cp ../../patches/simulator/*.yml .
+    touch kustomization.yml
+    $KUSTOMIZE edit add base $BASE
+    $KUSTOMIZE edit add patch --path agentNodeAffinity.yml
+    $KUSTOMIZE edit add patch --path controllerNodeAffinity.yml
+    $KUSTOMIZE edit add resource antrea-agent-simulator.yml
+    BASE=../simulator
     cd ..
 fi
 
