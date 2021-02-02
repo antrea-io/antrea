fa7dc85e7c19732b7eba0f1a05f0336c786847e2
Matthew Fenwick <mfenwick100@gmail.com>
2021-02-02 06:07:36 -0500

    look at job output

--- hack/netpol-generator/README.md
+++ hack/netpol-generator/README.md
@@ -15,29 +15,3 @@ Download a [Cyclonus release](https://github.com/mattfenwick/cyclonus/releases),
 ```bash
 $ cyclonus generate --mode=simple-fragments --noisy=true --cleanup-namespaces=true
 ```
-
-## More options
-
-```bash
-$ cyclonus_0.1.1 generate -h
-generate network policies, create and probe against kubernetes, and compare to expected results
-
-Usage:
-  cyclonus generate [flags]
-
-Flags:
-      --allow-dns                          if using egress, allow udp over port 53 for DNS resolution (default true)
-      --cleanup-namespaces                 if true, clean up namespaces after completion
-      --context string                     kubernetes context to use; if empty, uses default context
-  -h, --help                               help for generate
-      --ignore-loopback                    if true, ignore loopback for truthtable correctness verification
-      --mode string                        mode used to generate network policies
-      --namespaces strings                 namespaces to create/use pods in (default [x,y,z])
-      --noisy                              if true, print all results
-      --perturbation-wait-seconds int      number of seconds to wait after perturbing the cluster (i.e. create a network policy, modify a ns/pod label) before running probes, to give the CNI time to update the cluster state (default 5)
-      --pod-creation-timeout-seconds int   number of seconds to wait for pods to create, be running and have IP addresses (default 60)
-      --pods strings                       pods to create in namespaces (default [a,b,c])
-
-Global Flags:
-  -v, --verbosity string   log level; one of [info, debug, trace, warn, error, fatal, panic] (default "info")
-```
--- hack/netpol-generator/test-kind.sh
+++ hack/netpol-generator/test-kind.sh
@@ -8,12 +8,12 @@ ROOT_DIR=$THIS_DIR/../..
 
 KIND_CONFIG=$ROOT_DIR/ci/kind/config-3nodes.yml
 
-WAIT_TIMEOUT=45m
+WAIT_TIMEOUT=240m
 JOB_NAME=job.batch/cyclonus
 
 
-kind create cluster --config $KIND_CONFIG
-kind get nodes | xargs $ROOT_DIR/hack/kind-fix-networking.sh
+kind create cluster --config "$KIND_CONFIG"
+kind get nodes | xargs "$ROOT_DIR"/hack/kind-fix-networking.sh
 kind load docker-image projects.registry.vmware.com/antrea/antrea-ubuntu:latest
 
 # pre-load cyclonus image
@@ -23,12 +23,12 @@ kind load docker-image mfenwick100/cyclonus:latest
 docker pull k8s.gcr.io/e2e-test-images/agnhost:2.21
 kind load docker-image k8s.gcr.io/e2e-test-images/agnhost:2.21
 
-$ROOT_DIR/hack/generate-manifest.sh --kind --tun "vxlan" | kubectl apply -f -
+"$ROOT_DIR"/hack/generate-manifest.sh --kind --tun "vxlan" | kubectl apply -f -
 
 
 kubectl create clusterrolebinding cyclonus --clusterrole=cluster-admin --serviceaccount=kube-system:cyclonus
 kubectl create sa cyclonus -n kube-system
-kubectl create -f $THIS_DIR/install-cyclonus.yml
+kubectl create -f "$THIS_DIR"/install-cyclonus.yml
 
 
 time kubectl wait --for=condition=complete --timeout=$WAIT_TIMEOUT -n kube-system $JOB_NAME
@@ -36,12 +36,11 @@ time kubectl wait --for=condition=complete --timeout=$WAIT_TIMEOUT -n kube-syste
 echo "===> Checking cyclonus results <==="
 
 LOG_FILE=$(mktemp)
-kubectl logs -n kube-system job.batch/cyclonus > $LOG_FILE
-RESULT_STR=$(grep "TEST FAILURES" $LOG_FILE)
-echo $RESULT_STR
+kubectl logs -n kube-system job.batch/cyclonus > "$LOG_FILE"
+cat "$LOG_FILE"
+
 rc=0
-echo $RESULT_STR | grep "0/" > /dev/null 2>&1 || rc=$?
+cat "$LOG_FILE" | grep "discrepancy" > /dev/null 2>&1 || rc=$?
 if [ $rc -ne 0 ]; then
-    cat $LOG_FILE
     exit 1
 fi
