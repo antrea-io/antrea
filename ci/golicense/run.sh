691641c723e4fb2a9ca2955d4db3e8c2bc16f2d2
Antonin Bas <abas@vmware.com>
2021-02-02 16:42:49 -0800

    Replace golicense with lichen (#1800)
    golicense is no longer actively maintained and does not support Windows
    binaries. To avoid issues like #1663 in the future, we switch over to
    lichen, a project heavily inspired from golicense. It's a fairly recent
    project but works very well.

--- ci/golicense/run.sh
+++ ci/golicense/run.sh
@@ -7,11 +7,7 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-if [ -z "$GITHUB_TOKEN" ]; then
-    echo "GITHUB_TOKEN environment variable must be set to avoid aggressive API rate limiting"
-    exit 1
-fi
-
+IMG=antrea/lichen
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BINARIES_DIR="$( cd "$1" >/dev/null 2>&1 && pwd )"
 REPORTS_DIR="$( cd "$2" >/dev/null 2>&1 && pwd )"
@@ -21,15 +17,10 @@ failed_binaries=""
 for f in "$1"/*; do
     [ -e "$f" ] || continue
     if [[ $f =~ antrea-agent || $f =~ antrea-controller || $f =~ antrea-cni || $f =~ antctl || $f =~ antrea-octant-plugin ]]; then
-        if [[ $f =~ exe ]]; then
-            # skip Windows binaries for now
-            # See https://github.com/mitchellh/golicense/issues/4
-            continue
-        fi
         base=$(basename $f)
         echo "Processing $base"
         echo "****************"
-        docker run --rm -e GITHUB_TOKEN -v $THIS_DIR:/conf -v "$BINARIES_DIR":/bins antrea/golicense /conf/conf.json /bins/$base | tee "$REPORTS_DIR/$base.deps.txt" || failed_binaries="$failed_binaries $base"
+        docker run --rm -v $THIS_DIR:/conf -v "$BINARIES_DIR":/bins $IMG --config=/conf/conf.yml /bins/$base | tee "$REPORTS_DIR/$base.deps.txt" || failed_binaries="$failed_binaries $base"
         echo "****************"
     fi
 done
