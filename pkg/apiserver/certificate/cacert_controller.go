6fad6fec55db794bb567e58c15d99055ffce80a1
abhiraut <rauta@vmware.com>
2021-02-11 18:29:56 -0800

    Fix info log msg

--- pkg/apiserver/certificate/cacert_controller.go
+++ pkg/apiserver/certificate/cacert_controller.go
@@ -167,7 +167,7 @@ func (c *CACertController) syncMutatingWebhooks(caCert []byte) error {
 		mWebhook, err := c.client.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(context.TODO(), name, metav1.GetOptions{})
 		if err != nil {
 			if errors.IsNotFound(err) {
-				klog.V(2).Infof("optional mutation webhook %s not found, skipping it's update", name)
+				klog.V(2).Infof("Optional mutation webhook %s not found, skipping its update", name)
 				continue
 			}
 			return fmt.Errorf("error getting MutatingWebhookConfiguration %s: %v", name, err)
