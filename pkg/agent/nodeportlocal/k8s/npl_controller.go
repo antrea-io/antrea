50bdb377743e568288d9786d47f71c17541e5c92
shubhamavi <shubham@avinetworks.com>
2021-03-04 02:10:29 +0530

    avoid using getter functions for k8s namespace

--- pkg/agent/nodeportlocal/k8s/npl_controller.go
+++ pkg/agent/nodeportlocal/k8s/npl_controller.go
@@ -272,7 +272,7 @@ func (c *NPLController) isNPLEnabledForServiceOfPod(obj interface{}) bool {
 		svc, isSvc := service.(*corev1.Service)
 		// Selecting Services NOT of type NodePort, with Service selector matching Pod labels.
 		if isSvc && svc.Spec.Type != corev1.ServiceTypeNodePort {
-			if pod.GetNamespace() == svc.GetNamespace() &&
+			if pod.Namespace == svc.Namespace &&
 				matchSvcSelectorPodLabels(svc.Spec.Selector, pod.GetLabels()) {
 				return true
 			}
