55994b1db4579b4a5902435c3a236bd4faed5b5c
Antonin Bas <abas@vmware.com>
2021-05-20 16:09:06 -0700

    Build antrea-cni binary and release binaries without cgo (#2189)
    antrea-cni can be installed (by the initContainer) on systems with
    incompatible system libraries. To increase portability, we build
    antrea-cni without cgo by setting CG_ENABLED to 0.
    
    Same goes for release binaries (e.g. antctl). Note that when
    cross-compiling, cgo is disabled by default, so there is no change there
    (e.g. antctl binary for macOS). It's only for native Linux amd64 builds
    that there is an actual change.
    
    Fixes #2095
    
    Signed-off-by: Antonin Bas <abas@vmware.com>

--- hack/release/prepare-assets.sh
+++ hack/release/prepare-assets.sh
@@ -54,11 +54,16 @@ for build in "${ANTREA_BUILDS[@]}"; do
     arch="${args[1]}"
     suffix="${args[2]}"
 
-    GOOS=$os GOARCH=$arch ANTCTL_BINARY_NAME="antctl-$suffix" BINDIR="$OUTPUT_DIR"/ make antctl-release
-    cd ./plugins/octant && GOOS=$os GOARCH=$arch ANTREA_OCTANT_PLUGIN_BINARY_NAME="antrea-octant-plugin-$suffix" \
+    # cgo is disabled by default when cross-compiling, but enabled by default
+    # for native builds. We ensure it is always disabled for portability since
+    # these binaries will be distributed as release assets.
+    GOOS=$os GOARCH=$arch CGO_ENABLED=0 ANTCTL_BINARY_NAME="antctl-$suffix" BINDIR="$OUTPUT_DIR"/ make antctl-release
+    cd ./plugins/octant && GOOS=$os GOARCH=$arch CGO_ENABLED=0 ANTREA_OCTANT_PLUGIN_BINARY_NAME="antrea-octant-plugin-$suffix" \
     BINDIR="$OUTPUT_DIR" make antrea-octant-plugin-release && cd ../..
 done
 
+# the windows-bin Makefile target builds antrea-cni and antrea-agent with cgo
+# explicitly disabled.
 BINDIR="$OUTPUT_DIR" make windows-bin
 sed "s/AntreaVersion=\"latest\"/AntreaVersion=\"$VERSION\"/" ./hack/windows/Start.ps1 > "$OUTPUT_DIR"/Start.ps1
 
