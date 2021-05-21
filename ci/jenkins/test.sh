71fb9382d0446f7ac2a4049cf9907107e842e5e1
Zhecheng Li <lzhecheng@vmware.com>
2021-05-21 10:05:05 +0800

    [CI] Fix IPv6 e2e kubeconfig not found & simplify Jenkins log (#2195)
    Signed-off-by: Zhecheng Li <lzhecheng@vmware.com>

--- ci/jenkins/test.sh
+++ ci/jenkins/test.sh
@@ -375,7 +375,7 @@ function run_e2e {
 
     set +e
     mkdir -p `pwd`/antrea-test-logs
-    go test -v antrea.io/antrea/test/e2e --logs-export-dir `pwd`/antrea-test-logs -timeout=100m --prometheus
+    go test -v antrea.io/antrea/test/e2e --logs-export-dir `pwd`/antrea-test-logs --provider remote -timeout=100m --prometheus
     if [[ "$?" != "0" ]]; then
         TEST_FAILURE=true
     fi
