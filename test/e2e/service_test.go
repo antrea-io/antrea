23d714391f97f307fb5bfc09c8865f34bce75253
Zhecheng Li <lzhecheng@vmware.com>
2021-06-24 10:38:37 +0800

    [CI] Fix Windows conformance (#2286)
    * Pull necessary images for Windows Conformance tests
    * Added one e2e test to test NodePort Service on Windows to replace the
    one from Kubernetes conformance
    
    Signed-off-by: Zhecheng Li <lzhecheng@vmware.com>

--- test/e2e/service_test.go
+++ test/e2e/service_test.go
@@ -16,6 +16,7 @@ package e2e
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -72,6 +73,71 @@ func (data *TestData) createClusterIPServiceAndBackendPods(t *testing.T, name st
 	require.NoError(t, err)
 	require.NoError(t, data.podWaitForRunning(defaultTimeout, name, testNamespace))
 	svc, err := data.createNginxClusterIPService(name, false, &ipv4Protocol)
+	require.NoError(t, err)
+
+	cleanup := func() {
+		data.deletePodAndWait(defaultTimeout, name)
+		data.deleteServiceAndWait(defaultTimeout, name)
+	}
+
+	return svc, cleanup
+}
+
+// TestNodePortWindows tests NodePort Service on Windows Node. It is a temporary test to replace upstream Kubernetes one:
+// https://github.com/kubernetes/kubernetes/blob/ea0764452222146c47ec826977f49d7001b0ea8c/test/e2e/windows/service.go#L42
+// Issue: https://github.com/antrea-io/antrea/issues/2289
+func TestNodePortWindows(t *testing.T) {
+	skipIfNoWindowsNodes(t)
+
+	data, err := setupTest(t)
+	if err != nil {
+		t.Fatalf("Error when setting up test: %v", err)
+	}
+	defer teardownTest(t, data)
+
+	svcName := "agnhost"
+	svcNode := nodeName(clusterInfo.windowsNodes[0])
+	svc, cleanup := data.createAgnhostServiceAndBackendPods(t, svcName, svcNode, corev1.ServiceTypeNodePort)
+	defer cleanup()
+	t.Logf("%s Service is ready", svcName)
+
+	// Unlike upstream Kubernetes Conformance, here the client is on a Linux Node (nodeName(0)).
+	// It doesn't need to be the control-plane for e2e test and other Linux workers will work as well. However, in this
+	// e2e framework, nodeName(0)/Control-plane Node is guaranteed to be a Linux one.
+	clientName := "agnhost-client"
+	require.NoError(t, data.createAgnhostPodOnNode(clientName, nodeName(0)))
+	defer data.deletePodAndWait(defaultTimeout, clientName)
+	_, err = data.podWaitForIPs(defaultTimeout, clientName, testNamespace)
+	require.NoError(t, err)
+
+	nodeIP := clusterInfo.nodes[0].ip
+	nodePort := int(svc.Spec.Ports[0].NodePort)
+	addr := fmt.Sprintf("http://%s:%d", nodeIP, nodePort)
+
+	cmd := append([]string{"curl", "--connect-timeout", "1", "--retry", "5", "--retry-connrefused"}, addr)
+	stdout, stderr, err := data.runCommandFromPod(testNamespace, clientName, agnhostContainerName, cmd)
+	if err != nil {
+		t.Errorf("Error when running command '%s' from Pod '%s', stdout: %s, stderr: %s, error: %v",
+			strings.Join(cmd, " "), clientName, stdout, stderr, err)
+	} else {
+		t.Logf("curl from Pod '%s' to '%s' succeeded", clientName, addr)
+	}
+}
+
+func (data *TestData) createAgnhostServiceAndBackendPods(t *testing.T, name string, node string, svcType corev1.ServiceType) (*corev1.Service, func()) {
+	ipv4Protocol := corev1.IPv4Protocol
+	args := []string{"netexec", "--http-port=80", "--udp-port=80"}
+	require.NoError(t, data.createPodOnNode(name, node, agnhostImage, []string{}, args, nil, []corev1.ContainerPort{
+		{
+			Name:          "http",
+			ContainerPort: 80,
+			Protocol:      corev1.ProtocolTCP,
+		},
+	}, false, nil))
+	_, err := data.podWaitForIPs(defaultTimeout, name, testNamespace)
+	require.NoError(t, err)
+	require.NoError(t, data.podWaitForRunning(defaultTimeout, name, testNamespace))
+	svc, err := data.createService(name, 80, 80, map[string]string{"app": "agnhost"}, false, svcType, &ipv4Protocol)
 	require.NoError(t, err)
 
 	cleanup := func() {
