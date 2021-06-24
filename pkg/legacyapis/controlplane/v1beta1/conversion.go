d1921508065cebae73996799a393a54980522d37
Quan Tian <qtian@vmware.com>
2021-06-24 16:28:08 +0800

    Fix wrong types and missing field selectors in legacy APIs
    1. v1beta1.controlplane.antrea.tanzu.vmware.com used v1beta2 types by
    mistake.
    
    2. Field selectors for v1beta1.controlplane.antrea.tanzu.vmware.com
    was not registered, which broke "nodeName" fieldSelector.
    
    Signed-off-by: Quan Tian <qtian@vmware.com>

---
+++ pkg/legacyapis/controlplane/v1beta1/conversion.go
@@ -0,0 +1,46 @@
+// Copyright 2021 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1beta1
+
+import (
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+func init() {
+	localSchemeBuilder.Register(addConversionFuncs)
+}
+
+// addConversionFuncs adds non-generated conversion functions to the given scheme.
+func addConversionFuncs(scheme *runtime.Scheme) error {
+	for _, kind := range []string{"AppliedToGroup", "AddressGroup", "NetworkPolicy"} {
+		err := scheme.AddFieldLabelConversionFunc(SchemeGroupVersion.WithKind(kind),
+			func(label, value string) (string, string, error) {
+				switch label {
+				// Antrea Agents select resources by nodeName.
+				case "metadata.name", "nodeName":
+					return label, value, nil
+				default:
+					return "", "", fmt.Errorf("field label not supported: %s", label)
+				}
+			},
+		)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
