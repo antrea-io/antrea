677748f8dab0f127eb6e1496de592cd1edafede2
Antonin Bas <abas@vmware.com>
2021-07-12 15:04:20 -0700

    NodePortLocal: make sure port is available and bind to it to reserve it
    Fix the NPL controller so that only available ports are selected and
    allocated to NPL-enabled Pods. When a port is allocated, we create a
    listening socket to prevent other processes from using it. This is
    similar to how K8s handles NodePorts.
    
    Fixes #2381
    
    Signed-off-by: Antonin Bas <abas@vmware.com>

--- pkg/agent/nodeportlocal/portcache/port_table.go
+++ pkg/agent/nodeportlocal/portcache/port_table.go
@@ -18,7 +18,10 @@ package portcache
 
 import (
 	"fmt"
+	"net"
 	"sync"
+
+	"k8s.io/klog/v2"
 
 	"antrea.io/antrea/pkg/agent/nodeportlocal/rules"
 )
@@ -27,66 +30,39 @@ type NodePortData struct {
 	NodePort int
 	PodPort  int
 	PodIP    string
-	Status   int
+	socket   Closeable
 }
+
+type LocalPortOpener interface {
+	OpenLocalPort(port int) (Closeable, error)
+}
+
+type localPortOpener struct{}
 
 type PortTable struct {
-	Table        map[int]NodePortData
-	StartPort    int
-	EndPort      int
-	PodPortRules rules.PodPortRules
-	tableLock    sync.RWMutex
+	Table           map[int]NodePortData
+	StartPort       int
+	EndPort         int
+	PodPortRules    rules.PodPortRules
+	LocalPortOpener LocalPortOpener
+	tableLock       sync.RWMutex
 }
 
-func NewPortTable(start, end int) (*PortTable, bool) {
-	var ok bool
+func NewPortTable(start, end int) (*PortTable, error) {
 	ptable := PortTable{StartPort: start, EndPort: end}
 	ptable.Table = make(map[int]NodePortData)
 	ptable.PodPortRules = rules.InitRules()
-
-	if ptable.PodPortRules != nil {
-		ok = true
+	ptable.LocalPortOpener = &localPortOpener{}
+	if err := ptable.PodPortRules.Init(); err != nil {
+		return nil, err
 	}
-	return &ptable, ok
+	return &ptable, nil
 }
 
 func (pt *PortTable) CleanupAllEntries() {
 	pt.tableLock.Lock()
 	defer pt.tableLock.Unlock()
 	pt.Table = make(map[int]NodePortData)
-}
-
-func (pt *PortTable) AddUpdateEntry(nodeport, podport int, podip string) {
-	pt.tableLock.Lock()
-	defer pt.tableLock.Unlock()
-	data := NodePortData{NodePort: nodeport, PodPort: podport, PodIP: podip}
-	pt.Table[nodeport] = data
-}
-
-func (pt *PortTable) DeleteEntry(nodeport int) {
-	pt.tableLock.Lock()
-	defer pt.tableLock.Unlock()
-	delete(pt.Table, nodeport)
-}
-
-func (pt *PortTable) DeleteEntryByPodIP(ip string) {
-	pt.tableLock.Lock()
-	defer pt.tableLock.Unlock()
-	for i, data := range pt.Table {
-		if data.PodIP == ip {
-			delete(pt.Table, i)
-		}
-	}
-}
-
-func (pt *PortTable) DeleteEntryByPodIPPort(ip string, port int) {
-	pt.tableLock.Lock()
-	defer pt.tableLock.Unlock()
-	for i, data := range pt.Table {
-		if data.PodIP == ip && data.PodPort == port {
-			delete(pt.Table, i)
-		}
-	}
 }
 
 func (pt *PortTable) GetEntry(nodeport int) *NodePortData {
@@ -111,6 +87,10 @@ func (pt *PortTable) GetDataForPodIP(ip string) []NodePortData {
 func (pt *PortTable) GetEntryByPodIPPort(ip string, port int) *NodePortData {
 	pt.tableLock.RLock()
 	defer pt.tableLock.RUnlock()
+	return pt.getEntryByPodIPPort(ip, port)
+}
+
+func (pt *PortTable) getEntryByPodIPPort(ip string, port int) *NodePortData {
 	for _, data := range pt.Table {
 		if data.PodIP == ip && data.PodPort == port {
 			return &data
@@ -120,11 +100,18 @@ func (pt *PortTable) GetEntryByPodIPPort(ip string, port int) *NodePortData {
 }
 
 func (pt *PortTable) getFreePort(podIP string, podPort int) int {
-	pt.tableLock.Lock()
-	defer pt.tableLock.Unlock()
 	for i := pt.StartPort; i <= pt.EndPort; i++ {
 		if _, ok := pt.Table[i]; !ok {
-			pt.Table[i] = NodePortData{PodIP: podIP, PodPort: podPort}
+			socket, err := pt.LocalPortOpener.OpenLocalPort(i)
+			if err != nil {
+				continue
+			}
+			pt.Table[i] = NodePortData{
+				NodePort: i,
+				PodIP:    podIP,
+				PodPort:  podPort,
+				socket:   socket,
+			}
 			return i
 		}
 	}
@@ -132,25 +119,30 @@ func (pt *PortTable) getFreePort(podIP string, podPort int) int {
 }
 
 func (pt *PortTable) AddRule(podIP string, podPort int) (int, error) {
-	nodeport := pt.getFreePort(podIP, podPort)
-	if nodeport < 0 {
+	pt.tableLock.Lock()
+	defer pt.tableLock.Unlock()
+	nodePort := pt.getFreePort(podIP, podPort)
+	if nodePort < 0 {
 		return 0, fmt.Errorf("no free port found")
 	}
-	err := pt.PodPortRules.AddRule(nodeport, fmt.Sprintf("%s:%d", podIP, podPort))
+	err := pt.PodPortRules.AddRule(nodePort, fmt.Sprintf("%s:%d", podIP, podPort))
 	if err != nil {
 		return 0, err
 	}
-	pt.AddUpdateEntry(nodeport, podPort, podIP)
-	return nodeport, nil
+	return nodePort, nil
 }
 
 func (pt *PortTable) DeleteRule(podIP string, podPort int) error {
-	data := pt.GetEntryByPodIPPort(podIP, podPort)
-	err := pt.PodPortRules.DeleteRule(data.NodePort, fmt.Sprintf("%s:%d", podIP, podPort))
-	if err != nil {
+	pt.tableLock.Lock()
+	defer pt.tableLock.Unlock()
+	data := pt.getEntryByPodIPPort(podIP, podPort)
+	if err := pt.PodPortRules.DeleteRule(data.NodePort, fmt.Sprintf("%s:%d", podIP, podPort)); err != nil {
 		return err
 	}
-	pt.DeleteEntry(data.NodePort)
+	if err := data.socket.Close(); err != nil {
+		return fmt.Errorf("Error when releasing local port %d: %v", data.NodePort, err)
+	}
+	delete(pt.Table, data.NodePort)
 	return nil
 }
 
@@ -160,4 +152,43 @@ func (pt *PortTable) RuleExists(podIP string, podPort int) bool {
 		return true
 	}
 	return false
+}
+
+func (pt *PortTable) SyncRules(allNPLPorts []rules.PodNodePort) error {
+	validNPLPorts := make([]rules.PodNodePort, 0, len(allNPLPorts))
+	pt.tableLock.Lock()
+	defer pt.tableLock.Unlock()
+	for _, nplPort := range allNPLPorts {
+		socket, err := pt.LocalPortOpener.OpenLocalPort(nplPort.NodePort)
+		if err != nil {
+			// This should be handled gracefully by the NPL controller: if there is an
+			// annotation using this port, it will be removed and replaced with a new
+			// one with a valid port mapping.
+			klog.ErrorS(err, "Cannot bind to local port, skipping it", "port", nplPort.NodePort)
+			continue
+		}
+		data := NodePortData{
+			NodePort: nplPort.NodePort,
+			PodPort:  nplPort.PodPort,
+			PodIP:    nplPort.PodIP,
+			socket:   socket,
+		}
+		pt.Table[nplPort.NodePort] = data
+		validNPLPorts = append(validNPLPorts, nplPort)
+	}
+	return pt.PodPortRules.AddAllRules(validNPLPorts)
+}
+
+// openLocalPort binds to the provided port.
+// This is inspired by the openLocalPort function in kube-proxy:
+// https://github.com/kubernetes/kubernetes/blob/86f8c3ee91b6faec437f97e3991107747d7fc5e8/pkg/proxy/iptables/proxier.go#L1664
+func (lpo *localPortOpener) OpenLocalPort(port int) (Closeable, error) {
+	// For now, NodePortLocal only supports IPv4 and TCP.
+	network := "tcp4"
+	listener, err := net.Listen(network, fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	klog.V(2).InfoS("Opened local port", "port", port)
+	return listener, nil
 }
