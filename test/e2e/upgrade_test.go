d74d2a2547795a92722ed6b4dafa29f759235f9e
Wenying Dong <wenyingd@vmware.com>
2021-07-14 20:49:34 +0800

    [Windows] Improve e2e test case DeletePod to support Windows (#2337)
    1. Check if resources are removed from the  Windows Node or not when a
       Pod is deleted, including IPAM allocation, OVS port, and HNSEndpoint.
    2. Delever antctl-windows.exe in antrea-windows image, and copy it to
       path "C:\k\antrea\bin\antctl.exe" when deploying antrea-agent on
       Windows Node.
    
    Signed-off-by: wenyingd <wenyingd@vmware.com>

--- test/e2e/upgrade_test.go
+++ test/e2e/upgrade_test.go
@@ -119,5 +119,5 @@ func TestUpgrade(t *testing.T) {
 		t.Errorf("Namespace deletion failed: %v", err)
 	}
 
-	data.testDeletePod(t, podName, nodeName)
+	data.testDeletePod(t, podName, nodeName, false)
 }
