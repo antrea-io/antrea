155078688d1a2140eecbcac012afbc0b5a0ab128
Qiyue Yao <39061776+qiyueyao@users.noreply.github.com>
2021-08-20 11:17:31 -0700

    [Follow Up] Antrea Network Policy Audit Logging Deduplication and Unit Tests (#2578)
    This is a follow up PR for [Antrea Network Policy Audit Logging Deduplication and Unit Tests](https://github.com/antrea-io/antrea/pull/2294) to address comments.
    
    - Converted back to include both `antreaPolicyEnabled` and `loggingEnabled` for extensible usage.
    - Changed `AntreaPolicyLogger` method naming.
    - Moved `bufferLength` to a member of `AntreaPolicyLogger` to avoid confusion.
    
    Signed-off-by: Qiyue Yao <yaoq@vmware.com>

--- pkg/agent/controller/networkpolicy/networkpolicy_controller_test.go
+++ pkg/agent/controller/networkpolicy/networkpolicy_controller_test.go
@@ -53,7 +53,7 @@ func newTestController() (*Controller, *fake.Clientset, *mockReconciler) {
 	clientset := &fake.Clientset{}
 	ch := make(chan agenttypes.EntityReference, 100)
 	controller, _ := NewNetworkPolicyController(&antreaClientGetter{clientset}, nil, nil, "node1", ch,
-		true, true, nil, testAsyncDeleteInterval)
+		true, true, true, nil, testAsyncDeleteInterval)
 	reconciler := newMockReconciler()
 	controller.reconciler = reconciler
 	controller.antreaPolicyLogger = nil
