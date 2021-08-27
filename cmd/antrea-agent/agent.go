0f2929733823b02fdeffbc0fd586bfacb92e1dbb
Wu zhengdong <zhengdong.wu@transwarp.io>
2021-08-27 20:43:12 +0800

    Support configurable network interface CIDRs for Pod traffic across Node (#2704)
    Antrea Agent uses the configurable CIDRs for Pod traffic. The IP address
    used for tunneling or routing traffic to remote Nodes is decided in the
    following order of preference (from highest to lowest):
    1. TransportInterface
    2. TransportInterfaceCIDRs
    3. The Node Internal IP or External IP
    
    Signed-off-by: Wu zhengdong <zhengdong.wu@transwarp.io>

--- cmd/antrea-agent/agent.go
+++ cmd/antrea-agent/agent.go
@@ -128,6 +128,7 @@ func run(o *Options) error {
 		TrafficEncapMode:      encapMode,
 		TrafficEncryptionMode: encryptionMode,
 		TransportIface:        o.config.TransportInterface,
+		TransportIfaceCIDRs:   o.config.TransportInterfaceCIDRs,
 	}
 
 	wireguardConfig := &config.WireGuardConfig{
