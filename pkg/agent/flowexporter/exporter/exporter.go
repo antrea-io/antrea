cf72b326ff3dc604c69dfbf3374f868f551f4047
Yanjun Zhou <zhouya@vmware.com>
2021-10-19 17:30:38 -0700

    Upgrade to go-ipfix v0.5.9
    Signed-off-by: Yanjun Zhou <zhouya@vmware.com>

--- pkg/agent/flowexporter/exporter/exporter.go
+++ pkg/agent/flowexporter/exporter/exporter.go
@@ -138,7 +138,6 @@ func prepareExporterInputArgs(collectorAddr, collectorProto, nodeName string) ex
 		expInput.IsEncrypted = false
 		expInput.CollectorProtocol = collectorProto
 	}
-	expInput.PathMTU = 0
 
 	return expInput
 }
