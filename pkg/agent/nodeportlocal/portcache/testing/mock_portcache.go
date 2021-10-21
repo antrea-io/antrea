6c2687bfd4afd8c1729ba5860daf1f53040ded50
Antonin Bas <abas@vmware.com>
2021-10-21 01:18:22 -0700

    Improve multi-protocol support for NPL (#2903)
    There is an edge case, which is not handled well by the current NPL
    controller implementation: if a given Pod port needs to be exposed for
    both TCP and UDP, and the first available Node port is only available
    for TCP, it will be still be selected and NPL rule installation will
    succeed for TCP but not for UDP.
    
    We have 2 options:
    
    1. reserve the port for both protocols ahead of time, even if the port
    is only needed for one protocol initially.
    2. support using different Node ports for different protocols, even when
    the Pod port is the same.
    
    In this patch, we go with option 1) to preserve the "nice" property that
    a give Pod port maps to a unique Node port.
    
    Fixes #2894
    
    Signed-off-by: Antonin Bas <abas@vmware.com>

--- pkg/agent/nodeportlocal/portcache/testing/mock_portcache.go
+++ pkg/agent/nodeportlocal/portcache/testing/mock_portcache.go
@@ -20,8 +20,8 @@
 package testing
 
 import (
-	portcache "antrea.io/antrea/pkg/agent/nodeportlocal/portcache"
 	gomock "github.com/golang/mock/gomock"
+	io "io"
 	reflect "reflect"
 )
 
@@ -49,10 +49,10 @@ func (m *MockLocalPortOpener) EXPECT() *MockLocalPortOpenerMockRecorder {
 }
 
 // OpenLocalPort mocks base method
-func (m *MockLocalPortOpener) OpenLocalPort(arg0 int, arg1 string) (portcache.Closeable, error) {
+func (m *MockLocalPortOpener) OpenLocalPort(arg0 int, arg1 string) (io.Closer, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "OpenLocalPort", arg0, arg1)
-	ret0, _ := ret[0].(portcache.Closeable)
+	ret0, _ := ret[0].(io.Closer)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
