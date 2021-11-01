antrea-io/antrea

commit c3aa14c5bf5bf8a79737567e6fa1eb0ba140897b

Support reserving addresses in CIDR allocator (#2942)
* Support reserving addresses in CIDR allocator

Gateway and broadcast addresses should not be allocated.
This change allows the user to configure such addresses when
creating allocator. Before this change, broadcast was avoided
in allocator by shrinking range - now, broadcast may be configured
as reserved externally, along with other reserved addresses.
Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

* Fix allocation efficiency for full pool

Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

* Change argument types for NewAllocator functions

The arguments change from string to parsed struct.

Signed-off-by: Anna Khmelnitsky <akhmelnitsky@vmware.com>

pkg/ipam/ipallocator/allocator.go
@@ -49,24 +49,18 @@ type SingleIPAllocator struct {
 	allocated *big.Int
 	// count is the number of currently allocated elements in the range.
 	count int
+	// IPs inside the cidr not available for allocation
+	reservedIPs []net.IP
 }
 
 // NewCIDRAllocator creates an IPAllocator based on the provided CIDR.
-func NewCIDRAllocator(cidr string) (*SingleIPAllocator, error) {
-	ip, ipNet, err := net.ParseCIDR(cidr)
-	if err != nil {
-		return nil, err
-	}
-	base := utilnet.BigForIP(ip)
+func NewCIDRAllocator(cidr *net.IPNet, reservedIPs []net.IP) (*SingleIPAllocator, error) {
+	base := utilnet.BigForIP(cidr.IP)
 	// Start from "x.x.x.1".
 	base.Add(base, big.NewInt(1))
-	max := utilnet.RangeSize(ipNet) - 1
-	if ip.To4() != nil {
-		// Don't use the IPv4 network's broadcast address.
-		max--
-	}
+	max := utilnet.RangeSize(cidr) - 1
 	if max < 0 {
-		return nil, fmt.Errorf("no available IP in %s", cidr)
+		return nil, fmt.Errorf("no available IP in %s", cidr.String())
 	}
 	// In case a big range occupies too much memory, allow at most 65536 IP for each IP range.
 	if max > 65536 {
@@ -74,29 +68,22 @@ func NewCIDRAllocator(cidr string) (*SingleIPAllocator, error) {
 	}
 
 	allocator := &SingleIPAllocator{
-		ipRangeStr: cidr,
-		base:       base,
-		max:        int(max),
-		allocated:  big.NewInt(0),
-		count:      0,
+		ipRangeStr:  cidr.String(),
+		base:        base,
+		max:         int(max),
+		allocated:   big.NewInt(0),
+		count:       0,
+		reservedIPs: reservedIPs,
 	}
 	return allocator, nil
 }
 
 // NewIPRangeAllocator creates an IPAllocator based on the provided start IP and end IP.
 // The start IP and end IP are inclusive.
-func NewIPRangeAllocator(startIP, endIP string) (*SingleIPAllocator, error) {
-	ipRangeStr := fmt.Sprintf("%s-%s", startIP, endIP)
-	ip := net.ParseIP(startIP)
-	if ip == nil {
-		return nil, fmt.Errorf("invalid start IP %s", startIP)
-	}
-	base := utilnet.BigForIP(ip)
-	ip = net.ParseIP(endIP)
-	if ip == nil {
-		return nil, fmt.Errorf("invalid end IP %s", endIP)
-	}
-	offset := big.NewInt(0).Sub(utilnet.BigForIP(ip), base).Int64()
+func NewIPRangeAllocator(startIP, endIP net.IP) (*SingleIPAllocator, error) {
+	ipRangeStr := fmt.Sprintf("%s-%s", startIP.String(), endIP.String())
+	base := utilnet.BigForIP(startIP)
+	offset := big.NewInt(0).Sub(utilnet.BigForIP(endIP), base).Int64()
 	if offset < 0 {
 		return nil, fmt.Errorf("invalid IP range %s", ipRangeStr)
 	}
@@ -120,11 +107,25 @@ func (a *SingleIPAllocator) Name() string {
 	return a.ipRangeStr
 }
 
+func (a *SingleIPAllocator) checkReserved(ip net.IP) error {
+	for _, reservedIP := range a.reservedIPs {
+		if reservedIP.Equal(ip) {
+			return fmt.Errorf("IP %v is reserved and not available for allocation", ip)
+		}
+	}
+	return nil
+}
+
 // AllocateIP allocates the specified IP. It returns error if the IP is not in the range or already allocated.
 func (a *SingleIPAllocator) AllocateIP(ip net.IP) error {
 	offset := int(big.NewInt(0).Sub(utilnet.BigForIP(ip), a.base).Int64())
 	if offset < 0 || offset >= a.max {
 		return fmt.Errorf("IP %v is not in the ipset", ip)
+	}
+
+	err := a.checkReserved(ip)
+	if err != nil {
+		return err
 	}
 
 	a.mutex.Lock()
@@ -141,14 +142,17 @@ func (a *SingleIPAllocator) AllocateIP(ip net.IP) error {
 func (a *SingleIPAllocator) AllocateNext() (net.IP, error) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
-	if a.count >= a.max {
+	if a.count >= (a.max - len(a.reservedIPs)) {
 		return nil, fmt.Errorf("no available IP")
 	}
 	for i := 0; i < a.max; i++ {
 		if a.allocated.Bit(i) == 0 {
+			ip := utilnet.AddIPOffset(a.base, i)
+			if a.checkReserved(ip) != nil {
+				continue
+			}
 			a.allocated.SetBit(a.allocated, i, 1)
 			a.count++
-			ip := utilnet.AddIPOffset(a.base, i)
 			return ip, nil
 		}
 	}

pkg/ipam/poolallocator/allocator.go
@@ -22,6 +22,7 @@ import (
 	"antrea.io/antrea/pkg/apis/crd/v1alpha2"
 	crdclientset "antrea.io/antrea/pkg/client/clientset/versioned"
 	"antrea.io/antrea/pkg/ipam/ipallocator"
+	iputil "antrea.io/antrea/pkg/util/ip"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/retry"
@@ -57,13 +58,27 @@ func (a *IPPoolAllocator) initIPAllocators(ipPool *v1alpha2.IPPool) (ipallocator
 	// Initialize a list of IP allocators based on pool spec
 	for _, ipRange := range ipPool.Spec.IPRanges {
 		if len(ipRange.CIDR) > 0 {
-			allocator, err := ipallocator.NewCIDRAllocator(ipRange.CIDR)
+			// Reserve gateway address and broadcast address
+			reservedIPs := []net.IP{net.ParseIP(ipRange.SubnetInfo.Gateway)}
+			_, ipNet, err := net.ParseCIDR(ipRange.CIDR)
+			if err != nil {
+				return nil, err
+			}
+
+			size, bits := ipNet.Mask.Size()
+			if int32(size) == ipRange.SubnetInfo.PrefixLength && bits == 32 {
+				// Allocation CIDR covers entire subnet, thus we need
+				// to reserve broadcast IP as well for IPv4
+				reservedIPs = append(reservedIPs, iputil.GetLocalBroadcastIP(ipNet))
+			}
+
+			allocator, err := ipallocator.NewCIDRAllocator(ipNet, reservedIPs)
 			if err != nil {
 				return nil, err
 			}
 			allocators = append(allocators, allocator)
 		} else {
-			allocator, err := ipallocator.NewIPRangeAllocator(ipRange.Start, ipRange.End)
+			allocator, err := ipallocator.NewIPRangeAllocator(net.ParseIP(ipRange.Start), net.ParseIP(ipRange.End))
 			if err != nil {
 				return allocators, err
 			}

pkg/agent/route/route_windows.go
@@ -30,6 +30,7 @@ import (
 	"antrea.io/antrea/pkg/agent/util"
 	"antrea.io/antrea/pkg/agent/util/winfirewall"
 	binding "antrea.io/antrea/pkg/ovs/openflow"
+	iputil "antrea.io/antrea/pkg/util/ip"
 )
 
 const (
@@ -216,7 +217,7 @@ func (c *Client) listRoutes() (map[string]*util.Route, error) {
 		}
 		// Windows adds an active route entry for the local broadcast address automatically when a new IP address
 		// is configured on the interface. This route entry should be ignored in the list.
-		if rt.DestinationSubnet.IP.Equal(util.GetLocalBroadcastIP(rt.DestinationSubnet)) {
+		if rt.DestinationSubnet.IP.Equal(iputil.GetLocalBroadcastIP(rt.DestinationSubnet)) {
 			continue
 		}
 		// Skip Service corresponding routes. These route entries are added by kube-proxy. If these route entries

pkg/agent/util/net_windows.go
@@ -19,7 +19,6 @@ package util
 
 import (
 	"bufio"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -427,15 +426,6 @@ func enableHNSOnOVS(hnsNet *hcsshim.HNSNetwork) error {
 		return err
 	}
 	return err
-}
-
-// GetLocalBroadcastIP returns the last IP address in a subnet. This IP is always working as the broadcast address in
-// the subnet on Windows, and an active route entry that uses it as the destination is added by default when a new IP is
-// configured on the interface.
-func GetLocalBroadcastIP(ipNet *net.IPNet) net.IP {
-	lastAddr := make(net.IP, len(ipNet.IP.To4()))
-	binary.BigEndian.PutUint32(lastAddr, binary.BigEndian.Uint32(ipNet.IP.To4())|^binary.BigEndian.Uint32(net.IP(ipNet.Mask).To4()))
-	return lastAddr
 }
 
 // GetDefaultGatewayByInterfaceIndex returns the default gateway configured on the specified interface.

pkg/controller/egress/controller.go
@@ -45,6 +45,7 @@ import (
 	"antrea.io/antrea/pkg/controller/metrics"
 	antreatypes "antrea.io/antrea/pkg/controller/types"
 	"antrea.io/antrea/pkg/ipam/ipallocator"
+	iputil "antrea.io/antrea/pkg/util/ip"
 )
 
 const (
@@ -239,9 +240,16 @@ func (c *EgressController) createOrUpdateIPAllocator(ipPool *egressv1alpha2.Exte
 		var ipAllocator *ipallocator.SingleIPAllocator
 		var err error
 		if ipRange.CIDR != "" {
-			ipAllocator, err = ipallocator.NewCIDRAllocator(ipRange.CIDR)
+			_, ipNet, parseErr := net.ParseCIDR(ipRange.CIDR)
+			if parseErr != nil {
+				klog.ErrorS(err, "Failed to create IPAllocator", "ipRange", ipRange)
+				continue
+			}
+			// exclude broadcast address from allocation
+			reservedIPs := []net.IP{iputil.GetLocalBroadcastIP(ipNet)}
+			ipAllocator, err = ipallocator.NewCIDRAllocator(ipNet, reservedIPs)
 		} else {
-			ipAllocator, err = ipallocator.NewIPRangeAllocator(ipRange.Start, ipRange.End)
+			ipAllocator, err = ipallocator.NewIPRangeAllocator(net.ParseIP(ipRange.Start), net.ParseIP(ipRange.End))
 		}
 		if err != nil {
 			klog.ErrorS(err, "Failed to create IPAllocator", "ipRange", ipRange)

pkg/ipam/ipallocator/allocator_test.go
@@ -22,13 +22,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func newCIDRAllocator(cidr string) *SingleIPAllocator {
-	allocator, _ := NewCIDRAllocator(cidr)
+func newCIDRAllocator(cidr string, reservedIPs []string) *SingleIPAllocator {
+	_, ipNet, _ := net.ParseCIDR(cidr)
+	var parsedIPs []net.IP
+	for _, ip := range reservedIPs {
+		parsedIPs = append(parsedIPs, net.ParseIP(ip))
+	}
+	allocator, _ := NewCIDRAllocator(ipNet, parsedIPs)
 	return allocator
 }
 
 func newIPRangeAllocator(start, end string) *SingleIPAllocator {
-	allocator, _ := NewIPRangeAllocator(start, end)
+	allocator, _ := NewIPRangeAllocator(net.ParseIP(start), net.ParseIP(end))
 	return allocator
 }
 
@@ -43,17 +48,17 @@ func TestAllocateNext(t *testing.T) {
 	}{
 		{
 			name:        "IPv4-CIDR-prefix-24",
-			ipAllocator: newCIDRAllocator("10.10.10.0/24"),
+			ipAllocator: newCIDRAllocator("10.10.10.0/24", []string{"10.10.10.255"}),
 			wantNum:     254,
 			wantFirst:   net.ParseIP("10.10.10.1"),
 			wantLast:    net.ParseIP("10.10.10.254"),
 		},
 		{
 			name:        "IPv4-CIDR-prefix-30",
-			ipAllocator: newCIDRAllocator("10.10.10.128/30"),
-			wantNum:     2,
+			ipAllocator: newCIDRAllocator("10.10.10.128/30", nil),
+			wantNum:     3,
 			wantFirst:   net.ParseIP("10.10.10.129"),
-			wantLast:    net.ParseIP("10.10.10.130"),
+			wantLast:    net.ParseIP("10.10.10.131"),
 		},
 		{
 			name:        "IPv4-range",
@@ -64,7 +69,7 @@ func TestAllocateNext(t *testing.T) {
 		},
 		{
 			name:        "IPv4-multiple",
-			ipAllocator: MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30")},
+			ipAllocator: MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30", []string{"10.10.10.131"})},
 			wantNum:     13,
 			wantFirst:   net.ParseIP("1.1.1.10"),
 			wantLast:    net.ParseIP("10.10.10.130"),
@@ -100,7 +105,7 @@ func TestAllocateIP(t *testing.T) {
 	}{
 		{
 			name:         "IPv4-duplicate",
-			ipAllocator:  newCIDRAllocator("10.10.10.0/24"),
+			ipAllocator:  newCIDRAllocator("10.10.10.0/24", nil),
 			allocatedIP1: net.ParseIP("10.10.10.1"),
 			allocatedIP2: net.ParseIP("10.10.10.1"),
 			wantErr1:     false,
@@ -108,17 +113,25 @@ func TestAllocateIP(t *testing.T) {
 		},
 		{
 			name:         "IPv4-no-duplicate",
-			ipAllocator:  MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30")},
+			ipAllocator:  MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30", nil)},
 			allocatedIP1: net.ParseIP("1.1.1.10"),
-			allocatedIP2: net.ParseIP("10.10.10.129"),
+			allocatedIP2: net.ParseIP("10.10.10.130"),
 			wantErr1:     false,
 			wantErr2:     false,
 		},
 		{
 			name:         "IPv4-out-of-scope",
-			ipAllocator:  MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30")},
+			ipAllocator:  MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30", []string{"10.10.10.128"})},
 			allocatedIP1: net.ParseIP("1.1.1.21"),
 			allocatedIP2: net.ParseIP("10.10.10.127"),
+			wantErr1:     true,
+			wantErr2:     true,
+		},
+		{
+			name:         "IPv4-reserved",
+			ipAllocator:  MultiIPAllocator{newCIDRAllocator("10.10.10.128/30", []string{"10.10.10.1", "10.10.10.5"})},
+			allocatedIP1: net.ParseIP("10.10.10.1"),
+			allocatedIP2: net.ParseIP("10.10.10.5"),
 			wantErr1:     true,
 			wantErr2:     true,
 		},
@@ -148,11 +161,11 @@ func TestAllocateRelease(t *testing.T) {
 	}{
 		{
 			name:        "IPv4-single",
-			ipAllocator: newCIDRAllocator("10.10.10.0/24"),
+			ipAllocator: newCIDRAllocator("10.10.10.0/24", []string{"10.10.10.1", "10.10.10.255"}),
 		},
 		{
 			name:        "IPv4-multiple",
-			ipAllocator: MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30")},
+			ipAllocator: MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30", nil)},
 		},
 	}
 	for _, tt := range tests {
@@ -184,13 +197,13 @@ func TestHas(t *testing.T) {
 	}{
 		{
 			name:        "IPv4-single",
-			ipAllocator: newCIDRAllocator("10.10.10.0/24"),
+			ipAllocator: newCIDRAllocator("10.10.10.0/24", nil),
 			ip:          net.ParseIP("10.10.10.0"),
 			expectedHas: false,
 		},
 		{
 			name:        "IPv4-multiple",
-			ipAllocator: MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30")},
+			ipAllocator: MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30", nil)},
 			ip:          net.ParseIP("10.10.10.130"),
 			expectedHas: true,
 		},
@@ -203,6 +216,6 @@ func TestHas(t *testing.T) {
 }
 
 func TestName(t *testing.T) {
-	ma := MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30")}
+	ma := MultiIPAllocator{newIPRangeAllocator("1.1.1.10", "1.1.1.20"), newCIDRAllocator("10.10.10.128/30", nil)}
 	assert.Equal(t, []string{"1.1.1.10-1.1.1.20", "10.10.10.128/30"}, ma.Names())
 }

pkg/ipam/poolallocator/allocator_test.go
@@ -192,7 +192,7 @@ func TestAllocateNextMultiRange(t *testing.T) {
 	allocator := newIPPoolAllocator(poolName, []runtime.Object{&pool})
 
 	// Allocate the 2 available IPs from first range then switch to second range
-	validateAllocationSequence(t, allocator, subnetInfo, []string{"10.2.2.100", "10.2.2.101", "10.2.2.1", "10.2.2.2"})
+	validateAllocationSequence(t, allocator, subnetInfo, []string{"10.2.2.100", "10.2.2.101", "10.2.2.2", "10.2.2.3"})
 }
 
 func TestAllocateNextMultiRangeExausted(t *testing.T) {
@@ -255,7 +255,7 @@ func TestAllocateReleaseSequence(t *testing.T) {
 	allocator := newIPPoolAllocator(poolName, []runtime.Object{&pool})
 
 	// Allocate the single available IPs from first range then 3 IPs from second range
-	validateAllocationSequence(t, allocator, subnetInfo, []string{"2001::1000", "2001::1", "2001::2", "2001::3"})
+	validateAllocationSequence(t, allocator, subnetInfo, []string{"2001::1000", "2001::2", "2001::3", "2001::4"})
 
 	// Release first IP from first range and middle IP from second range
 	for _, ipToRelease := range []string{"2001::1000", "2001::2"} {
@@ -263,5 +263,5 @@ func TestAllocateReleaseSequence(t *testing.T) {
 		require.NoError(t, err)
 	}
 
-	validateAllocationSequence(t, allocator, subnetInfo, []string{"2001::1000", "2001::2", "2001::4"})
+	validateAllocationSequence(t, allocator, subnetInfo, []string{"2001::1000", "2001::2", "2001::5"})
 }

pkg/util/ip/ip.go
@@ -16,6 +16,7 @@ package ip
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"sort"
@@ -197,4 +198,13 @@ func MustIPv6(s string) net.IP {
 		panic(fmt.Errorf("invalid IPv6 address: %s", s))
 	}
 	return ip
+}
+
+// GetLocalBroadcastIP returns the last IP address in a subnet. This IP is always working as the broadcast address in
+// the subnet on Windows, and an active route entry that uses it as the destination is added by default when a new IP is
+// configured on the interface.
+func GetLocalBroadcastIP(ipNet *net.IPNet) net.IP {
+	lastAddr := make(net.IP, len(ipNet.IP.To4()))
+	binary.BigEndian.PutUint32(lastAddr, binary.BigEndian.Uint32(ipNet.IP.To4())|^binary.BigEndian.Uint32(net.IP(ipNet.Mask).To4()))
+	return lastAddr
 }

commit ea7f8456eb8d927819c85a2585962cefc54e3219

[egress/flexible-ipam] Fix wrong total IP count
This issue is caused by PR #2942 and detcted by e2e test.

Signed-off-by: gran <gran@vmware.com>

pkg/ipam/ipallocator/allocator.go
@@ -238,7 +238,7 @@ func (ma MultiIPAllocator) Used() int {
 func (ma MultiIPAllocator) Total() int {
 	total := 0
 	for _, a := range ma {
-		total += a.max
+		total += a.max - len(a.reservedIPs)
 	}
 	return total
 }