40aa21164a2555909078f5ee453c76a542bc3a04
Yang Ding <dingyang@vmware.com>
2021-11-01 19:53:21 -0700

    Add Pass action for Antrea-native policies
    Signed-off-by: Yang Ding <dingyang@vmware.com>

--- pkg/agent/openflow/pipeline.go
+++ pkg/agent/openflow/pipeline.go
@@ -218,6 +218,7 @@ const (
 	DispositionAllow = 0b00
 	DispositionDrop  = 0b01
 	DispositionRej   = 0b10
+	DispositionPass  = 0b11
 
 	// CustomReasonLogging is used when send packet-in to controller indicating this
 	// packet need logging.
@@ -239,6 +240,7 @@ var DispositionToString = map[uint32]string{
 	DispositionAllow: "Allow",
 	DispositionDrop:  "Drop",
 	DispositionRej:   "Reject",
+	DispositionPass:  "Pass",
 }
 
 var (
@@ -1745,6 +1747,28 @@ func (c *client) conjunctionActionDenyFlow(conjunctionID uint32, table binding.T
 
 	// We do not drop the packet immediately but send the packet to the metric table to update the rule metrics.
 	return flowBuilder.Action().GotoTable(metricTable.GetID()).
+		Cookie(c.cookieAllocator.Request(cookie.Policy).Raw()).
+		Done()
+}
+
+func (c *client) conjunctionActionPassFlow(conjunctionID uint32, table binding.Table, priority *uint16, disposition uint32, enableLogging bool) binding.Flow {
+	ofPriority := *priority
+	nextTable := IngressRuleTable
+	tableID := table.GetID()
+	if _, ok := egressTables[tableID]; ok {
+		nextTable = EgressRuleTable
+	}
+	flowBuilder := table.BuildFlow(ofPriority).
+		MatchConjID(conjunctionID).
+		Action().LoadRegMark(DisposttionPassRegMark)
+	if enableLogging {
+		customReason := CustomReasonLogging
+		flowBuilder = flowBuilder.
+			Action().LoadToRegField(APDispositionField, disposition).
+			Action().LoadToRegField(CustomReasonField, uint32(customReason)).
+			Action().SendToController(uint8(PacketInReasonNP))
+	}
+	return flowBuilder.Action().GotoTable(nextTable.GetID()).
 		Cookie(c.cookieAllocator.Request(cookie.Policy).Raw()).
 		Done()
 }
