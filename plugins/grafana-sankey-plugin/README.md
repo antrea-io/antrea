18a79ed92b6e85ae73a9de16cbc4d2b8aef59645
zyiou <zyiou@vmware.com>
2021-11-16 22:24:48 -0800

    Add grafana and clickhouse deployment file
    This PR is for adding deployment file and custom plugin for Grafana
    configuration and setup for Clickhouse, which are the UI solution
    and database solution for flow visibility long term architecture.
    
    Signed-off-by: zyiou <zyiou@vmware.com>

---
+++ plugins/grafana-sankey-plugin/README.md
@@ -0,0 +1,40 @@
+# Grafana Sankey Plugin
+
+## What is Grafana Sankey Panel Plugin?
+
+Panels are the building blocks of Grafana. They allow you to visualize data in different ways. Grafana Sankey Panel Plugin enables users to create Sankey Diagram panel in Grafana Dashboards.
+
+For more information about panels, refer to the documentation on [Panels](https://grafana.com/docs/grafana/latest/features/panels/panels/)
+
+## Getting started
+
+1. Install dependencies
+
+   ```bash
+   yarn install
+   ```
+
+2. Build plugin in development mode or run in watch mode
+
+   ```bash
+   yarn dev
+   ```
+
+   or
+
+   ```bash
+   yarn watch
+   ```
+
+3. Build plugin in production mode
+
+   ```bash
+   yarn build
+   ```
+
+## Learn more
+
+- [Build a panel plugin tutorial](https://grafana.com/tutorials/build-a-panel-plugin)
+- [Grafana documentation](https://grafana.com/docs/)
+- [Grafana Tutorials](https://grafana.com/tutorials/) - Grafana Tutorials are step-by-step guides that help you make the most of Grafana
+- [Grafana UI Library](https://developers.grafana.com/ui) - UI components to help you build interfaces using Grafana Design System
---
+++ plugins/grafana-sankey-plugin/src/SankeyPanel.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { SankeyOptions } from 'types';
+import Chart from 'react-google-charts';
+
+interface Props extends PanelProps<SankeyOptions> {}
+
+export const SankeyPanel: React.FC<Props> = ({ options, data, width, height }) => {
+  let result = [
+    ['From', 'To', 'Bytes'],
+    ['Source N/A', 'Destination N/A', 1],
+  ];
+  let sources = data.series
+    .map((series) => series.fields.find((field) => field.name === 'source'))
+    .map((field) => {
+      let record = field?.values as any;
+      return record?.buffer;
+    })[0];
+  if (sources !== undefined) {
+    let destinations = data.series
+      .map((series) => series.fields.find((field) => field.name === 'destination'))
+      .map((field) => {
+        let record = field?.values as any;
+        return record?.buffer;
+      })[0];
+    let destinationIPs = data.series
+      .map((series) => series.fields.find((field) => field.name === 'destinationIP'))
+      .map((field) => {
+        let record = field?.values as any;
+        return record?.buffer;
+      })[0];
+    let bytes = data.series
+      .map((series) => series.fields.find((field) => field.name === 'bytes'))
+      .map((field) => {
+        let record = field?.values as any;
+        return record?.buffer;
+      })[0];
+    let n = sources.length;
+    for (let i = 0; i < n; i++) {
+      if (bytes[i] === 0) continue;
+      let record = [];
+      let source = sources[i];
+      let destination = destinations[i];
+      record.push(source);
+      if (destination === '') {
+        record.push(destinationIPs[i]);
+      } else {
+        // Google Chart will not be rendered if source is equal to destination.
+        // Add an extra space to differentiate to-self traffic (e.g. intra-Node).
+        if (source === destination) {
+          destination = destination + ' ';
+        }
+        record.push(destination);
+      }
+      record.push(bytes[i]);
+      if (i === 0) {
+        result = [['From', 'To', 'Bytes']];
+      }
+      result.push(record);
+    }
+  }
+  return (
+    <div>
+      <Chart width={600} height={'600px'} chartType="Sankey" loader={<div>Loading Chart</div>} data={result} />
+    </div>
+  );
+};
