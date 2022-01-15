aab9ae8065b306d26ecc6e0f22474ffe6c6bde75
luolanzone <luola@vmware.com>
2022-01-15 10:10:53 +0800

    Add release manifests for multicluster (#3197)
    Signed-off-by: Lan Luo <luola@vmware.com>

--- multicluster/hack/generate-manifest.sh
+++ multicluster/hack/generate-manifest.sh
@@ -20,16 +20,20 @@ function echoerr {
     >&2 echo "$@"
 }
 
-_usage="Usage: $0 [--global|-g] [--leader|-l <namespace>] [--member|-m] [--help|-h]
+_usage="Usage: $0 [--release|-r] [--global|-g] [--leader|-l <namespace>] [--member|-m] [--help|-h]
 Generate a YAML manifest for Antrea MultiCluster using Kustomize and print it to stdout.
-        --global                       Generate a global manifest for a Cluster as leader in a ClusterSet
-        --leader                       Generate a per-namespace manifest for a Cluster as leader in a ClusterSet.
-                                       All resources will be in the given namespace
-        --member                       Generate a manifest for a Cluster as member in a ClusterSet
-        --help, -h                     Print this message and exit
+        --release | -r                      Enable release mode which will set correct release variant.
+        --global  | -g                      Generate a global manifest for a Cluster as leader in a ClusterSet
+        --leader  | -l                      Generate a per-namespace manifest for a Cluster as leader in a ClusterSet.
+                                            All resources will be in the given namespace
+        --member  | -m                      Generate a manifest for a Cluster as member in a ClusterSet
+        --help    | -h                      Print this message and exit
 
 This tool uses kustomize (https://github.com/kubernetes-sigs/kustomize) to generate manifests for
-Antrea MultiCluster Controller. please run 'make kustomize' first"
+Antrea MultiCluster Controller. please run 'make kustomize' first
+
+Environment variables IMG_NAME and IMG_TAG must be set when release mode is enabled.
+"
 
 function print_usage {
     echoerr "$_usage"
@@ -41,30 +45,51 @@ function print_help {
 
 OVERLAY=member
 NAMESPACE=changeme
+MODE=""
 
-while getopts gl:mhu flag
+while [[ $# -gt 0 ]]
 do
-    case "${flag}" in
-        g)
-          OVERLAY=leader-global
-          ;;
-        l)
-          OVERLAY=leader-ns
-          NAMESPACE=$OPTARG
-          ;;
-        m)
-          OVERLAY=member
-          ;;
-        h)
-          print_help
-          exit 0
-          ;;
-        u | *)
-          print_usage
-          exit 0
-          ;;
-    esac
+key="$1"
+case $key in
+    --release|-r)
+    MODE="release"
+    shift
+    ;;
+    --leader|-l)
+    OVERLAY=leader-ns
+    NAMESPACE="$2"
+    shift 2
+    ;;
+    --global|-g)
+    OVERLAY=leader-global
+    shift
+    ;;
+    --member|-m)
+    OVERLAY=member
+    shift
+    ;;
+    -h|--help)
+    print_usage
+    exit 0
+    ;;
+    *)    # unknown option
+    echoerr "Unknown option $1"
+    exit 1
+    ;;
+esac
 done
+
+if [ "$MODE" == "release" ] && [ -z "$IMG_TAG" ]; then
+    echoerr "In 'release' mode, environment variable IMG_TAG must be set"
+    print_help
+    exit 1
+fi
+
+if [ "$MODE" == "release" ] && [ -z "$IMG_NAME" ]; then
+    echoerr "In 'release' mode, environment variable IMG_NAME must be set"
+    print_help
+    exit 1
+fi
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -74,10 +99,11 @@ KUSTOMIZATION_DIR=$THIS_DIR/../config
 
 cd $KUSTOMIZATION_DIR
 
+TMP_DIR=$(mktemp -d $KUSTOMIZATION_DIR/overlays.XXXXXXXX)
+pushd $TMP_DIR > /dev/null
+
 if [ "$OVERLAY" == "leader-ns" ] ;
 then
-    TMP_DIR=$(mktemp -d $KUSTOMIZATION_DIR/overlays.XXXXXXXX)
-    pushd $TMP_DIR > /dev/null
     mkdir config && cd config
     cp $KUSTOMIZATION_DIR/overlays/leader-ns/prefix_transformer.yaml .
     sed -ie "s/changeme/$NAMESPACE/g" prefix_transformer.yaml
@@ -86,13 +112,20 @@ cat << EOF > kustomization.yaml
 namespace: $NAMESPACE
 
 bases:
-  - ../../overlays/leader-ns
+  - ../../overlays/$OVERLAY
 
 transformers:
   - prefix_transformer.yaml
 EOF
-    $KUSTOMIZE build
-    rm -rf $TMP_DIR
 else
-    $KUSTOMIZE build overlays/$OVERLAY
+cat << EOF > kustomization.yaml
+bases:
+  - ../overlays/$OVERLAY
+EOF
 fi
+
+if [ "$MODE" == "release" ]; then
+    $KUSTOMIZE edit set image antrea/antrea-mc-controller=$IMG_NAME:$IMG_TAG
+fi
+$KUSTOMIZE build
+rm -rf $TMP_DIR
