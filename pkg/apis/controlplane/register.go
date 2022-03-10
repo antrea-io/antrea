3d994ccb3ac951332379d952392bda1e0019732c
Qiyue Yao <39061776+qiyueyao@users.noreply.github.com>
2022-03-10 06:44:10 -0800

    Support Pagination for ClusterGroupMembership (#3183)
    Fixes #2492 
    
    Signed-off-by: Qiyue Yao <yaoq@vmware.com>

--- pkg/apis/controlplane/register.go
+++ pkg/apis/controlplane/register.go
@@ -56,6 +56,7 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&NetworkPolicyStatus{},
 		&NodeStatsSummary{},
 		&ClusterGroupMembers{},
+		&PaginationGetOptions{},
 		&GroupAssociation{},
 		&EgressGroup{},
 		&EgressGroupPatch{},
