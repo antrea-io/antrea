3a3707f64c1c580167f88d05e30b6fb8faaa2c47
Hongliang Liu <lhongliang@vmware.com>
2022-03-29 14:12:30 +0800

    Fix NetworkPolicyOnly mode issue
    When NetworkPolicyOnly mode is enabled, PodIPv4CIDR of nodeConfig is nil,
    then l3FwdFlowToLocalPodCIDR should not be called.
    
    Signed-off-by: Hongliang Liu <lhongliang@vmware.com>

--- pkg/agent/openflow/pod_connectivity.go
+++ pkg/agent/openflow/pod_connectivity.go
@@ -63,13 +63,17 @@ func newFeaturePodConnectivity(
 		if ipProtocol == binding.ProtocolIP {
 			ctZones[ipProtocol] = CtZone
 			gatewayIPs[ipProtocol] = nodeConfig.GatewayConfig.IPv4
-			localCIDRs[ipProtocol] = *nodeConfig.PodIPv4CIDR
 			nodeIPs[ipProtocol] = nodeConfig.NodeIPv4Addr.IP
+			if nodeConfig.PodIPv4CIDR != nil {
+				localCIDRs[ipProtocol] = *nodeConfig.PodIPv4CIDR
+			}
 		} else if ipProtocol == binding.ProtocolIPv6 {
 			ctZones[ipProtocol] = CtZoneV6
 			gatewayIPs[ipProtocol] = nodeConfig.GatewayConfig.IPv6
-			localCIDRs[ipProtocol] = *nodeConfig.PodIPv6CIDR
 			nodeIPs[ipProtocol] = nodeConfig.NodeIPv6Addr.IP
+			if nodeConfig.PodIPv6CIDR != nil {
+				localCIDRs[ipProtocol] = *nodeConfig.PodIPv6CIDR
+			}
 		}
 	}
 
@@ -104,7 +108,6 @@ func (f *featurePodConnectivity) initFlows() []binding.Flow {
 			}
 		}
 	}
-	flows = append(flows, f.l3FwdFlowToLocalPodCIDR()...)
 	if f.connectUplinkToBridge {
 		flows = append(flows, f.l3FwdFlowToNode()...)
 	}
@@ -117,6 +120,11 @@ func (f *featurePodConnectivity) initFlows() []binding.Flow {
 		// If IPv6 is enabled, this flow will never get hit.
 		// Replies any ARP request with the same global virtual MAC.
 		flows = append(flows, f.arpResponderStaticFlow())
+	} else {
+		// If NetworkPolicyOnly mode is enabled, IPAM is implemented by the primary CNI, which may not use the Pod CIDR
+		// of the Node. Therefore, it doesn't make sense to install flows for the Pod CIDR. Individual flow for each local
+		// Pod IP will take care of routing the traffic to destination Pod.
+		flows = append(flows, f.l3FwdFlowToLocalPodCIDR()...)
 	}
 	return flows
 }
