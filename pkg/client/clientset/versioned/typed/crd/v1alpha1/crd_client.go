5c1cc99fa17eac0d86caa925b490c98f34f37ec3
Mengdie Song <songm@vmware.com>
2022-04-21 02:24:36 +0800

    [ExternalNode] Add ExternalNode CRD (#3639)
    This change adds API definition for ExternalNode CRD
    and generates the corresponding client and yaml files.
    
    Signed-off-by: Mengdie Song <songm@vmware.com>

--- pkg/client/clientset/versioned/typed/crd/v1alpha1/crd_client.go
+++ pkg/client/clientset/versioned/typed/crd/v1alpha1/crd_client.go
@@ -27,6 +27,7 @@ import (
 type CrdV1alpha1Interface interface {
 	RESTClient() rest.Interface
 	ClusterNetworkPoliciesGetter
+	ExternalNodesGetter
 	NetworkPoliciesGetter
 	TiersGetter
 	TraceflowsGetter
@@ -39,6 +40,10 @@ type CrdV1alpha1Client struct {
 
 func (c *CrdV1alpha1Client) ClusterNetworkPolicies() ClusterNetworkPolicyInterface {
 	return newClusterNetworkPolicies(c)
+}
+
+func (c *CrdV1alpha1Client) ExternalNodes(namespace string) ExternalNodeInterface {
+	return newExternalNodes(c, namespace)
 }
 
 func (c *CrdV1alpha1Client) NetworkPolicies(namespace string) NetworkPolicyInterface {
