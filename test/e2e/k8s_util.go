antrea-io/antrea

commit 513484660f806a2532e991f06267e973e7c1c618

E2E test of Antrea native policy ICMP support (#3635)
Add E2E tests and related content in doc for ICMP support PR #3472

Signed-off-by: wgrayson <wgrayson@vmware.com>

test/e2e/k8s_util.go
@@ -32,6 +32,7 @@ import (
 	crdv1alpha1 "antrea.io/antrea/pkg/apis/crd/v1alpha1"
 	crdv1alpha2 "antrea.io/antrea/pkg/apis/crd/v1alpha2"
 	crdv1alpha3 "antrea.io/antrea/pkg/apis/crd/v1alpha3"
+	"antrea.io/antrea/test/e2e/utils"
 )
 
 type KubernetesUtils struct {
@@ -60,7 +61,7 @@ type TestStep struct {
 	Reachability  *Reachability
 	TestResources []metav1.Object
 	Ports         []int32
-	Protocol      v1.Protocol
+	Protocol      utils.AntreaPolicyProtocol
 	Duration      time.Duration
 	CustomProbes  []*CustomProbe
 }
@@ -120,12 +121,12 @@ func (k *KubernetesUtils) probe(
 	dstAddr string,
 	dstName string,
 	port int32,
-	protocol v1.Protocol,
+	protocol utils.AntreaPolicyProtocol,
 ) PodConnectivityMark {
-	protocolStr := map[v1.Protocol]string{
-		v1.ProtocolTCP:  "tcp",
-		v1.ProtocolUDP:  "udp",
-		v1.ProtocolSCTP: "sctp",
+	protocolStr := map[utils.AntreaPolicyProtocol]string{
+		utils.ProtocolTCP:  "tcp",
+		utils.ProtocolUDP:  "udp",
+		utils.ProtocolSCTP: "sctp",
 	}
 	// We try to connect 3 times. This dates back to when we were using the OVS netdev datapath
 	// for Kind clusters, as the first packet sent on a tunnel was always dropped
@@ -178,11 +179,77 @@ func DecideProbeResult(stderr string, probeNum int) PodConnectivityMark {
 	return Error
 }
 
+func (k *KubernetesUtils) pingProbe(
+	pod *v1.Pod,
+	podName string,
+	containerName string,
+	dstAddr string,
+	dstName string,
+) PodConnectivityMark {
+	pingCmd := fmt.Sprintf("ping -4 -c 3 -W 1 %s", dstAddr)
+	if strings.Contains(dstAddr, ":") {
+		pingCmd = fmt.Sprintf("ping -6 -c 3 -W 1 %s", dstAddr)
+	}
+	cmd := []string{
+		"/bin/sh",
+		"-c",
+		pingCmd,
+	}
+	log.Tracef("Running: kubectl exec %s -c %s -n %s -- %s", pod.Name, containerName, pod.Namespace, strings.Join(cmd, " "))
+	stdout, stderr, err := k.RunCommandFromPod(pod.Namespace, pod.Name, containerName, cmd)
+	log.Tracef("%s -> %s: error when running command: err - %v /// stdout - %s /// stderr - %s", podName, dstName, err, stdout, stderr)
+	return DecidePingProbeResult(stdout, 3)
+}
+
+// DecidePingProbeResult uses the pingProbe stdout to decide the connectivity.
+func DecidePingProbeResult(stdout string, probeNum int) PodConnectivityMark {
+	// Provide stdout example for different connectivity:
+	// ================== Connected stdout ==================
+	// PING 10.10.1.2 (10.10.1.2) 56(84) bytes of data.
+	// 64 bytes from 10.10.1.2: icmp_seq=1 ttl=64 time=0.695 ms
+	// 64 bytes from 10.10.1.2: icmp_seq=2 ttl=64 time=0.250 ms
+	// 64 bytes from 10.10.1.2: icmp_seq=3 ttl=64 time=0.058 ms
+	//
+	// --- 10.10.1.2 ping statistics ---
+	// 3 packets transmitted, 3 received, 0% packet loss, time 2043ms
+	// rtt min/avg/max/mdev = 0.058/0.334/0.695/0.266 ms
+	// ======================================================
+	// =================== Dropped stdout ===================
+	// PING 10.10.1.2 (10.10.1.2) 56(84) bytes of data.
+	//
+	// --- 10.10.1.2 ping statistics ---
+	// 3 packets transmitted, 0 received, 100% packet loss, time 2037ms
+	// =======================================================
+	// =================== Rejected stdout ===================
+	// PING 10.10.1.2 (10.10.1.2) 56(84) bytes of data.
+	// From 10.10.1.2 icmp_seq=1 Destination Host Prohibited
+	// From 10.10.1.2 icmp_seq=2 Destination Host Prohibited
+	// From 10.10.1.2 icmp_seq=3 Destination Host Prohibited
+	//
+	// --- 10.10.1.2 ping statistics ---
+	// 3 packets transmitted, 0 received, +3 errors, 100% packet loss, time 2042ms
+	// =======================================================
+	countConnected := strings.Count(stdout, "bytes from")
+	countRejected := strings.Count(stdout, "Prohibited")
+	countDropped := probeNum - strings.Count(stdout, "icmp_seq")
+
+	if countRejected == 0 && countConnected > 0 {
+		return Connected
+	}
+	if countConnected == 0 && countRejected > 0 {
+		return Rejected
+	}
+	if countDropped == probeNum {
+		return Dropped
+	}
+	return Error
+}
+
 // Probe execs into a Pod and checks its connectivity to another Pod. Of course it
 // assumes that the target Pod is serving on the input port, and also that agnhost
 // is installed. The connectivity from source Pod to all IPs of the target Pod
 // should be consistent. Otherwise, Error PodConnectivityMark will be returned.
-func (k *KubernetesUtils) Probe(ns1, pod1, ns2, pod2 string, port int32, protocol v1.Protocol) (PodConnectivityMark, error) {
+func (k *KubernetesUtils) Probe(ns1, pod1, ns2, pod2 string, port int32, protocol utils.AntreaPolicyProtocol) (PodConnectivityMark, error) {
 	fromPods, err := k.GetPodsByLabel(ns1, "pod", pod1)
 	if err != nil {
 		return Error, fmt.Errorf("unable to get Pods from Namespace %s: %v", ns1, err)
@@ -223,7 +290,7 @@ func (k *KubernetesUtils) Probe(ns1, pod1, ns2, pod2 string, port int32, protoco
 
 // ProbeAddr execs into a Pod and checks its connectivity to an arbitrary destination
 // address.
-func (k *KubernetesUtils) ProbeAddr(ns, podLabelKey, podLabelValue, dstAddr string, port int32, protocol v1.Protocol) (PodConnectivityMark, error) {
+func (k *KubernetesUtils) ProbeAddr(ns, podLabelKey, podLabelValue, dstAddr string, port int32, protocol utils.AntreaPolicyProtocol) (PodConnectivityMark, error) {
 	fromPods, err := k.GetPodsByLabel(ns, podLabelKey, podLabelValue)
 	if err != nil {
 		return Error, fmt.Errorf("unable to get Pods from Namespace %s: %v", ns, err)
@@ -233,11 +300,16 @@ func (k *KubernetesUtils) ProbeAddr(ns, podLabelKey, podLabelValue, dstAddr stri
 	}
 	fromPod := fromPods[0]
 	containerName := fromPod.Spec.Containers[0].Name
-	// If it's an IPv6 address, add "[]" around it.
-	if strings.Contains(dstAddr, ":") {
-		dstAddr = fmt.Sprintf("[%s]", dstAddr)
+	var connectivity PodConnectivityMark
+	if protocol == utils.ProtocolICMP {
+		connectivity = k.pingProbe(&fromPod, fmt.Sprintf("%s/%s", ns, podLabelValue), containerName, dstAddr, dstAddr)
+	} else {
+		// If it's an IPv6 address, add "[]" around it.
+		if strings.Contains(dstAddr, ":") {
+			dstAddr = fmt.Sprintf("[%s]", dstAddr)
+		}
+		connectivity = k.probe(&fromPod, fmt.Sprintf("%s/%s", ns, podLabelValue), containerName, dstAddr, dstAddr, port, protocol)
 	}
-	connectivity := k.probe(&fromPod, fmt.Sprintf("%s/%s", ns, podLabelValue), containerName, dstAddr, dstAddr, port, protocol)
 	return connectivity, nil
 }
 
@@ -807,17 +879,17 @@ func (k *KubernetesUtils) waitForHTTPServers(allPods []Pod) error {
 
 	serversAreReady := func() bool {
 		reachability := NewReachability(allPods, Connected)
-		k.Validate(allPods, reachability, []int32{80, 81, 8080, 8081, 8082, 8083, 8084, 8085}, v1.ProtocolTCP)
+		k.Validate(allPods, reachability, []int32{80, 81, 8080, 8081, 8082, 8083, 8084, 8085}, utils.ProtocolTCP)
 		if _, wrong, _ := reachability.Summary(); wrong != 0 {
 			return false
 		}
 
-		k.Validate(allPods, reachability, []int32{80, 81}, v1.ProtocolUDP)
+		k.Validate(allPods, reachability, []int32{80, 81}, utils.ProtocolUDP)
 		if _, wrong, _ := reachability.Summary(); wrong != 0 {
 			return false
 		}
 
-		k.Validate(allPods, reachability, []int32{80, 81}, v1.ProtocolSCTP)
+		k.Validate(allPods, reachability, []int32{80, 81}, utils.ProtocolSCTP)
 		if _, wrong, _ := reachability.Summary(); wrong != 0 {
 			return false
 		}
@@ -834,7 +906,7 @@ func (k *KubernetesUtils) waitForHTTPServers(allPods []Pod) error {
 	return errors.Errorf("after %d tries, HTTP servers are not ready", maxTries)
 }
 
-func (k *KubernetesUtils) validateOnePort(allPods []Pod, reachability *Reachability, port int32, protocol v1.Protocol) {
+func (k *KubernetesUtils) validateOnePort(allPods []Pod, reachability *Reachability, port int32, protocol utils.AntreaPolicyProtocol) {
 	type probeResult struct {
 		podFrom      Pod
 		podTo        Pod
@@ -884,7 +956,7 @@ func (k *KubernetesUtils) validateOnePort(allPods []Pod, reachability *Reachabil
 // list of ports and a protocol. The connectivity from a Pod to another Pod should
 // be consistent across all provided ports. Otherwise, this connectivity will be
 // treated as Error.
-func (k *KubernetesUtils) Validate(allPods []Pod, reachability *Reachability, ports []int32, protocol v1.Protocol) {
+func (k *KubernetesUtils) Validate(allPods []Pod, reachability *Reachability, ports []int32, protocol utils.AntreaPolicyProtocol) {
 	for _, port := range ports {
 		// we do not run all the probes in parallel as we have experienced that on some
 		// machines, this can cause a fraction of the probes to always fail, despite the

multicluster/test/e2e/antreapolicy_test.go
@@ -20,9 +20,9 @@ import (
 	"time"
 
 	log "github.com/sirupsen/logrus"
-	v1 "k8s.io/api/core/v1"
 
 	antreae2e "antrea.io/antrea/test/e2e"
+	"antrea.io/antrea/test/e2e/utils"
 )
 
 const (
@@ -102,7 +102,7 @@ func (data *MCTestData) testAntreaPolicyCopySpanNSIsolation(t *testing.T) {
 		Name:         "Port 80",
 		Reachability: reachability,
 		Ports:        []int32{80},
-		Protocol:     v1.ProtocolTCP,
+		Protocol:     utils.ProtocolTCP,
 	}
 	testCaseList := []*antreae2e.TestCase{
 		{

test/e2e/flowaggregator_test.go
@@ -1159,7 +1159,7 @@ func deployAntreaNetworkPolicies(t *testing.T, data *TestData, srcPod, dstPod st
 	builder1 = builder1.SetName(testNamespace, ingressAntreaNetworkPolicyName).
 		SetPriority(2.0).
 		SetAppliedToGroup([]utils.ANPAppliedToSpec{{PodSelector: map[string]string{"antrea-e2e": dstPod}}})
-	builder1 = builder1.AddIngress(corev1.ProtocolTCP, nil, nil, nil, nil, map[string]string{"antrea-e2e": srcPod}, map[string]string{},
+	builder1 = builder1.AddIngress(utils.ProtocolTCP, nil, nil, nil, nil, nil, nil, map[string]string{"antrea-e2e": srcPod}, map[string]string{},
 		nil, nil, nil, secv1alpha1.RuleActionAllow, testIngressRuleName)
 	anp1 = builder1.Get()
 	anp1, err1 := k8sUtils.CreateOrUpdateANP(anp1)
@@ -1172,7 +1172,7 @@ func deployAntreaNetworkPolicies(t *testing.T, data *TestData, srcPod, dstPod st
 	builder2 = builder2.SetName(testNamespace, egressAntreaNetworkPolicyName).
 		SetPriority(2.0).
 		SetAppliedToGroup([]utils.ANPAppliedToSpec{{PodSelector: map[string]string{"antrea-e2e": srcPod}}})
-	builder2 = builder2.AddEgress(corev1.ProtocolTCP, nil, nil, nil, nil, map[string]string{"antrea-e2e": dstPod}, map[string]string{},
+	builder2 = builder2.AddEgress(utils.ProtocolTCP, nil, nil, nil, nil, nil, nil, map[string]string{"antrea-e2e": dstPod}, map[string]string{},
 		nil, nil, nil, secv1alpha1.RuleActionAllow, testEgressRuleName)
 	anp2 = builder2.Get()
 	anp2, err2 := k8sUtils.CreateOrUpdateANP(anp2)
@@ -1197,24 +1197,24 @@ func deployDenyAntreaNetworkPolicies(t *testing.T, data *TestData, srcPod, podRe
 		builder1 = builder1.SetName(testNamespace, ingressRejectANPName).
 			SetPriority(2.0).
 			SetAppliedToGroup([]utils.ANPAppliedToSpec{{PodSelector: map[string]string{"antrea-e2e": podReject}}})
-		builder1 = builder1.AddIngress(corev1.ProtocolTCP, nil, nil, nil, nil, map[string]string{"antrea-e2e": srcPod}, map[string]string{},
+		builder1 = builder1.AddIngress(utils.ProtocolTCP, nil, nil, nil, nil, nil, nil, map[string]string{"antrea-e2e": srcPod}, map[string]string{},
 			nil, nil, nil, secv1alpha1.RuleActionReject, testIngressRuleName)
 		builder2 = builder2.SetName(testNamespace, ingressDropANPName).
 			SetPriority(2.0).
 			SetAppliedToGroup([]utils.ANPAppliedToSpec{{PodSelector: map[string]string{"antrea-e2e": podDrop}}})
-		builder2 = builder2.AddIngress(corev1.ProtocolTCP, nil, nil, nil, nil, map[string]string{"antrea-e2e": srcPod}, map[string]string{},
+		builder2 = builder2.AddIngress(utils.ProtocolTCP, nil, nil, nil, nil, nil, nil, map[string]string{"antrea-e2e": srcPod}, map[string]string{},
 			nil, nil, nil, secv1alpha1.RuleActionDrop, testIngressRuleName)
 	} else {
 		// apply reject and drop egress rule to source pod
 		builder1 = builder1.SetName(testNamespace, egressRejectANPName).
 			SetPriority(2.0).
 			SetAppliedToGroup([]utils.ANPAppliedToSpec{{PodSelector: map[string]string{"antrea-e2e": srcPod}}})
-		builder1 = builder1.AddEgress(corev1.ProtocolTCP, nil, nil, nil, nil, map[string]string{"antrea-e2e": podReject}, map[string]string{},
+		builder1 = builder1.AddEgress(utils.ProtocolTCP, nil, nil, nil, nil, nil, nil, map[string]string{"antrea-e2e": podReject}, map[string]string{},
 			nil, nil, nil, secv1alpha1.RuleActionReject, testEgressRuleName)
 		builder2 = builder2.SetName(testNamespace, egressDropANPName).
 			SetPriority(2.0).
 			SetAppliedToGroup([]utils.ANPAppliedToSpec{{PodSelector: map[string]string{"antrea-e2e": srcPod}}})
-		builder2 = builder2.AddEgress(corev1.ProtocolTCP, nil, nil, nil, nil, map[string]string{"antrea-e2e": podDrop}, map[string]string{},
+		builder2 = builder2.AddEgress(utils.ProtocolTCP, nil, nil, nil, nil, nil, nil, map[string]string{"antrea-e2e": podDrop}, map[string]string{},
 			nil, nil, nil, secv1alpha1.RuleActionDrop, testEgressRuleName)
 	}
 	anp1 = builder1.Get()

test/e2e/framework.go
@@ -1163,9 +1163,9 @@ func (data *TestData) createMcJoinPodOnNode(name string, ns string, nodeName str
 
 // createNetshootPodOnNode creates a Pod in the test namespace with a single netshoot container. The
 // Pod will be scheduled on the specified Node (if nodeName is not empty).
-func (data *TestData) createNetshootPodOnNode(name string, ns string, nodeName string, _ bool) error {
+func (data *TestData) createNetshootPodOnNode(name string, ns string, nodeName string, hostNetwork bool) error {
 	sleepDuration := 3600 // seconds
-	return data.createPodOnNode(name, ns, nodeName, netshootImage, []string{"sleep", strconv.Itoa(sleepDuration)}, nil, nil, nil, true, nil)
+	return data.createPodOnNode(name, ns, nodeName, netshootImage, []string{"sleep", strconv.Itoa(sleepDuration)}, nil, nil, nil, hostNetwork, nil)
 }
 
 // createNginxPodOnNode creates a Pod in the test namespace with a single nginx container. The

test/e2e/utils/anpspecbuilder.go
@@ -15,9 +15,7 @@
 package utils
 
 import (
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 
 	crdv1alpha1 "antrea.io/antrea/pkg/apis/crd/v1alpha1"
 )
@@ -87,8 +85,8 @@ func (b *AntreaNetworkPolicySpecBuilder) GetAppliedToPeer(podSelector map[string
 	}
 }
 
-func (b *AntreaNetworkPolicySpecBuilder) AddIngress(protoc v1.Protocol,
-	port *int32, portName *string, endPort *int32, cidr *string,
+func (b *AntreaNetworkPolicySpecBuilder) AddIngress(protoc AntreaPolicyProtocol,
+	port *int32, portName *string, endPort, icmpType, icmpCode *int32, cidr *string,
 	podSelector map[string]string, nsSelector map[string]string,
 	podSelectorMatchExp []metav1.LabelSelectorRequirement, nsSelectorMatchExp []metav1.LabelSelectorRequirement,
 	ruleAppliedToSpecs []ANPAppliedToSpec, action crdv1alpha1.RuleAction, name string) *AntreaNetworkPolicySpecBuilder {
@@ -129,36 +127,11 @@ func (b *AntreaNetworkPolicySpecBuilder) AddIngress(protoc v1.Protocol,
 			IPBlock:           ipBlock,
 		}}
 	}
-
-	var ports []crdv1alpha1.NetworkPolicyPort
-	if port != nil && portName != nil {
-		panic("specify portname or port, not both")
-	}
-	if portName != nil {
-		ports = []crdv1alpha1.NetworkPolicyPort{
-			{
-				Port:     &intstr.IntOrString{Type: intstr.String, StrVal: *portName},
-				Protocol: &protoc,
-			},
-		}
-	}
-	if port != nil || endPort != nil {
-		var pVal *intstr.IntOrString
-		if port != nil {
-			pVal = &intstr.IntOrString{IntVal: *port}
-		}
-		ports = []crdv1alpha1.NetworkPolicyPort{
-			{
-				Port:     pVal,
-				EndPort:  endPort,
-				Protocol: &protoc,
-			},
-		}
-	}
-
+	ports, protocols := GenPortsOrProtocols(protoc, port, portName, endPort, icmpType, icmpCode)
 	newRule := crdv1alpha1.Rule{
 		From:      policyPeer,
 		Ports:     ports,
+		Protocols: protocols,
 		Action:    &action,
 		Name:      name,
 		AppliedTo: appliedTos,
@@ -167,8 +140,8 @@ func (b *AntreaNetworkPolicySpecBuilder) AddIngress(protoc v1.Protocol,
 	return b
 }
 
-func (b *AntreaNetworkPolicySpecBuilder) AddEgress(protoc v1.Protocol,
-	port *int32, portName *string, endPort *int32, cidr *string,
+func (b *AntreaNetworkPolicySpecBuilder) AddEgress(protoc AntreaPolicyProtocol,
+	port *int32, portName *string, endPort, icmpType, icmpCode *int32, cidr *string,
 	podSelector map[string]string, nsSelector map[string]string,
 	podSelectorMatchExp []metav1.LabelSelectorRequirement, nsSelectorMatchExp []metav1.LabelSelectorRequirement,
 	ruleAppliedToSpecs []ANPAppliedToSpec, action crdv1alpha1.RuleAction, name string) *AntreaNetworkPolicySpecBuilder {
@@ -176,7 +149,7 @@ func (b *AntreaNetworkPolicySpecBuilder) AddEgress(protoc v1.Protocol,
 	// For simplicity, we just reuse the Ingress code here.  The underlying data model for ingress/egress is identical
 	// With the exception of calling the rule `To` vs. `From`.
 	c := &AntreaNetworkPolicySpecBuilder{}
-	c.AddIngress(protoc, port, portName, endPort, cidr, podSelector, nsSelector,
+	c.AddIngress(protoc, port, portName, endPort, icmpType, icmpCode, cidr, podSelector, nsSelector,
 		podSelectorMatchExp, nsSelectorMatchExp, ruleAppliedToSpecs, action, name)
 	theRule := c.Get().Spec.Ingress[0]
 

test/e2e/utils/cnpspecbuilder.go
@@ -15,7 +15,6 @@
 package utils
 
 import (
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 
@@ -104,8 +103,8 @@ func (b *ClusterNetworkPolicySpecBuilder) GetAppliedToPeer(podSelector map[strin
 	return peer
 }
 
-func (b *ClusterNetworkPolicySpecBuilder) AddIngress(protoc v1.Protocol,
-	port *int32, portName *string, endPort *int32, cidr *string,
+func (b *ClusterNetworkPolicySpecBuilder) AddIngress(protoc AntreaPolicyProtocol,
+	port *int32, portName *string, endPort, icmpType, icmpCode *int32, cidr *string,
 	podSelector map[string]string, nsSelector map[string]string,
 	podSelectorMatchExp []metav1.LabelSelectorRequirement, nsSelectorMatchExp []metav1.LabelSelectorRequirement, selfNS bool,
 	ruleAppliedToSpecs []ACNPAppliedToSpec, action crdv1alpha1.RuleAction, ruleClusterGroup, name string, serviceAccount *crdv1alpha1.NamespacedName) *ClusterNetworkPolicySpecBuilder {
@@ -158,35 +157,11 @@ func (b *ClusterNetworkPolicySpecBuilder) AddIngress(protoc v1.Protocol,
 			ServiceAccount:    serviceAccount,
 		}}
 	}
-
-	var ports []crdv1alpha1.NetworkPolicyPort
-	if port != nil && portName != nil {
-		panic("specify portname or port, not both")
-	}
-	if portName != nil {
-		ports = []crdv1alpha1.NetworkPolicyPort{
-			{
-				Port:     &intstr.IntOrString{Type: intstr.String, StrVal: *portName},
-				Protocol: &protoc,
-			},
-		}
-	}
-	if port != nil || endPort != nil {
-		var pVal *intstr.IntOrString
-		if port != nil {
-			pVal = &intstr.IntOrString{IntVal: *port}
-		}
-		ports = []crdv1alpha1.NetworkPolicyPort{
-			{
-				Port:     pVal,
-				EndPort:  endPort,
-				Protocol: &protoc,
-			},
-		}
-	}
+	ports, protocols := GenPortsOrProtocols(protoc, port, portName, endPort, icmpType, icmpCode)
 	newRule := crdv1alpha1.Rule{
 		From:      policyPeer,
 		Ports:     ports,
+		Protocols: protocols,
 		Action:    &action,
 		Name:      name,
 		AppliedTo: appliedTos,
@@ -195,8 +170,8 @@ func (b *ClusterNetworkPolicySpecBuilder) AddIngress(protoc v1.Protocol,
 	return b
 }
 
-func (b *ClusterNetworkPolicySpecBuilder) AddEgress(protoc v1.Protocol,
-	port *int32, portName *string, endPort *int32, cidr *string,
+func (b *ClusterNetworkPolicySpecBuilder) AddEgress(protoc AntreaPolicyProtocol,
+	port *int32, portName *string, endPort, icmpType, icmpCode *int32, cidr *string,
 	podSelector map[string]string, nsSelector map[string]string,
 	podSelectorMatchExp []metav1.LabelSelectorRequirement, nsSelectorMatchExp []metav1.LabelSelectorRequirement, selfNS bool,
 	ruleAppliedToSpecs []ACNPAppliedToSpec, action crdv1alpha1.RuleAction, ruleClusterGroup, name string, serviceAccount *crdv1alpha1.NamespacedName) *ClusterNetworkPolicySpecBuilder {
@@ -204,7 +179,7 @@ func (b *ClusterNetworkPolicySpecBuilder) AddEgress(protoc v1.Protocol,
 	// For simplicity, we just reuse the Ingress code here.  The underlying data model for ingress/egress is identical
 	// With the exception of calling the rule `To` vs. `From`.
 	c := &ClusterNetworkPolicySpecBuilder{}
-	c.AddIngress(protoc, port, portName, endPort, cidr, podSelector, nsSelector,
+	c.AddIngress(protoc, port, portName, endPort, icmpType, icmpCode, cidr, podSelector, nsSelector,
 		podSelectorMatchExp, nsSelectorMatchExp, selfNS, ruleAppliedToSpecs, action, ruleClusterGroup, name, serviceAccount)
 	theRule := c.Get().Spec.Ingress[0]
 
@@ -218,17 +193,17 @@ func (b *ClusterNetworkPolicySpecBuilder) AddEgress(protoc v1.Protocol,
 	return b
 }
 
-func (b *ClusterNetworkPolicySpecBuilder) AddNodeSelectorRule(nodeSelector *metav1.LabelSelector, protoc v1.Protocol, port *int32, name string,
+func (b *ClusterNetworkPolicySpecBuilder) AddNodeSelectorRule(nodeSelector *metav1.LabelSelector, protoc AntreaPolicyProtocol, port *int32, name string,
 	ruleAppliedToSpecs []ACNPAppliedToSpec, action crdv1alpha1.RuleAction, isEgress bool) *ClusterNetworkPolicySpecBuilder {
 	var appliedTos []crdv1alpha1.NetworkPolicyPeer
 	for _, at := range ruleAppliedToSpecs {
 		appliedTos = append(appliedTos, b.GetAppliedToPeer(at.PodSelector, at.NSSelector, at.PodSelectorMatchExp, at.NSSelectorMatchExp, at.Group))
 	}
 	policyPeer := []crdv1alpha1.NetworkPolicyPeer{{NodeSelector: nodeSelector}}
-
+	k8sProtocol, _ := AntreaPolicyProtocolToK8sProtocol(protoc)
 	newRule := crdv1alpha1.Rule{
 		Ports: []crdv1alpha1.NetworkPolicyPort{
-			{Protocol: &protoc, Port: &intstr.IntOrString{IntVal: *port}},
+			{Protocol: &k8sProtocol, Port: &intstr.IntOrString{IntVal: *port}},
 		},
 		Action:    &action,
 		Name:      name,
@@ -245,35 +220,14 @@ func (b *ClusterNetworkPolicySpecBuilder) AddNodeSelectorRule(nodeSelector *meta
 }
 
 func (b *ClusterNetworkPolicySpecBuilder) AddFQDNRule(fqdn string,
-	protoc v1.Protocol, port *int32, portName *string, endPort *int32, name string,
+	protoc AntreaPolicyProtocol, port *int32, portName *string, endPort *int32, name string,
 	ruleAppliedToSpecs []ACNPAppliedToSpec, action crdv1alpha1.RuleAction) *ClusterNetworkPolicySpecBuilder {
 	var appliedTos []crdv1alpha1.NetworkPolicyPeer
 	for _, at := range ruleAppliedToSpecs {
 		appliedTos = append(appliedTos, b.GetAppliedToPeer(at.PodSelector, at.NSSelector, at.PodSelectorMatchExp, at.NSSelectorMatchExp, at.Group))
 	}
 	policyPeer := []crdv1alpha1.NetworkPolicyPeer{{FQDN: fqdn}}
-	var ports []crdv1alpha1.NetworkPolicyPort
-	if portName != nil {
-		ports = []crdv1alpha1.NetworkPolicyPort{
-			{
-				Port:     &intstr.IntOrString{Type: intstr.String, StrVal: *portName},
-				Protocol: &protoc,
-			},
-		}
-	}
-	if port != nil || endPort != nil {
-		var pVal *intstr.IntOrString
-		if port != nil {
-			pVal = &intstr.IntOrString{IntVal: *port}
-		}
-		ports = []crdv1alpha1.NetworkPolicyPort{
-			{
-				Port:     pVal,
-				EndPort:  endPort,
-				Protocol: &protoc,
-			},
-		}
-	}
+	ports, _ := GenPortsOrProtocols(protoc, port, portName, endPort, nil, nil)
 	newRule := crdv1alpha1.Rule{
 		To:        policyPeer,
 		Ports:     ports,
@@ -304,7 +258,7 @@ func (b *ClusterNetworkPolicySpecBuilder) AddToServicesRule(svcRefs []crdv1alpha
 
 // AddEgressDNS mutates the nth policy rule to allow DNS, convenience method
 func (b *ClusterNetworkPolicySpecBuilder) WithEgressDNS() *ClusterNetworkPolicySpecBuilder {
-	protocolUDP := v1.ProtocolUDP
+	protocolUDP, _ := AntreaPolicyProtocolToK8sProtocol(ProtocolUDP)
 	route53 := crdv1alpha1.NetworkPolicyPort{
 		Protocol: &protocolUDP,
 		Port:     &intstr.IntOrString{Type: intstr.Int, IntVal: 53},

test/e2e/utils/helper.go
@@ -1,0 +1,86 @@
+// Copyright 2022 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	crdv1alpha1 "antrea.io/antrea/pkg/apis/crd/v1alpha1"
+)
+
+type AntreaPolicyProtocol string
+
+const (
+	ProtocolTCP  AntreaPolicyProtocol = "TCP"
+	ProtocolUDP  AntreaPolicyProtocol = "UDP"
+	ProtocolSCTP AntreaPolicyProtocol = "SCTP"
+	ProtocolICMP AntreaPolicyProtocol = "ICMP"
+)
+
+func AntreaPolicyProtocolToK8sProtocol(antreaProtocol AntreaPolicyProtocol) (v1.Protocol, error) {
+	switch antreaProtocol {
+	case ProtocolTCP:
+		return v1.ProtocolTCP, nil
+	case ProtocolUDP:
+		return v1.ProtocolUDP, nil
+	case ProtocolSCTP:
+		return v1.ProtocolSCTP, nil
+	default:
+		return "", fmt.Errorf("k8s doesn't support protocol %s", antreaProtocol)
+	}
+}
+
+func GenPortsOrProtocols(protoc AntreaPolicyProtocol, port *int32, portName *string, endPort, icmpType, icmpCode *int32) ([]crdv1alpha1.NetworkPolicyPort, []crdv1alpha1.NetworkPolicyProtocol) {
+	if protoc == ProtocolICMP {
+		return nil, []crdv1alpha1.NetworkPolicyProtocol{
+			{
+				ICMP: &crdv1alpha1.ICMPProtocol{
+					ICMPType: icmpType,
+					ICMPCode: icmpCode,
+				},
+			},
+		}
+	}
+	var ports []crdv1alpha1.NetworkPolicyPort
+	k8sProtocol, _ := AntreaPolicyProtocolToK8sProtocol(protoc)
+	if port != nil && portName != nil {
+		panic("specify portname or port, not both")
+	}
+	if portName != nil {
+		ports = []crdv1alpha1.NetworkPolicyPort{
+			{
+				Port:     &intstr.IntOrString{Type: intstr.String, StrVal: *portName},
+				Protocol: &k8sProtocol,
+			},
+		}
+	}
+	if port != nil || endPort != nil {
+		var pVal *intstr.IntOrString
+		if port != nil {
+			pVal = &intstr.IntOrString{IntVal: *port}
+		}
+		ports = []crdv1alpha1.NetworkPolicyPort{
+			{
+				Port:     pVal,
+				EndPort:  endPort,
+				Protocol: &k8sProtocol,
+			},
+		}
+	}
+	return ports, nil
+}

commit 9816a951d82b85d6c826b3aa2babfd84b2538352

[e2e] Fix ACNPICMPSupport error on IPv6 env
Issue introduced by #3635

Signed-off-by: gran <gran@vmware.com>

test/e2e/k8s_util.go
@@ -229,8 +229,17 @@ func DecidePingProbeResult(stdout string, probeNum int) PodConnectivityMark {
 	// --- 10.10.1.2 ping statistics ---
 	// 3 packets transmitted, 0 received, +3 errors, 100% packet loss, time 2042ms
 	// =======================================================
+	// =================== Rejected ICMPv6 stdout ===================
+	// PING fd02:0:0:f8::11(fd02:0:0:f8::11) 56 data bytes
+	// From fd02:0:0:f8::11 icmp_seq=1 Destination unreachable: Administratively prohibited
+	// From fd02:0:0:f8::11 icmp_seq=2 Destination unreachable: Administratively prohibited
+	// From fd02:0:0:f8::11 icmp_seq=3 Destination unreachable: Administratively prohibited
+	//
+	// --- fd02:0:0:f8::11 ping statistics ---
+	// 3 packets transmitted, 0 received, +3 errors, 100% packet loss, time 2047ms
+	// =======================================================
 	countConnected := strings.Count(stdout, "bytes from")
-	countRejected := strings.Count(stdout, "Prohibited")
+	countRejected := strings.Count(stdout, "Prohibited") + strings.Count(stdout, "prohibited")
 	countDropped := probeNum - strings.Count(stdout, "icmp_seq")
 
 	if countRejected == 0 && countConnected > 0 {