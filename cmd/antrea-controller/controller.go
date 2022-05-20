cfca248c293392c3265ae48c0ccd2f83ce078279
Mengdie Song <songm@vmware.com>
2022-05-21 02:44:14 +0800

    [ExternalNode]Add ExternalNodeController for processing ExternalNode changes (#3687)
    1. Antrea Controller watches ExternalNode CRUD and converts it to
    the corresponding ExternalEntity.
    
    2. The conversion includes the following changes:
    a.These ExternalEntity name is generated by this format:
      If NetworkInterface name is empty, uses ExternalNode name
      If NetworkInterface name is not empty, uses [ExternalNode name]-[Interface name].
    b.The ExternalNode's labels are added on the ExternalEntity(ies).
    c.The ExternalNode name is used to set "ExternalNode" field in the ExternalEntity.
    d.ExternalNode NetworkInterface is used to set "Endpoint" field in the ExternalEntity.
    
    3. Handle ExternalNode reconciliation and cleanup stale ExternalEntities.
    
    Signed-off-by: Mengdie Song <songm@vmware.com>

--- cmd/antrea-controller/controller.go
+++ cmd/antrea-controller/controller.go
@@ -49,6 +49,7 @@ import (
 	"antrea.io/antrea/pkg/controller/egress"
 	egressstore "antrea.io/antrea/pkg/controller/egress/store"
 	"antrea.io/antrea/pkg/controller/externalippool"
+	"antrea.io/antrea/pkg/controller/externalnode"
 	"antrea.io/antrea/pkg/controller/grouping"
 	antreaipam "antrea.io/antrea/pkg/controller/ipam"
 	"antrea.io/antrea/pkg/controller/metrics"
@@ -131,6 +132,7 @@ func run(o *Options) error {
 	cgInformer := crdInformerFactory.Crd().V1alpha3().ClusterGroups()
 	egressInformer := crdInformerFactory.Crd().V1alpha2().Egresses()
 	externalIPPoolInformer := crdInformerFactory.Crd().V1alpha2().ExternalIPPools()
+	externalNodeInformer := crdInformerFactory.Crd().V1alpha1().ExternalNodes()
 
 	clusterIdentityAllocator := clusteridentity.NewClusterIdentityAllocator(
 		env.GetAntreaNamespace(),
@@ -162,6 +164,11 @@ func run(o *Options) error {
 		appliedToGroupStore,
 		networkPolicyStore,
 		groupStore)
+
+	var externalNodeController *externalnode.ExternalNodeController
+	if features.DefaultFeatureGate.Enabled(features.ExternalNode) {
+		externalNodeController = externalnode.NewExternalNodeController(crdClient, externalNodeInformer, eeInformer)
+	}
 
 	var networkPolicyStatusController *networkpolicy.StatusController
 	if features.DefaultFeatureGate.Enabled(features.AntreaPolicy) {
@@ -336,6 +343,10 @@ func run(o *Options) error {
 
 	if features.DefaultFeatureGate.Enabled(features.ServiceExternalIP) {
 		go externalIPController.Run(stopCh)
+	}
+
+	if features.DefaultFeatureGate.Enabled(features.ExternalNode) {
+		go externalNodeController.Run(stopCh)
 	}
 
 	if antreaIPAMController != nil {
