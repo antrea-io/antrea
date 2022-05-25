314f7b2607f29ac5f5a2f1e58e09a93fbcfbceff
Naman Agarwal <naman.agarwal75@gmail.com>
2022-05-25 08:33:50 +0000

    Replace hack/netpol/ with New Upstream NetworkPolicy Test Suite

--- ci/kind/test-np.sh
+++ ci/kind/test-np.sh
@@ -22,37 +22,15 @@ ROOT_DIR=$THIS_DIR/../..
 KIND_CONFIG=$ROOT_DIR/ci/kind/config-3nodes.yml
 
 WAIT_TIMEOUT=15m
-JOB_NAME=job.batch/netpol
 
 echo "===> Creating Kind cluster <==="
 
 kind create cluster --config $KIND_CONFIG
 kind load docker-image projects.registry.vmware.com/antrea/antrea-ubuntu:latest
-kind load docker-image antrea/netpol:latest
 # pre-load the test container image on all the Nodes
-docker pull antrea/netpol-test
-kind load docker-image antrea/netpol-test
-$ROOT_DIR/hack/generate-manifest.sh | kubectl apply -f -
+$ROOT_DIR/hack/generate-manifest.sh  --tun "vxlan" | kubectl apply -f -
 
 echo "===> Creating netpol ClusterRoleBinding, ServiceAccount and Job <==="
 
-kubectl create clusterrolebinding netpol --clusterrole=cluster-admin --serviceaccount=kube-system:netpol
-kubectl create sa netpol -n kube-system
-kubectl create -f $THIS_DIR/install-latest.yml
+$ROOT_DIR/ci/run-k8s-e2e-tests.sh --e2e-network-policy
 
-echo "===> Waiting for netpol Job to complete. This can take a while... <==="
-
-time kubectl wait --for=condition=complete --timeout=$WAIT_TIMEOUT -n kube-system $JOB_NAME
-
-echo "===> Checking netpol results <==="
-
-LOG_FILE=$(mktemp)
-kubectl logs -n kube-system job.batch/netpol > $LOG_FILE
-RESULT_STR=$(grep "TEST FAILURES" $LOG_FILE)
-echo $RESULT_STR
-rc=0
-echo $RESULT_STR | grep "0/" > /dev/null 2>&1 || rc=$?
-if [ $rc -ne 0 ]; then
-    cat $LOG_FILE
-    exit 1
-fi
