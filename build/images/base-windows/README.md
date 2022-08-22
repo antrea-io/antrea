3b7538de85d336724daef5b8b6574cd7b05f4ea9
Antonin Bas <abas@vmware.com>
2022-08-22 16:39:10 -0700

    Support updating the antrea/base-windows image with Github Actions (#4109)
    At the moment, updating this image is a very manual process and requires
    access to a Windows machine. To simplify this process, we add a new
    Github Workflow which can be used to manually trigger a Docker image
    build and push on Github hosted Windows workers.
    
    Ideally, the workflow would not need to be triggered manually and would
    run as part of CI (like we do for Linux base images). However this is a
    good compromise / first step. Even with Docker caching, running
    ./hack/build-antrea-windows-all.sh for every pull request and push event
    may be a bit too expensive. Something that can be investigated later.
    
    Signed-off-by: Antonin Bas <abas@vmware.com>

--- build/images/base-windows/README.md
+++ build/images/base-windows/README.md
@@ -11,15 +11,15 @@ GO_VERSION=$(head -n 1 ../deps/go-version)
 CNI_BINARIES_VERSION=$(head -n 1 ../deps/cni-binaries-version)
 NANOSERVER_VERSION=$(head -n 1 ../deps/nanoserver-version)
 WIN_BUILD_TAG=$(echo $GO_VERSION $CNI_BINARIES_VERSION $NANOSERVER_VERSION| md5sum| head -c 10)
-docker build -t antrea/base-windows:$WIN_BUILD_TAG --network host --build-arg GO_VERSION=$GO_VERSION --build-arg CNI_BINARIES_VERSION=$CNI_BINARIES_VERSION --build-arg NANOSERVER_VERSION=$NANOSERVER_VERSION .
+docker build -t antrea/base-windows:$WIN_BUILD_TAG --build-arg GO_VERSION=$GO_VERSION --build-arg CNI_BINARIES_VERSION=$CNI_BINARIES_VERSION --build-arg NANOSERVER_VERSION=$NANOSERVER_VERSION .
 docker push antrea/base-windows:$WIN_BUILD_TAG
 ```
 
 The `docker push` command will fail if you do not have permission to push to the
 `antrea` Dockerhub repository.
 
-Here is the table of codegen images that have been uploaded:
-
-| Tag                              | Change                                  |
-| :-----------------------------   | --------------------------------------- |
-| a43c77d755                       | The first image based on nanoserver-1809 |
+However, the easiest way to push a new image on Dockerhub is to run the
+`Manually update antrea/base-windows Docker image` Github workflow. Only
+contributors with `write` access to the antrea-io/antrea Github repository can
+trigger the workflow. If you need to update the image, please check with a
+maintainer first.
---
+++ hack/build-antrea-windows-all.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Antrea Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script must be run on a Windows machine and requires a Bash shell to be
+# available, e.g., Git Bash (https://gitforwindows.org/).
+
+set -eo pipefail
+
+function echoerr {
+    >&2 echo "$@"
+}
+
+_usage="Usage: $0 [--pull] [--push-base-images]
+Build the antrea/antrea-windows image, as well as all the base images in the build chain. This is
+typically used in CI to build the image with the latest version of all dependencies, taking into
+account changes to all Dockerfiles.
+        --pull                  Always attempt to pull a newer version of the base images.
+        --push-base-images      Push built images to the registry. Only base images will be pushed.
+This script must run on a Windows machine!"
+
+function print_usage {
+    echoerr "$_usage"
+}
+
+PULL=false
+PUSH=false
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --pull)
+    PULL=true
+    shift
+    ;;
+    --push-base-images)
+    PUSH=true
+    shift
+    ;;
+    -h|--help)
+    print_usage
+    exit 0
+    ;;
+    *)    # unknown option
+    echoerr "Unknown option $1"
+    exit 1
+    ;;
+esac
+done
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd "$THIS_DIR/.." > /dev/null
+
+NANOSERVER_VERSION=$(head -n 1 build/images/deps/nanoserver-version)
+CNI_BINARIES_VERSION=$(head -n 1 build/images/deps/cni-binaries-version)
+GO_VERSION=$(head -n 1 build/images/deps/go-version)
+WIN_BUILD_TAG=$(echo $GO_VERSION $CNI_BINARIES_VERSION $NANOSERVER_VERSION| md5sum| head -c 10)
+
+echo "WIN_BUILD_TAG=$WIN_BUILD_TAG"
+
+if $PULL; then
+    docker pull mcr.microsoft.com/windows/servercore:$NANOSERVER_VERSION
+    docker pull golang:$GO_VERSION-nanoserver
+    docker pull mcr.microsoft.com/windows/nanoserver:$NANOSERVER_VERSION
+    docker pull mcr.microsoft.com/powershell:lts-nanoserver-$NANOSERVER_VERSION
+    docker pull antrea/windows-utility-base:$WIN_BUILD_TAG || true
+    docker pull antrea/windows-golang:$WIN_BUILD_TAG || true
+    docker pull antrea/base-windows:$WIN_BUILD_TAG || true
+fi
+
+cd build/images/base-windows
+docker build --target windows-utility-base \
+       --cache-from antrea/windows-utility-base:$WIN_BUILD_TAG \
+       -t antrea/windows-utility-base:$WIN_BUILD_TAG \
+       --build-arg CNI_BINARIES_VERSION=$CNI_BINARIES_VERSION \
+       --build-arg NANOSERVER_VERSION=$NANOSERVER_VERSION .
+docker build --target windows-golang \
+       --cache-from antrea/windows-golang:$WIN_BUILD_TAG \
+       --build-arg GO_VERSION=$GO_VERSION \
+       --build-arg NANOSERVER_VERSION=$NANOSERVER_VERSION .
+docker build \
+       --cache-from antrea/windows-utility-base:$WIN_BUILD_TAG \
+       --cache-from antrea/windows-golang:$WIN_BUILD_TAG \
+       --cache-from antrea/base-windows:$WIN_BUILD_TAG \
+       -t antrea/base-windows:$WIN_BUILD_TAG \
+       --build-arg GO_VERSION=$GO_VERSION \
+       --build-arg NANOSERVER_VERSION=$NANOSERVER_VERSION .
+cd -
+
+if $PUSH; then
+    docker push antrea/windows-utility-base:$WIN_BUILD_TAG
+    docker push antrea/windows-golang:$WIN_BUILD_TAG
+    docker push antrea/base-windows:$WIN_BUILD_TAG
+fi
+
+export NO_PULL=1
+
+make build-windows
+
+popd > /dev/null
