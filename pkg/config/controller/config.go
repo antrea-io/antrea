0407d4d0e819c5a8f4a7d2c19376dee8b426224f
Quan Tian <qtian@vmware.com>
2022-08-26 10:30:24 +0800

    Fix names of packages in pkg/config (#4153)
    Somehow they were named differently from the base names of their source
    directories, which make code consuming them look very confusing if they
    are not imported with alias. For example, code would look like the
    following:
    
    ```
    import "antrea.io/antrea/pkg/config/agent"
    
    func newOptions() *Options {
    	return &Options{
    		config: &config.AgentConfig{},
    	}
    }
    ```
    
    Signed-off-by: Quan Tian <qtian@vmware.com>

--- pkg/config/controller/config.go
+++ pkg/config/controller/config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package config
+package controller
 
 import (
 	componentbaseconfig "k8s.io/component-base/config"
