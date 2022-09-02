e6f41f75e567960d4faac521286b6620348cfbed
Lan <luola@vmware.com>
2022-09-02 12:45:33 +0800

    Fix antctl ClusterSet output when status is empty (#4174)
    After a ClusterSet is just created in the leader cluster, it might have
    no status yet, then `antctl mc get clustersets -A` will skip print the
    ClusterSet when its status is empty.
    Fix it by passing an empty status and condition.
    
    Signed-off-by: Lan Luo <luola@vmware.com>

--- pkg/antctl/raw/multicluster/get/clusterset.go
+++ pkg/antctl/raw/multicluster/get/clusterset.go
@@ -36,6 +36,7 @@ type clusterSetOptions struct {
 	namespace     string
 	outputFormat  string
 	allNamespaces bool
+	k8sClient     client.Client
 }
 
 var optionsClusterSet *clusterSetOptions
@@ -53,15 +54,23 @@ Get the specified ClusterSet
 $ antctl mc get clusterset <CLUSTERSETID>
 `, "\n")
 
-func (o *clusterSetOptions) validateAndComplete() {
+func (o *clusterSetOptions) validateAndComplete(cmd *cobra.Command) error {
 	if o.allNamespaces {
 		o.namespace = metav1.NamespaceAll
-		return
-	}
-	if o.namespace == "" {
+	} else if o.namespace == "" {
 		o.namespace = metav1.NamespaceDefault
-		return
 	}
+	if o.k8sClient == nil {
+		kubeconfig, err := raw.ResolveKubeconfig(cmd)
+		if err != nil {
+			return err
+		}
+		o.k8sClient, err = client.New(kubeconfig, client.Options{Scheme: multiclusterscheme.Scheme})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func NewClusterSetCommand() *cobra.Command {
@@ -79,42 +88,28 @@ func NewClusterSetCommand() *cobra.Command {
 	optionsClusterSet = o
 	cmdClusterSet.Flags().StringVarP(&o.namespace, "namespace", "n", "", "Namespace of ClusterSets")
 	cmdClusterSet.Flags().StringVarP(&o.outputFormat, "output", "o", "", "Output format. Supported formats: json|yaml")
-	cmdClusterSet.Flags().BoolVarP(&o.allNamespaces, "all-namespaces", "A", false, "If present, list ClusterSets across all namespaces")
+	cmdClusterSet.Flags().BoolVarP(&o.allNamespaces, "all-namespaces", "A", false, "If present, list ClusterSets across all Namespaces")
 
 	return cmdClusterSet
 }
 
 func runEClusterSet(cmd *cobra.Command, args []string) error {
-	optionsClusterSet.validateAndComplete()
-
-	kubeconfig, err := raw.ResolveKubeconfig(cmd)
+	err := optionsClusterSet.validateAndComplete(cmd)
 	if err != nil {
 		return err
 	}
-
-	argsNum := len(args)
-	singleResource := false
-	if argsNum > 0 {
-		singleResource = true
-	}
-
-	k8sClient, err := client.New(kubeconfig, client.Options{Scheme: multiclusterscheme.Scheme})
-	if err != nil {
-		return err
-	}
-
 	var clusterSets []multiclusterv1alpha1.ClusterSet
-	if singleResource {
+	if len(args) > 0 {
 		clusterSetName := args[0]
 		clusterSet := multiclusterv1alpha1.ClusterSet{}
-		err = k8sClient.Get(context.TODO(), types.NamespacedName{
+		err = optionsClusterSet.k8sClient.Get(context.TODO(), types.NamespacedName{
 			Namespace: optionsClusterSet.namespace,
 			Name:      clusterSetName,
 		}, &clusterSet)
 		if err != nil {
 			return err
 		}
-		gvks, unversioned, err := k8sClient.Scheme().ObjectKinds(&clusterSet)
+		gvks, unversioned, err := optionsClusterSet.k8sClient.Scheme().ObjectKinds(&clusterSet)
 		if err != nil {
 			return err
 		}
@@ -124,7 +119,7 @@ func runEClusterSet(cmd *cobra.Command, args []string) error {
 		clusterSets = append(clusterSets, clusterSet)
 	} else {
 		clusterSetList := &multiclusterv1alpha1.ClusterSetList{}
-		err = k8sClient.List(context.TODO(), clusterSetList, &client.ListOptions{Namespace: optionsClusterSet.namespace})
+		err = optionsClusterSet.k8sClient.List(context.TODO(), clusterSetList, &client.ListOptions{Namespace: optionsClusterSet.namespace})
 		if err != nil {
 			return err
 		}
@@ -140,23 +135,9 @@ func runEClusterSet(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	switch optionsClusterSet.outputFormat {
-	case "json", "yaml":
-		err := output(clusterSets, true, optionsClusterSet.outputFormat, clusterset.Transform)
-		if err != nil {
-			return err
-		}
-	default:
-		clusterSetsNum := len(clusterSets)
-		for i, singleclusterset := range clusterSets {
-			err := output(singleclusterset, true, optionsClusterSet.outputFormat, clusterset.Transform)
-			if err != nil {
-				return err
-			}
-			if i != clusterSetsNum-1 {
-				fmt.Print("\n")
-			}
-		}
+	err = output(clusterSets, false, optionsClusterSet.outputFormat, cmd.OutOrStdout(), clusterset.Transform)
+	if err != nil {
+		return err
 	}
 	return nil
 }
