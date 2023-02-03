d4cd0c4f638fdcb4a71b098ed64f64c5679ff83b
wenyingd <wenyingd@vmware.com>
2023-02-03 09:14:32 +0800

    test remove marks

--- pkg/agent/cniserver/server_test.go
+++ pkg/agent/cniserver/server_test.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux || windows
-// +build linux windows
-
 package cniserver
 
 import (
