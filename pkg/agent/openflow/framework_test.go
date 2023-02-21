2f54131dfebd432697b6ecb4522e57537d24448a
Hongliang Liu <75655411+hongliangl@users.noreply.github.com>
2023-02-21 09:52:57 +0800

    Fix that TrafficControlTable is always created by default (#4642)
    For feature NetworkPolicy in flexible pipeline, TrafficControlTable
    should be created when L7 NetworkPolicy is enabled since it is used
    by L7 NetworkPolicy, and TrafficControlTable shouldn't be created by
    default when L7 NetworkPolicy is disabled.
    
    This PR also refines some unit test code.
    
    Signed-off-by: Hongliang Liu <lhongliang@vmware.com>

--- pkg/agent/openflow/framework_test.go
+++ pkg/agent/openflow/framework_test.go
@@ -32,6 +32,70 @@ const (
 	dualStack
 )
 
+var (
+	defaultOptions = clientOptions{
+		enableProxy:           true,
+		enableAntreaPolicy:    true,
+		proxyAll:              false,
+		connectUplinkToBridge: false,
+		enableMulticast:       false,
+		enableTrafficControl:  false,
+		enableMulticluster:    false,
+		enableL7NetworkPolicy: false,
+	}
+)
+
+func newTestFeaturePodConnectivity(ipProtocols []binding.Protocol, options ...clientOptionsFn) *featurePodConnectivity {
+	o := defaultOptions
+	for _, fn := range options {
+		fn(&o)
+	}
+	return &featurePodConnectivity{
+		ipProtocols:           ipProtocols,
+		connectUplinkToBridge: o.connectUplinkToBridge,
+		enableMulticast:       o.enableMulticast,
+		enableTrafficControl:  o.enableTrafficControl,
+		proxyAll:              o.proxyAll,
+	}
+}
+
+func newTestFeatureNetworkPolicy(nodeType config.NodeType, options ...clientOptionsFn) *featureNetworkPolicy {
+	o := defaultOptions
+	for _, fn := range options {
+		fn(&o)
+	}
+	return &featureNetworkPolicy{
+		nodeType:              nodeType,
+		enableMulticast:       o.enableMulticast,
+		enableAntreaPolicy:    o.enableAntreaPolicy,
+		enableL7NetworkPolicy: o.enableL7NetworkPolicy,
+	}
+}
+
+func newTestFeatureService(options ...clientOptionsFn) *featureService {
+	o := defaultOptions
+	for _, fn := range options {
+		fn(&o)
+	}
+	return &featureService{
+		enableAntreaPolicy: o.enableAntreaPolicy,
+		enableProxy:        o.enableProxy,
+		proxyAll:           o.proxyAll,
+	}
+}
+
+func newTestFeatureEgress() *featureEgress {
+	return &featureEgress{}
+}
+
+func newTestFeatureMulticast() *featureMulticast {
+	return &featureMulticast{}
+}
+
+func newTestFeatureExternalNodeConnectivity() *featureExternalNodeConnectivity {
+	return &featureExternalNodeConnectivity{}
+}
+
 func TestBuildPipeline(t *testing.T) {
 	ipStackMap := map[ipStack][]binding.Protocol{
 		ipv4Only:  {binding.ProtocolIP},
@@ -39,17 +103,19 @@ func TestBuildPipeline(t *testing.T) {
 		dualStack: {binding.ProtocolIP, binding.ProtocolIPv6},
 	}
 	for _, tc := range []struct {
+		name           string
 		ipStack        ipStack
 		features       []feature
 		expectedTables map[binding.PipelineID][]*Table
 	}{
 		{
+			name:    "K8s Node, dual stack, with default options",
 			ipStack: dualStack,
 			features: []feature{
-				&featurePodConnectivity{ipProtocols: ipStackMap[dualStack]},
-				&featureNetworkPolicy{enableAntreaPolicy: true},
-				&featureService{enableProxy: true, proxyAll: true},
-				&featureEgress{},
+				newTestFeaturePodConnectivity(ipStackMap[dualStack]),
+				newTestFeatureNetworkPolicy(config.K8sNode),
+				newTestFeatureService(),
+				newTestFeatureEgress(),
 			},
 			expectedTables: map[binding.PipelineID][]*Table{
 				pipelineRoot: {
@@ -63,7 +129,6 @@ func TestBuildPipeline(t *testing.T) {
 					ConntrackTable,
 					ConntrackStateTable,
 					PreRoutingClassifierTable,
-					NodePortMarkTable,
 					SessionAffinityTable,
 					ServiceLBTable,
 					EndpointDNATTable,
@@ -77,7 +142,6 @@ func TestBuildPipeline(t *testing.T) {
 					SNATMarkTable,
 					SNATTable,
 					L2ForwardingCalcTable,
-					TrafficControlTable,
 					AntreaPolicyIngressRuleTable,
 					IngressRuleTable,
 					IngressDefaultTable,
@@ -92,12 +156,13 @@ func TestBuildPipeline(t *testing.T) {
 			},
 		},
 		{
+			name:    "K8s Node, IPv6 only, with default options",
 			ipStack: ipv6Only,
 			features: []feature{
-				&featurePodConnectivity{ipProtocols: ipStackMap[ipv6Only]},
-				&featureNetworkPolicy{enableAntreaPolicy: true},
-				&featureService{enableProxy: true, proxyAll: true},
-				&featureEgress{},
+				newTestFeaturePodConnectivity(ipStackMap[ipv6Only]),
+				newTestFeatureNetworkPolicy(config.K8sNode),
+				newTestFeatureService(),
+				newTestFeatureEgress(),
 			},
 			expectedTables: map[binding.PipelineID][]*Table{
 				pipelineRoot: {
@@ -111,7 +176,134 @@ func TestBuildPipeline(t *testing.T) {
 					ConntrackTable,
 					ConntrackStateTable,
 					PreRoutingClassifierTable,
-					NodePortMarkTable,
+					SessionAffinityTable,
+					ServiceLBTable,
+					EndpointDNATTable,
+					AntreaPolicyEgressRuleTable,
+					EgressRuleTable,
+					EgressDefaultTable,
+					EgressMetricTable,
+					L3ForwardingTable,
+					EgressMarkTable,
+					L3DecTTLTable,
+					SNATMarkTable,
+					SNATTable,
+					L2ForwardingCalcTable,
+					AntreaPolicyIngressRuleTable,
+					IngressRuleTable,
+					IngressDefaultTable,
+					IngressMetricTable,
+					ConntrackCommitTable,
+					L2ForwardingOutTable,
+				},
+			},
+		},
+		{
+			name:    "K8s Node, IPv4 only, with default options",
+			ipStack: ipv6Only,
+			features: []feature{
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only]),
+				newTestFeatureNetworkPolicy(config.K8sNode),
+				newTestFeatureService(),
+				newTestFeatureEgress(),
+			},
+			expectedTables: map[binding.PipelineID][]*Table{
+				pipelineRoot: {
+					PipelineRootClassifierTable,
+				},
+				pipelineIP: {
+					ClassifierTable,
+					SpoofGuardTable,
+					UnSNATTable,
+					ConntrackTable,
+					ConntrackStateTable,
+					PreRoutingClassifierTable,
+					SessionAffinityTable,
+					ServiceLBTable,
+					EndpointDNATTable,
+					AntreaPolicyEgressRuleTable,
+					EgressRuleTable,
+					EgressDefaultTable,
+					EgressMetricTable,
+					L3ForwardingTable,
+					EgressMarkTable,
+					L3DecTTLTable,
+					SNATMarkTable,
+					SNATTable,
+					L2ForwardingCalcTable,
+					AntreaPolicyIngressRuleTable,
+					IngressRuleTable,
+					IngressDefaultTable,
+					IngressMetricTable,
+					ConntrackCommitTable,
+					L2ForwardingOutTable,
+				},
+			},
+		},
+		{
+			name:    "K8s Node, IPv4 only, with TrafficControl and connectUplinkToBridge enabled",
+			ipStack: ipv6Only,
+			features: []feature{
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only], enableTrafficControl, enableConnectUplinkToBridge),
+				newTestFeatureNetworkPolicy(config.K8sNode),
+				newTestFeatureService(),
+				newTestFeatureEgress(),
+			},
+			expectedTables: map[binding.PipelineID][]*Table{
+				pipelineRoot: {
+					PipelineRootClassifierTable,
+				},
+				pipelineIP: {
+					ClassifierTable,
+					SpoofGuardTable,
+					UnSNATTable,
+					ConntrackTable,
+					ConntrackStateTable,
+					PreRoutingClassifierTable,
+					SessionAffinityTable,
+					ServiceLBTable,
+					EndpointDNATTable,
+					AntreaPolicyEgressRuleTable,
+					EgressRuleTable,
+					EgressDefaultTable,
+					EgressMetricTable,
+					L3ForwardingTable,
+					EgressMarkTable,
+					L3DecTTLTable,
+					SNATMarkTable,
+					SNATTable,
+					L2ForwardingCalcTable,
+					TrafficControlTable,
+					AntreaPolicyIngressRuleTable,
+					IngressRuleTable,
+					IngressDefaultTable,
+					IngressMetricTable,
+					ConntrackCommitTable,
+					VLANTable,
+					L2ForwardingOutTable,
+				},
+			},
+		},
+		{
+			name:    "K8s Node, IPv4 only, with L7NetworkPolicy enabled",
+			ipStack: ipv6Only,
+			features: []feature{
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only]),
+				newTestFeatureNetworkPolicy(config.K8sNode, enableL7NetworkPolicy),
+				newTestFeatureService(),
+				newTestFeatureEgress(),
+			},
+			expectedTables: map[binding.PipelineID][]*Table{
+				pipelineRoot: {
+					PipelineRootClassifierTable,
+				},
+				pipelineIP: {
+					ClassifierTable,
+					SpoofGuardTable,
+					UnSNATTable,
+					ConntrackTable,
+					ConntrackStateTable,
+					PreRoutingClassifierTable,
 					SessionAffinityTable,
 					ServiceLBTable,
 					EndpointDNATTable,
@@ -136,12 +328,53 @@ func TestBuildPipeline(t *testing.T) {
 			},
 		},
 		{
+			name:    "K8s Node, IPv4 only, with AntreaPolicy disabled",
+			ipStack: ipv6Only,
+			features: []feature{
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only]),
+				newTestFeatureNetworkPolicy(config.K8sNode, disableAntreaPolicy),
+				newTestFeatureService(),
+				newTestFeatureEgress(),
+			},
+			expectedTables: map[binding.PipelineID][]*Table{
+				pipelineRoot: {
+					PipelineRootClassifierTable,
+				},
+				pipelineIP: {
+					ClassifierTable,
+					SpoofGuardTable,
+					UnSNATTable,
+					ConntrackTable,
+					ConntrackStateTable,
+					PreRoutingClassifierTable,
+					SessionAffinityTable,
+					ServiceLBTable,
+					EndpointDNATTable,
+					EgressRuleTable,
+					EgressDefaultTable,
+					EgressMetricTable,
+					L3ForwardingTable,
+					EgressMarkTable,
+					L3DecTTLTable,
+					SNATMarkTable,
+					SNATTable,
+					L2ForwardingCalcTable,
+					IngressRuleTable,
+					IngressDefaultTable,
+					IngressMetricTable,
+					ConntrackCommitTable,
+					L2ForwardingOutTable,
+				},
+			},
+		},
+		{
+			name:    "K8s Node, IPv4 only, with AntreaProxy disabled",
 			ipStack: ipv4Only,
 			features: []feature{
-				&featurePodConnectivity{ipProtocols: ipStackMap[ipv4Only]},
-				&featureNetworkPolicy{enableAntreaPolicy: true},
-				&featureService{enableProxy: false},
-				&featureEgress{},
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only]),
+				newTestFeatureNetworkPolicy(config.K8sNode),
+				newTestFeatureService(disableProxy),
+				newTestFeatureEgress(),
 			},
 			expectedTables: map[binding.PipelineID][]*Table{
 				pipelineRoot: {
@@ -160,7 +393,6 @@ func TestBuildPipeline(t *testing.T) {
 					L3ForwardingTable,
 					L3DecTTLTable,
 					L2ForwardingCalcTable,
-					TrafficControlTable,
 					AntreaPolicyIngressRuleTable,
 					IngressRuleTable,
 					IngressDefaultTable,
@@ -175,12 +407,13 @@ func TestBuildPipeline(t *testing.T) {
 			},
 		},
 		{
+			name:    "K8s Node, IPv4 only, with proxyAll enabled",
 			ipStack: ipv4Only,
 			features: []feature{
-				&featurePodConnectivity{ipProtocols: ipStackMap[ipv4Only]},
-				&featureNetworkPolicy{enableAntreaPolicy: true},
-				&featureService{enableProxy: true, proxyAll: false},
-				&featureEgress{},
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only]),
+				newTestFeatureNetworkPolicy(config.K8sNode),
+				newTestFeatureService(enableProxyAll),
+				newTestFeatureEgress(),
 			},
 			expectedTables: map[binding.PipelineID][]*Table{
 				pipelineRoot: {
@@ -193,6 +426,7 @@ func TestBuildPipeline(t *testing.T) {
 					ConntrackTable,
 					ConntrackStateTable,
 					PreRoutingClassifierTable,
+					NodePortMarkTable,
 					SessionAffinityTable,
 					ServiceLBTable,
 					EndpointDNATTable,
@@ -206,7 +440,6 @@ func TestBuildPipeline(t *testing.T) {
 					SNATMarkTable,
 					SNATTable,
 					L2ForwardingCalcTable,
-					TrafficControlTable,
 					AntreaPolicyIngressRuleTable,
 					IngressRuleTable,
 					IngressDefaultTable,
@@ -221,12 +454,13 @@ func TestBuildPipeline(t *testing.T) {
 			},
 		},
 		{
+			name:    "K8s Node, IPv4 only, with multicast enabled",
 			ipStack: ipv4Only,
 			features: []feature{
-				&featurePodConnectivity{ipProtocols: ipStackMap[ipv4Only], enableMulticast: true},
-				&featureNetworkPolicy{enableAntreaPolicy: true, enableMulticast: true},
-				&featureService{enableProxy: true, proxyAll: false},
-				&featureMulticast{enableAntreaPolicy: true},
+				newTestFeaturePodConnectivity(ipStackMap[ipv4Only], enableMulticast),
+				newTestFeatureNetworkPolicy(config.K8sNode, enableMulticast),
+				newTestFeatureService(),
+				newTestFeatureMulticast(),
 			},
 			expectedTables: map[binding.PipelineID][]*Table{
 				pipelineRoot: {
@@ -252,7 +486,6 @@ func TestBuildPipeline(t *testing.T) {
 					SNATMarkTable,
 					SNATTable,
 					L2ForwardingCalcTable,
-					TrafficControlTable,
 					AntreaPolicyIngressRuleTable,
 					IngressRuleTable,
 					IngressDefaultTable,
@@ -280,10 +513,10 @@ func TestBuildPipeline(t *testing.T) {
 			},
 		},
 		{
-			ipStack: ipv4Only,
+			name: "External Node, IPv4 only",
 			features: []feature{
-				&featureExternalNodeConnectivity{ipProtocols: ipStackMap[ipv4Only]},
-				&featureNetworkPolicy{enableAntreaPolicy: true, nodeType: config.ExternalNode},
+				newTestFeatureExternalNodeConnectivity(),
+				newTestFeatureNetworkPolicy(config.ExternalNode),
 			},
 			expectedTables: map[binding.PipelineID][]*Table{
 				pipelineRoot: {
@@ -312,39 +545,41 @@ func TestBuildPipeline(t *testing.T) {
 			},
 		},
 	} {
-		pipelineRequiredTablesMap := make(map[binding.PipelineID]map[*Table]struct{})
-		for pipelineID := range tc.expectedTables {
-			pipelineRequiredTablesMap[pipelineID] = make(map[*Table]struct{})
-		}
-		pipelineRequiredTablesMap[pipelineRoot][PipelineRootClassifierTable] = struct{}{}
-		for _, f := range tc.features {
-			for _, table := range f.getRequiredTables() {
-				if _, ok := pipelineRequiredTablesMap[table.pipeline]; ok {
-					pipelineRequiredTablesMap[table.pipeline][table] = struct{}{}
+		t.Run(tc.name, func(t *testing.T) {
+			pipelineRequiredTablesMap := make(map[binding.PipelineID]map[*Table]struct{})
+			for pipelineID := range tc.expectedTables {
+				pipelineRequiredTablesMap[pipelineID] = make(map[*Table]struct{})
+			}
+			pipelineRequiredTablesMap[pipelineRoot][PipelineRootClassifierTable] = struct{}{}
+			for _, f := range tc.features {
+				for _, table := range f.getRequiredTables() {
+					if _, ok := pipelineRequiredTablesMap[table.pipeline]; ok {
+						pipelineRequiredTablesMap[table.pipeline][table] = struct{}{}
+					}
 				}
 			}
-		}
 
-		for pipelineID := firstPipeline; pipelineID <= lastPipeline; pipelineID++ {
-			if _, ok := pipelineRequiredTablesMap[pipelineID]; !ok {
-				continue
-			}
-			var requiredTables []*Table
-			for _, table := range tableOrderCache[pipelineID] {
-				if _, ok := pipelineRequiredTablesMap[pipelineID][table]; ok {
-					requiredTables = append(requiredTables, table)
+			for pipelineID := firstPipeline; pipelineID <= lastPipeline; pipelineID++ {
+				if _, ok := pipelineRequiredTablesMap[pipelineID]; !ok {
+					continue
 				}
-			}
-			generatePipeline(pipelineID, requiredTables)
+				var requiredTables []*Table
+				for _, table := range tableOrderCache[pipelineID] {
+					if _, ok := pipelineRequiredTablesMap[pipelineID][table]; ok {
+						requiredTables = append(requiredTables, table)
+					}
+				}
+				generatePipeline(pipelineID, requiredTables)
 
-			tables := tc.expectedTables[pipelineID]
-			for i := 0; i < len(tables)-1; i++ {
-				require.NotNil(t, tables[i].ofTable, "table %q should be initialized", tables[i].name)
-				require.Less(t, tables[i].GetID(), tables[i+1].GetID(), fmt.Sprintf("id of table %q should less than that of table %q", tables[i].GetName(), tables[i+1].GetName()))
+				tables := tc.expectedTables[pipelineID]
+				for i := 0; i < len(tables)-1; i++ {
+					require.NotNil(t, tables[i].ofTable, "table %q should be initialized", tables[i].name)
+					require.Less(t, tables[i].GetID(), tables[i+1].GetID(), fmt.Sprintf("id of table %q should less than that of table %q", tables[i].GetName(), tables[i+1].GetName()))
+				}
+				require.NotNil(t, tables[len(tables)-1].ofTable, "table %q should be initialized", tables[len(tables)-1].name)
 			}
-			require.NotNil(t, tables[len(tables)-1].ofTable, "table %q should be initialized", tables[len(tables)-1].name)
-		}
-		resetPipelines()
+			resetPipelines()
+		})
 	}
 }
 
