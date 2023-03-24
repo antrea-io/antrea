28cb9249c8098a3bd0305feeae162b420a696f4d
Lan <luola@vmware.com>
2023-03-24 14:56:39 +0800

    Remove config for Endpoints API when feature gate EndpointSlice is enabled (#4752)
    For #4681
    
    Signed-off-by: Hongliang Liu <lhongliang@vmware.com>

--- pkg/agent/proxy/proxier.go
+++ pkg/agent/proxy/proxier.go
@@ -940,7 +940,6 @@ func NewProxier(
 	}
 
 	p := &proxier{
-		endpointsConfig:           config.NewEndpointsConfig(informerFactory.Core().V1().Endpoints(), resyncPeriod),
 		serviceConfig:             config.NewServiceConfig(informerFactory.Core().V1().Services(), resyncPeriod),
 		endpointsChanges:          newEndpointsChangesTracker(hostname, endpointSliceEnabled, isIPv6),
 		serviceChanges:            newServiceChangesTracker(recorder, ipFamily, skipServices),
@@ -966,7 +965,6 @@ func NewProxier(
 	}
 
 	p.serviceConfig.RegisterEventHandler(p)
-	p.endpointsConfig.RegisterEventHandler(p)
 	p.runner = k8sproxy.NewBoundedFrequencyRunner(componentName, p.syncProxyRules, time.Second, 30*time.Second, 2)
 	if endpointSliceEnabled {
 		p.endpointSliceConfig = config.NewEndpointSliceConfig(informerFactory.Discovery().V1().EndpointSlices(), resyncPeriod)
