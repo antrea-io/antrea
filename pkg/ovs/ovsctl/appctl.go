cf78a58de1760f8bb839d0fe3bc1797bf7ca2b84
Antonin Bas <abas@vmware.com>
2023-07-26 15:04:32 -0700

    Stop using /bin/sh for OVS commands
    We are currently using the shell (/bin/sh) to run ovs-appctl and
    ovs-ofctl commands, but there does not seem to be a reason to do so.
    
    Instead, we can invoke the binary directly with exec.Command and pass
    the arguments as a slice.
    
    Before this change, ovsVSwitchdUDS would return
    "/var/run/openvswitch/ovs-vswitchd.*.ctl" as a fallback value in case
    the PID of ovs-vswitchd could not be determined. The shell would then
    expand the wildcard patterns. I now believe that it is better to return
    an error in that case. We want to make sure that we use a valid file
    always (and returning the glob pattern would cause an error if multiple
    files match). If we run into issues because of this approach, we can
    update the code to expand the glob pattern programmatically and, if a
    single path matches, we can return that path.
    
    Signed-off-by: Antonin Bas <abas@vmware.com>

--- pkg/ovs/ovsctl/appctl.go
+++ pkg/ovs/ovsctl/appctl.go
@@ -15,28 +15,31 @@
 package ovsctl
 
 import (
+	"context"
 	"fmt"
-	"strings"
+	"os/exec"
 )
 
 type ovsAppctlRunner struct {
 	bridge string
 }
 
-func (r *ovsAppctlRunner) RunAppctlCmd(cmd string, needsBridge bool, args ...string) ([]byte, *ExecError) {
+func (r *ovsAppctlRunner) RunAppctlCmd(cmd string, needsBridge bool, args ...string) ([]byte, error) {
 	// Use the control UNIX domain socket to connect to ovs-vswitchd, as Agent can
 	// run in a different PID namespace from ovs-vswitchd. Relying on ovs-appctl to
 	// determine the control socket based on the pidfile will then give a "stale
 	// pidfile" error, as it tries to validate that the PID read from the pidfile
 	// corresponds to a valid process in the current PID namespace.
-	var cmdStr string
-	if needsBridge {
-		cmdStr = fmt.Sprintf("ovs-appctl -t %s %s %s", ovsVSwitchdUDS(), cmd, r.bridge)
-	} else {
-		cmdStr = fmt.Sprintf("ovs-appctl -t %s %s", ovsVSwitchdUDS(), cmd)
+	uds, err := ovsVSwitchdUDS(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get UDS for OVS: %w", err)
 	}
-	cmdStr = cmdStr + " " + strings.Join(args, " ")
-	out, err := getOVSCommand(cmdStr).CombinedOutput()
+	cmdArgs := []string{"-t", uds, cmd}
+	if needsBridge {
+		cmdArgs = append(cmdArgs, r.bridge)
+	}
+	ovsCmd := exec.CommandContext(context.TODO(), "ovs-apptcl", cmdArgs...)
+	out, err := ovsCmd.CombinedOutput()
 	if err != nil {
 		return nil, newExecError(err, string(out))
 	}
