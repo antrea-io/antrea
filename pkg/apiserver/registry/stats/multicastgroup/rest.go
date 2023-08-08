ab42866f72acb37ee1003306fbae66c26ab26912
ceclinux <src655@gmail.com>
2023-08-08 19:08:02 +0800

    Remove NetworkPolicyStats dependency of MulticastGroup API
    Fixes #5329
    
    Signed-off-by: ceclinux <src655@gmail.com>

--- pkg/apiserver/registry/stats/multicastgroup/rest.go
+++ pkg/apiserver/registry/stats/multicastgroup/rest.go
@@ -69,9 +69,6 @@ func (r *REST) NewList() runtime.Object {
 }
 
 func (r *REST) List(ctx context.Context, options *internalversion.ListOptions) (runtime.Object, error) {
-	if !features.DefaultFeatureGate.Enabled(features.NetworkPolicyStats) {
-		return &statsv1alpha1.MulticastGroupList{}, nil
-	}
 	if !features.DefaultFeatureGate.Enabled(features.Multicast) {
 		return &statsv1alpha1.MulticastGroupList{}, nil
 	}
@@ -82,9 +79,6 @@ func (r *REST) List(ctx context.Context, options *internalversion.ListOptions) (
 }
 
 func (r *REST) Get(ctx context.Context, name string, options *metav1.GetOptions) (runtime.Object, error) {
-	if !features.DefaultFeatureGate.Enabled(features.NetworkPolicyStats) {
-		return &statsv1alpha1.MulticastGroup{}, nil
-	}
 	if !features.DefaultFeatureGate.Enabled(features.Multicast) {
 		return &statsv1alpha1.MulticastGroup{}, nil
 	}
