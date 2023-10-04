719dfeff4ebbf55a1f2866e25892af34ec0625e7
pulkit_jain <42600268+jainpulkit22@users.noreply.github.com>
2023-10-04 23:12:38 +0530

    Rename skip flag in custom Test Options (#5544)
    Fixes #5525
    
    Go has an inbuilt skip flag with go test options so we cannot use a
    flag with same name in our custom test options.
    
    Signed-off-by: Pulkit Jain <jainpu@vmware.com>

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -83,7 +83,7 @@ func testMain(m *testing.M) int {
 	flag.BoolVar(&testOptions.flowVisibility, "flow-visibility", false, "Run flow visibility tests")
 	flag.BoolVar(&testOptions.deployAntrea, "deploy-antrea", true, "Deploy Antrea before running tests")
 	flag.StringVar(&testOptions.coverageDir, "coverage-dir", "", "Directory for coverage data files")
-	flag.StringVar(&testOptions.skipCases, "skip", "", "Key words to skip cases")
+	flag.StringVar(&testOptions.skipCases, "skip-cases", "", "Key words to skip cases")
 	flag.StringVar(&testOptions.linuxVMs, "linuxVMs", "", "hostname of Linux VMs")
 	flag.StringVar(&testOptions.windowsVMs, "windowsVMs", "", "hostname of Windows VMs")
 	flag.Parse()
