7e99115ee8f619c68558e8cc3222fd0b0e6f30ad
Hang Yan <yhang@vmware.com>
2023-11-07 18:55:06 +0800

    update
    Signed-off-by: Hang Yan <yhang@vmware.com>

--- pkg/controller/packetsampling/controller.go
+++ pkg/controller/packetsampling/controller.go
@@ -1,16 +1,19 @@
 package packetsampling
 
 import (
+	"sync"
+	"time"
+
 	crdv1alpha1 "antrea.io/antrea/pkg/apis/crd/v1alpha1"
+	"k8s.io/apimachinery/pkg/util/wait"
 	coreinformers "k8s.io/client-go/informers/core/v1"
 	corelisters "k8s.io/client-go/listers/core/v1"
-	"time"
+	"k8s.io/klog"
 
 	crdinformers "antrea.io/antrea/pkg/client/informers/externalversions/crd/v1alpha1"
 	crdlisters "antrea.io/antrea/pkg/client/listers/crd/v1alpha1"
 	"k8s.io/client-go/tools/cache"
 
-	"antrea.io/antrea/pkg/client/clientset/versioned"
 	"k8s.io/client-go/util/workqueue"
 )
 
@@ -42,6 +45,9 @@ type Controller struct {
 	packetSamplingListerSynced cache.InformerSynced
 
 	queue workqueue.RateLimitingInterface
+
+	runningPacketSamplingMutex sync.Mutex
+	runningPacketSamplings     map[string]string
 }
 
 func NewPacketSamplingController(client versiond.Interface, podInformer coreinformers.PodInformer, packetSamplingInformer crdinformers.PacketSamplingInformer) *Controller {
@@ -56,4 +62,67 @@ func NewPacketSamplingController(client versiond.Interface, podInformer coreinfo
 	}
 
 	// add handlers
+	packetSamplingInformer.Informer().AddEventHandlerWithResyncPeriod(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc:    c.addPacketSampling,
+			UpdateFunc: c.updatePacketSampling,
+			DeleteFunc: c.deletePacketSampling,
+		},
+	)
+}
+
+func (c *Controller) enqueuePacketSampling(ps *crdv1alpha1.PacketSampling) {
+	c.queue.Add(ps.Name)
+}
+
+func (c *Controller) Run(stopCh <-chan struct{}) {
+	defer c.queue.ShutDown()
+	klog.Infof("Starting %s", controllerName)
+	defer klog.Infof("Shutting down %s", controllerName)
+
+	if !cache.WaitForNamedCacheSync(controllerName, stopCh, c.packetSamplingListerSynced) {
+		return
+	}
+
+	for i := 0; i < defaultWorkers; i++ {
+		go wait.Until(c.worker, time.Second, stopCh)
+	}
+
+}
+
+func (c *Controller) worker() {
+	for c.processPacketSamplingItem() {
+
+	}
+}
+
+func (c *Controller) repostPacketSampling() {
+	c.runningPacketSamplingMutex.Lock()
+
+	pss := make([]string, 0, len(c.runningPacketSamplings))
+	for _, psName := range c.runningPacketSamplings {
+		pss = append(pss, psName)
+	}
+	c.runningPacketSamplingMutex.Unlock()
+
+	for _, psName := range pss {
+		c.queue.Add(psName)
+	}
+
+}
+
+func (c *Controller) processPacketSamplingItem() bool {
+	obj, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+
+	defer c.queue.Done(obj)
+
+	key, ok := obj.(string)
+	if !ok {
+		c.queue.Forget(obj)
+		klog.Errorf("Expected string in work queue but got %#v", obj)
+		return true
+	}
 }
