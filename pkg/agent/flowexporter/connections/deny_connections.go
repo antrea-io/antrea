d6766ccab27e2297298f0e549909d160f92e733c
Yun-Tang Hsu <59460118+yuntanghsu@users.noreply.github.com>
2023-11-14 10:23:46 -0800

    Remove redundant log in fillPodInfo/fillServiceInfo and update deny connection store. (#5592)
    1. Remove redundant logs in fillPodInfo/fillServiceInfo to avoid Flow
       Exporter from flooding logs.
    2. Add Mark field for deny connections. We were missing this field
       previously, resulting in trying to fill service information for
       non-service IPs.
    3. Update the DestinationServiceAddress for deny connections when we can
       find this information in PacketIn.
    4. Update e2e/unit tests to verify that the Service-relateds field are
       filled correctly.
    
    Fixes #5573
    
    Signed-off-by: Yun-Tang Hsu <hsuy@vmware.com>

--- pkg/agent/flowexporter/connections/deny_connections.go
+++ pkg/agent/flowexporter/connections/deny_connections.go
@@ -23,6 +23,7 @@ import (
 	"antrea.io/antrea/pkg/agent/flowexporter"
 	"antrea.io/antrea/pkg/agent/flowexporter/priorityqueue"
 	"antrea.io/antrea/pkg/agent/metrics"
+	"antrea.io/antrea/pkg/agent/openflow"
 	"antrea.io/antrea/pkg/agent/proxy"
 	"antrea.io/antrea/pkg/util/ip"
 	"antrea.io/antrea/pkg/util/podstore"
@@ -96,9 +97,17 @@ func (ds *DenyConnectionStore) AddOrUpdateConn(conn *flowexporter.Connection, ti
 		conn.OriginalBytes = bytes
 		conn.OriginalPackets = uint64(1)
 		ds.fillPodInfo(conn)
+		if conn.SourcePodName == "" && conn.DestinationPodName == "" {
+			// We don't add connections to connection map or expirePriorityQueue if we can't find the pod
+			// information for both srcPod and dstPod
+			klog.V(5).InfoS("Skip this connection as we cannot map any of the connection IPs to a local Pod", "srcIP", conn.FlowKey.SourceAddress.String(), "dstIP", conn.FlowKey.DestinationAddress.String())
+			return
+		}
 		protocolStr := ip.IPProtocolNumberToString(conn.FlowKey.Protocol, "UnknownProtocol")
 		serviceStr := fmt.Sprintf("%s:%d/%s", conn.DestinationServiceAddress, conn.DestinationServicePort, protocolStr)
-		ds.fillServiceInfo(conn, serviceStr)
+		if conn.Mark&openflow.ServiceCTMark.GetRange().ToNXRange().ToUint32Mask() == openflow.ServiceCTMark.GetValue() {
+			ds.fillServiceInfo(conn, serviceStr)
+		}
 		metrics.TotalDenyConnections.Inc()
 		conn.IsActive = true
 		ds.connections[connKey] = conn
