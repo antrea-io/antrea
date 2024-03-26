antrea-io/antrea

commit 2aafb2e6c0b8765fedf463e3b7d3a3a5294e4cd6

Upgrade k8s libraries to v0.29.2 (#5843)
1. Upgrade k8s libraries to v0.29.2
2. Upgrade controller-runtime to v0.16.3

Signed-off-by: hjiajing <hjiajing@vmware.com>

multicluster/cmd/multicluster-controller/controller.go
@@ -35,8 +35,10 @@ import (
 	aggregatorclientset "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	controllerruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	k8smcsv1alpha1 "sigs.k8s.io/mcs-api/pkg/apis/v1alpha1"
 
 	mcv1alpha1 "antrea.io/antrea/multicluster/apis/multicluster/v1alpha1"
@@ -148,7 +150,7 @@ func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, 
 
 	secureServing := genericoptions.NewSecureServingOptions().WithLoopback()
 	caCertController, err := certificate.ApplyServerCert(o.SelfSignedCert, client, aggregatorClient, apiExtensionClient,
-		secureServing, getCaConfig(isLeader, o.options.Namespace))
+		secureServing, getCaConfig(isLeader, o.Namespace))
 	if err != nil {
 		return nil, fmt.Errorf("error applying server cert: %v", err)
 	}
@@ -157,25 +159,31 @@ func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, 
 	}
 
 	if o.SelfSignedCert {
-		o.options.CertDir = selfSignedCertDir
+		o.options.Metrics.CertDir = selfSignedCertDir
+		o.WebhookConfig.CertDir = selfSignedCertDir
 	} else {
-		o.options.CertDir = certDir
+		o.options.Metrics.CertDir = certDir
+		o.WebhookConfig.CertDir = certDir
 	}
+	o.options.WebhookServer = webhook.NewServer(webhook.Options{
+		Port:    *o.WebhookConfig.Port,
+		Host:    o.WebhookConfig.Host,
+		CertDir: o.WebhookConfig.CertDir,
+	})
 
 	namespaceFieldSelector := fields.SelectorFromSet(fields.Set{"metadata.namespace": env.GetPodNamespace()})
-	o.options.NewCache = cache.BuilderWithOptions(cache.Options{
-		SelectorsByObject: cache.SelectorsByObject{
-			&mcv1alpha1.Gateway{}: {
-				Field: namespaceFieldSelector,
-			},
-			&mcv1alpha2.ClusterSet{}: {
-				Field: namespaceFieldSelector,
-			},
-			&mcv1alpha1.MemberClusterAnnounce{}: {
-				Field: namespaceFieldSelector,
-			},
+	o.options.Cache.DefaultFieldSelector = namespaceFieldSelector
+	o.options.Cache.ByObject = map[controllerruntimeclient.Object]cache.ByObject{
+		&mcv1alpha1.Gateway{}: {
+			Field: namespaceFieldSelector,
 		},
-	})
+		&mcv1alpha2.ClusterSet{}: {
+			Field: namespaceFieldSelector,
+		},
+		&mcv1alpha1.MemberClusterAnnounce{}: {
+			Field: namespaceFieldSelector,
+		},
+	}
 
 	// EndpointSlice is enabled in AntreaProxy by default since v1.11, so Antrea MC
 	// will use EndpointSlice API by default to keep consistent with AntreaProxy.
@@ -198,9 +206,13 @@ func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, 
 	}
 	o.ClusterCalimCRDAvailable = clusterClaimCRDAvailable
 
-	mgr, err := ctrl.NewManager(k8sConfig, o.options)
+	mgr, err := ctrl.NewManager(k8sConfig, manager.Options{
+		Scheme:                 o.options.Scheme,
+		Metrics:                o.options.Metrics,
+		HealthProbeBindAddress: o.options.HealthProbeBindAddress,
+	})
 	if err != nil {
-		return nil, fmt.Errorf("error starting manager: %v", err)
+		return nil, fmt.Errorf("error creating manager: %v", err)
 	}
 
 	//+kubebuilder:scaffold:builder

multicluster/cmd/multicluster-controller/leader.go
@@ -52,7 +52,7 @@ func newLeaderCommand() *cobra.Command {
 func runLeader(o *Options) error {
 	// on the leader we want the reconciler to run for a given Namespace instead of cluster scope
 	podNamespace := env.GetPodNamespace()
-	o.options.Namespace = podNamespace
+	o.Namespace = podNamespace
 	stopCh := signals.RegisterSignalHandlers()
 
 	mgr, err := setupManagerAndCertControllerFunc(true, o)

multicluster/cmd/multicluster-controller/options.go
@@ -17,10 +17,15 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 
 	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	mcsv1alpha1 "antrea.io/antrea/multicluster/apis/multicluster/v1alpha1"
 	"antrea.io/antrea/multicluster/controllers/multicluster/common"
@@ -31,6 +36,7 @@ type Options struct {
 	configFile     string
 	SelfSignedCert bool
 	options        ctrl.Options
+	Namespace      string
 	// The Service ClusterIP range used in the member cluster.
 	ServiceCIDR string
 	// PodCIDRs is the Pod IP address CIDRs of the member cluster.
@@ -49,6 +55,8 @@ type Options struct {
 	// ClusterCalimCRDAvailable indicates if the ClusterClaim CRD is available or not
 	// in the cluster.
 	ClusterCalimCRDAvailable bool
+	// WebhookConfig contains the controllers webhook configuration
+	WebhookConfig mcsv1alpha1.ControllerWebhook
 }
 
 func newOptions() *Options {
@@ -64,10 +72,8 @@ func (o *Options) complete(args []string) error {
 	ctrlConfig := &mcsv1alpha1.MultiClusterConfig{}
 	if len(o.configFile) > 0 {
 		klog.InfoS("Loading config", "file", o.configFile)
-		options, err = options.AndFrom(ctrl.ConfigFile().AtPath(o.configFile).OfKind(ctrlConfig))
-		if err != nil {
-			klog.ErrorS(err, "Failed to load options")
-			return fmt.Errorf("failed to load options from configuration file %s", o.configFile)
+		if err = o.loadConfigFromFile(ctrlConfig); err != nil {
+			return err
 		}
 		o.options = options
 		if ctrlConfig.ServiceCIDR != "" {
@@ -87,6 +93,7 @@ func (o *Options) complete(args []string) error {
 		o.ServiceCIDR = ctrlConfig.ServiceCIDR
 		o.PodCIDRs = cidrs
 		o.GatewayIPPrecedence = ctrlConfig.GatewayIPPrecedence
+		o.WebhookConfig = ctrlConfig.Webhook
 		if ctrlConfig.EndpointIPType == "" {
 			o.EndpointIPType = common.EndpointIPTypeClusterIP
 		} else {
@@ -110,10 +117,33 @@ func (o *Options) addFlags(fs *pflag.FlagSet) {
 
 func (o *Options) setDefaults() {
 	o.options = ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     "0",
-		Port:                   9443,
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: "0",
+		},
 		HealthProbeBindAddress: ":8080",
-		LeaderElection:         false,
 	}
+}
+
+func (o *Options) loadConfigFromFile(multiclusterConfig *mcsv1alpha1.MultiClusterConfig) error {
+	data, err := os.ReadFile(o.configFile)
+	if err != nil {
+		return err
+	}
+	codecs := serializer.NewCodecFactory(scheme)
+	if err := yaml.Unmarshal(data, multiclusterConfig); err != nil {
+		return err
+	}
+	if err = runtime.DecodeInto(codecs.UniversalDecoder(), data, multiclusterConfig); err != nil {
+		return err
+	}
+
+	if multiclusterConfig.Metrics.BindAddress != "" {
+		o.options.Metrics.BindAddress = multiclusterConfig.Metrics.BindAddress
+	}
+	if multiclusterConfig.Health.HealthProbeBindAddress != "" {
+		o.options.HealthProbeBindAddress = multiclusterConfig.Health.HealthProbeBindAddress
+	}
+
+	return nil
 }

cmd/antrea-agent/options.go
@@ -27,7 +27,7 @@ import (
 	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/featuregate"
 	"k8s.io/klog/v2"
-	"k8s.io/utils/pointer"
+	"k8s.io/utils/ptr"
 
 	"antrea.io/antrea/pkg/agent/config"
 	"antrea.io/antrea/pkg/apis"
@@ -411,10 +411,10 @@ func (o *Options) setK8sNodeDefaultOptions() {
 		o.config.HostProcPathPrefix = defaultHostProcPathPrefix
 	}
 	if o.config.AntreaProxy.Enable == nil {
-		o.config.AntreaProxy.Enable = pointer.Bool(true)
+		o.config.AntreaProxy.Enable = ptr.To(true)
 	}
 	if o.config.AntreaProxy.ProxyLoadBalancerIPs == nil {
-		o.config.AntreaProxy.ProxyLoadBalancerIPs = pointer.Bool(true)
+		o.config.AntreaProxy.ProxyLoadBalancerIPs = ptr.To(true)
 	}
 	if o.config.ServiceCIDR == "" {
 		//It's okay to set the default value of this field even when AntreaProxy is enabled and the field is not used.
@@ -427,7 +427,7 @@ func (o *Options) setK8sNodeDefaultOptions() {
 		o.config.ClusterMembershipPort = apis.AntreaAgentClusterMembershipPort
 	}
 	if o.config.EnablePrometheusMetrics == nil {
-		o.config.EnablePrometheusMetrics = pointer.Bool(true)
+		o.config.EnablePrometheusMetrics = ptr.To(true)
 	}
 	if o.config.WireGuard.Port == 0 {
 		o.config.WireGuard.Port = apis.WireGuardListenPort

cmd/antrea-agent/options_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	featuregatetesting "k8s.io/component-base/featuregate/testing"
-	"k8s.io/utils/pointer"
+	"k8s.io/utils/ptr"
 
 	"antrea.io/antrea/pkg/agent/config"
 	agentconfig "antrea.io/antrea/pkg/config/agent"
@@ -93,7 +93,7 @@ func TestOptionsValidateAntreaProxyConfig(t *testing.T) {
 			name:             "default",
 			trafficEncapMode: config.TrafficEncapModeEncap,
 			antreaProxyConfig: agentconfig.AntreaProxyConfig{
-				Enable:                  pointer.Bool(true),
+				Enable:                  ptr.To(true),
 				DefaultLoadBalancerMode: config.LoadBalancerModeNAT.String(),
 			},
 			expectedDefaultLoadBalancerMode: config.LoadBalancerModeNAT,
@@ -103,7 +103,7 @@ func TestOptionsValidateAntreaProxyConfig(t *testing.T) {
 			enabledDSR:       true,
 			trafficEncapMode: config.TrafficEncapModeEncap,
 			antreaProxyConfig: agentconfig.AntreaProxyConfig{
-				Enable:                  pointer.Bool(true),
+				Enable:                  ptr.To(true),
 				DefaultLoadBalancerMode: config.LoadBalancerModeDSR.String(),
 			},
 			expectedDefaultLoadBalancerMode: config.LoadBalancerModeDSR,
@@ -111,7 +111,7 @@ func TestOptionsValidateAntreaProxyConfig(t *testing.T) {
 		{
 			name: "LoadBalancerModeDSR disabled",
 			antreaProxyConfig: agentconfig.AntreaProxyConfig{
-				Enable:                  pointer.Bool(true),
+				Enable:                  ptr.To(true),
 				DefaultLoadBalancerMode: config.LoadBalancerModeDSR.String(),
 			},
 			trafficEncapMode: config.TrafficEncapModeEncap,
@@ -121,7 +121,7 @@ func TestOptionsValidateAntreaProxyConfig(t *testing.T) {
 			name:       "unsupported encap mode",
 			enabledDSR: true,
 			antreaProxyConfig: agentconfig.AntreaProxyConfig{
-				Enable:                  pointer.Bool(true),
+				Enable:                  ptr.To(true),
 				DefaultLoadBalancerMode: config.LoadBalancerModeDSR.String(),
 			},
 			trafficEncapMode: config.TrafficEncapModeNoEncap,
@@ -131,7 +131,7 @@ func TestOptionsValidateAntreaProxyConfig(t *testing.T) {
 			name:             "invalid LoadBalancerMode",
 			trafficEncapMode: config.TrafficEncapModeEncap,
 			antreaProxyConfig: agentconfig.AntreaProxyConfig{
-				Enable:                  pointer.Bool(true),
+				Enable:                  ptr.To(true),
 				DefaultLoadBalancerMode: "drs",
 			},
 			expectedErr: "LoadBalancerMode drs is unknown",

cmd/antrea-controller/controller.go
@@ -542,6 +542,9 @@ func createAPIServerConfig(kubeconfig string,
 	serverConfig.OpenAPIConfig = genericapiserver.DefaultOpenAPIConfig(
 		openapi.GetOpenAPIDefinitions,
 		genericopenapi.NewDefinitionNamer(apiserver.Scheme))
+	serverConfig.OpenAPIV3Config = genericapiserver.DefaultOpenAPIV3Config(
+		openapi.GetOpenAPIDefinitions,
+		genericopenapi.NewDefinitionNamer(apiserver.Scheme))
 	serverConfig.OpenAPIConfig.Info.Title = "Antrea"
 	serverConfig.EnableMetrics = enableMetrics
 	serverConfig.MinRequestTimeout = int(serverMinWatchTimeout.Seconds())

pkg/agent/controller/trafficcontrol/controller.go
@@ -15,6 +15,7 @@
 package trafficcontrol
 
 import (
+	"context"
 	"crypto/sha1" // #nosec G505: not used for security purposes
 	"encoding/binary"
 	"encoding/hex"
@@ -589,16 +590,17 @@ func (c *Controller) createOVSInternalPort(portName string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	if pollErr := wait.PollImmediate(time.Second, 5*time.Second, func() (bool, error) {
-		_, _, err := util.SetLinkUp(portName)
-		if err == nil {
-			return true, nil
-		}
-		if _, ok := err.(util.LinkNotFound); ok {
-			return false, nil
-		}
-		return false, err
-	}); pollErr != nil {
+	if pollErr := wait.PollUntilContextTimeout(context.TODO(), time.Second, 5*time.Second, true,
+		func(ctx context.Context) (bool, error) {
+			_, _, err := util.SetLinkUp(portName)
+			if err == nil {
+				return true, nil
+			}
+			if _, ok := err.(util.LinkNotFound); ok {
+				return false, nil
+			}
+			return false, err
+		}); pollErr != nil {
 		return "", pollErr
 	}
 	return portUUID, nil

pkg/controller/grouping/controller.go
@@ -15,6 +15,7 @@
 package grouping
 
 import (
+	"context"
 	"fmt"
 	"sync/atomic"
 	"time"
@@ -184,7 +185,7 @@ func (c *GroupEntityController) Run(stopCh <-chan struct{}) {
 	}
 
 	// Wait until all event handlers process the initial resources before setting groupEntityIndex as synced.
-	if err := wait.PollImmediateUntil(100*time.Millisecond, func() (done bool, err error) {
+	if err := wait.PollUntilContextCancel(wait.ContextForChannel(stopCh), 100*time.Millisecond, true, func(ctx context.Context) (done bool, err error) {
 		if uint64(initialPodCount) > c.podAddEvents.Load() {
 			return false, nil
 		}
@@ -197,7 +198,7 @@ func (c *GroupEntityController) Run(stopCh <-chan struct{}) {
 			}
 		}
 		return true, nil
-	}, stopCh); err == nil {
+	}); err == nil {
 		c.groupEntityIndex.setSynced(true)
 	}
 

commit f7e82602a6d7c9588134d9555adb46d54be69122

[Multicluster] Fix cache options for controller Manager
A few issues were introduced by #5843 because of changes in the
sigs.k8s.io/controller-runtime interface.

The biggest issue was that the call to ctrl.NewManager was not using the
Options object populated earlier in the setupManagerAndCertController
function. Instead it was creating and using a new, incomplete Options
object.

Fixes #6149

Signed-off-by: Antonin Bas <antonin.bas@broadcom.com>

multicluster/cmd/multicluster-controller/controller.go
@@ -25,7 +25,6 @@ import (
 	apiextensionclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	genericoptions "k8s.io/apiserver/pkg/server/options"
 	clientset "k8s.io/client-go/kubernetes"
@@ -139,6 +138,15 @@ func getWebhookLabel(isLeader bool, controllerNs string) *metav1.LabelSelector {
 func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, error) {
 	ctrl.SetLogger(klog.NewKlogr())
 
+	podNamespace := env.GetPodNamespace()
+
+	var caConfig *certificate.CAConfig
+	if isLeader {
+		caConfig = getCaConfig(isLeader, podNamespace)
+	} else {
+		caConfig = getCaConfig(isLeader, "")
+	}
+
 	// build up cert controller to manage certificate for MC Controller
 	k8sConfig := ctrl.GetConfigOrDie()
 	k8sConfig.QPS = common.ResourceExchangeQPS
@@ -149,8 +157,7 @@ func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, 
 	}
 
 	secureServing := genericoptions.NewSecureServingOptions().WithLoopback()
-	caCertController, err := certificate.ApplyServerCert(o.SelfSignedCert, client, aggregatorClient, apiExtensionClient,
-		secureServing, getCaConfig(isLeader, o.Namespace))
+	caCertController, err := certificate.ApplyServerCert(o.SelfSignedCert, client, aggregatorClient, apiExtensionClient, secureServing, caConfig)
 	if err != nil {
 		return nil, fmt.Errorf("error applying server cert: %v", err)
 	}
@@ -158,31 +165,40 @@ func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, 
 		return nil, err
 	}
 
+	options := o.options
 	if o.SelfSignedCert {
-		o.options.Metrics.CertDir = selfSignedCertDir
+		options.Metrics.CertDir = selfSignedCertDir
 		o.WebhookConfig.CertDir = selfSignedCertDir
 	} else {
-		o.options.Metrics.CertDir = certDir
+		options.Metrics.CertDir = certDir
 		o.WebhookConfig.CertDir = certDir
 	}
-	o.options.WebhookServer = webhook.NewServer(webhook.Options{
+	options.WebhookServer = webhook.NewServer(webhook.Options{
 		Port:    *o.WebhookConfig.Port,
 		Host:    o.WebhookConfig.Host,
 		CertDir: o.WebhookConfig.CertDir,
 	})
 
-	namespaceFieldSelector := fields.SelectorFromSet(fields.Set{"metadata.namespace": env.GetPodNamespace()})
-	o.options.Cache.DefaultFieldSelector = namespaceFieldSelector
-	o.options.Cache.ByObject = map[controllerruntimeclient.Object]cache.ByObject{
-		&mcv1alpha1.Gateway{}: {
-			Field: namespaceFieldSelector,
-		},
-		&mcv1alpha2.ClusterSet{}: {
-			Field: namespaceFieldSelector,
-		},
-		&mcv1alpha1.MemberClusterAnnounce{}: {
-			Field: namespaceFieldSelector,
-		},
+	cacheOptions := &options.Cache
+	if isLeader {
+		// For the leader, restrict the cache to the controller's Namespace.
+		cacheOptions.DefaultNamespaces = map[string]cache.Config{
+			podNamespace: {},
+		}
+	} else {
+		// For a member, restict the cache to the controller's Namespace for the following objects.
+		cacheOptions.ByObject = map[controllerruntimeclient.Object]cache.ByObject{
+			&mcv1alpha1.Gateway{}: {
+				Namespaces: map[string]cache.Config{
+					podNamespace: {},
+				},
+			},
+			&mcv1alpha2.ClusterSet{}: {
+				Namespaces: map[string]cache.Config{
+					podNamespace: {},
+				},
+			},
+		}
 	}
 
 	// EndpointSlice is enabled in AntreaProxy by default since v1.11, so Antrea MC
@@ -206,11 +222,7 @@ func setupManagerAndCertController(isLeader bool, o *Options) (manager.Manager, 
 	}
 	o.ClusterCalimCRDAvailable = clusterClaimCRDAvailable
 
-	mgr, err := ctrl.NewManager(k8sConfig, manager.Options{
-		Scheme:                 o.options.Scheme,
-		Metrics:                o.options.Metrics,
-		HealthProbeBindAddress: o.options.HealthProbeBindAddress,
-	})
+	mgr, err := ctrl.NewManager(k8sConfig, options)
 	if err != nil {
 		return nil, fmt.Errorf("error creating manager: %v", err)
 	}

multicluster/cmd/multicluster-controller/leader.go
@@ -50,9 +50,7 @@ func newLeaderCommand() *cobra.Command {
 }
 
 func runLeader(o *Options) error {
-	// on the leader we want the reconciler to run for a given Namespace instead of cluster scope
 	podNamespace := env.GetPodNamespace()
-	o.Namespace = podNamespace
 	stopCh := signals.RegisterSignalHandlers()
 
 	mgr, err := setupManagerAndCertControllerFunc(true, o)

multicluster/cmd/multicluster-controller/options.go
@@ -35,8 +35,8 @@ type Options struct {
 	// The path of configuration file.
 	configFile     string
 	SelfSignedCert bool
-	options        ctrl.Options
-	Namespace      string
+	// options store some base controller Manager options (initialized from the provided config).
+	options ctrl.Options
 	// The Service ClusterIP range used in the member cluster.
 	ServiceCIDR string
 	// PodCIDRs is the Pod IP address CIDRs of the member cluster.
@@ -68,14 +68,12 @@ func newOptions() *Options {
 func (o *Options) complete(args []string) error {
 	var err error
 	o.setDefaults()
-	options := ctrl.Options{Scheme: scheme}
 	ctrlConfig := &mcsv1alpha1.MultiClusterConfig{}
 	if len(o.configFile) > 0 {
 		klog.InfoS("Loading config", "file", o.configFile)
 		if err = o.loadConfigFromFile(ctrlConfig); err != nil {
 			return err
 		}
-		o.options = options
 		if ctrlConfig.ServiceCIDR != "" {
 			if _, _, err := net.ParseCIDR(ctrlConfig.ServiceCIDR); err != nil {
 				return fmt.Errorf("failed to parse serviceCIDR, invalid CIDR string %s", ctrlConfig.ServiceCIDR)

multicluster/cmd/multicluster-controller/options_test.go
@@ -19,7 +19,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 func TestComplete(t *testing.T) {
@@ -34,7 +33,6 @@ func TestComplete(t *testing.T) {
 			o: Options{
 				configFile:          "./testdata/antrea-mc-config-with-valid-podcidrs.yml",
 				SelfSignedCert:      false,
-				options:             ctrl.Options{},
 				ServiceCIDR:         "",
 				PodCIDRs:            nil,
 				GatewayIPPrecedence: "",
@@ -47,7 +45,6 @@ func TestComplete(t *testing.T) {
 			o: Options{
 				configFile:          "./testdata/antrea-mc-config-with-empty-podcidrs.yml",
 				SelfSignedCert:      false,
-				options:             ctrl.Options{},
 				ServiceCIDR:         "",
 				PodCIDRs:            nil,
 				GatewayIPPrecedence: "",
@@ -60,7 +57,6 @@ func TestComplete(t *testing.T) {
 			o: Options{
 				configFile:          "./testdata/antrea-mc-config-with-invalid-podcidrs.yml",
 				SelfSignedCert:      false,
-				options:             ctrl.Options{},
 				ServiceCIDR:         "10.100.0.0/16",
 				PodCIDRs:            nil,
 				GatewayIPPrecedence: "",
@@ -73,7 +69,6 @@ func TestComplete(t *testing.T) {
 			o: Options{
 				configFile:          "./testdata/antrea-mc-config-with-invalid-endpointiptype.yml",
 				SelfSignedCert:      false,
-				options:             ctrl.Options{},
 				ServiceCIDR:         "10.100.0.0/16",
 				PodCIDRs:            nil,
 				GatewayIPPrecedence: "",