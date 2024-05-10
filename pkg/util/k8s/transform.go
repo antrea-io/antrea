278458ccc7a4a91c899c94e8a601574865e97d19
Quan Tian <quan.tian@broadcom.com>
2024-05-10 19:25:55 +0800

    Trim unneeded fields stored in informers to reduce memory footprint
    Unused fields stored in informers can occupy significant memory usage.
    The informer provides a transformer func to allow mutating the objects
    before they are stored, via which we can trim unneeded fields.
    
    In general, managedFields is never used for all types of objects. In
    addition, we trim some unneeded fields from Pod objects given their
    unused fields take the most considerable space.
    
    It can reduce the memory usage of 1k simple Pod by approximately 5m.
    
    Signed-off-by: Quan Tian <quan.tian@broadcom.com>

---
+++ pkg/util/k8s/transform.go
@@ -0,0 +1,91 @@
+// Copyright 2024 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package k8s
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/client-go/tools/cache"
+)
+
+// NewTrimmer returns a cache.TransformFunc that can be used to trim objects stored in informers.
+// The function must be idempotent before client-go v0.31.0 to avoid a race condition happening when objects were
+// accessed during Resync operation, see https://github.com/kubernetes/kubernetes/issues/124337.
+// But it's generally more efficient to avoid trimming the same object more than once.
+//
+// Note: be cautious when adding fields to be trimmed, ensuring they do not inadvertently clear the original values when
+// objects are updated by Antrea to kube-apiserver.
+func NewTrimmer(extraTrimmers ...cache.TransformFunc) cache.TransformFunc {
+	return func(obj interface{}) (interface{}, error) {
+		accessor, err := meta.Accessor(obj)
+		if err != nil {
+			return obj, nil
+		}
+		// It means the objects has been trimmed.
+		if accessor.GetManagedFields() == nil {
+			return obj, nil
+		}
+		// Trim common fields for all objects.
+		// According to https://kubernetes.io/docs/reference/using-api/server-side-apply/#clearing-managedfields,
+		// setting the managedFields to an empty list will not reset the field when the objects are updated, so it's
+		// safe to trim it for all objects.
+		accessor.SetManagedFields(nil)
+
+		// Trim type specific fields for each type.
+		for _, trimmer := range extraTrimmers {
+			trimmer(obj)
+		}
+		return obj, nil
+	}
+}
+
+// TrimPod clears unused fields from a Pod that are not required by Antrea.
+// It's safe to do so because Antrea only patches Pod.
+func TrimPod(obj interface{}) (interface{}, error) {
+	pod, ok := obj.(*corev1.Pod)
+	if !ok {
+		return obj, nil
+	}
+
+	pod.OwnerReferences = nil
+	pod.Spec.Volumes = nil
+	pod.Spec.InitContainers = nil
+	for i := range pod.Spec.Containers {
+		container := &pod.Spec.Containers[i]
+		container.Command = nil
+		container.Args = nil
+		container.EnvFrom = nil
+		container.Env = nil
+		container.VolumeMounts = nil
+		container.VolumeDevices = nil
+		container.LivenessProbe = nil
+		container.ReadinessProbe = nil
+		container.StartupProbe = nil
+		container.Lifecycle = nil
+		container.SecurityContext = nil
+	}
+	pod.Spec.EphemeralContainers = nil
+	pod.Spec.Affinity = nil
+	pod.Spec.Tolerations = nil
+	pod.Spec.ResourceClaims = nil
+
+	pod.Status.Conditions = nil
+	pod.Status.StartTime = nil
+	pod.Status.InitContainerStatuses = nil
+	pod.Status.ContainerStatuses = nil
+	pod.Status.EphemeralContainerStatuses = nil
+	pod.Status.ResourceClaimStatuses = nil
+	return pod, nil
+}
