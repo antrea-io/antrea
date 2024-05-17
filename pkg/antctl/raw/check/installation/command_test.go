8b37d97c4e5313c6d8997a031f35b0a3fd24ce53
Antonin Bas <antonin.bas@broadcom.com>
2024-05-17 11:46:17 -0700

    Add "-run" filter for antctl check installation command (#6333)
    To support running a subset only of tests, based on which test names
    match the provided regex.
    
    We also log stderr when `/agnhost connect` fails, to assist in
    troubleshooting. I have seen the `antctl check installation` command
    fail in CI, and at the moment it is impossible to troubleshoot.
    
    Signed-off-by: Antonin Bas <antonin.bas@broadcom.com>

---
+++ pkg/antctl/raw/check/installation/command_test.go
@@ -0,0 +1,106 @@
+// Copyright 2024 Antrea Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package installation
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func overrideTestsRegistry(t *testing.T, registry map[string]Test) {
+	oldRegistry := testsRegistry
+	testsRegistry = registry
+	t.Cleanup(func() {
+		testsRegistry = oldRegistry
+	})
+}
+
+type notRunnableTest struct{}
+
+func (t *notRunnableTest) Run(ctx context.Context, testContext *testContext) error {
+	return newNotRunnableError("not runnable")
+}
+
+type failedTest struct{}
+
+func (t *failedTest) Run(ctx context.Context, testContext *testContext) error {
+	return fmt.Errorf("failed")
+}
+
+type successfulTest struct{}
+
+func (t *successfulTest) Run(ctx context.Context, testContext *testContext) error {
+	return nil
+}
+
+func TestRun(t *testing.T) {
+	ctx := context.Background()
+
+	registry := map[string]Test{
+		"not-runnable": &notRunnableTest{},
+		"failure":      &failedTest{},
+		"success":      &successfulTest{},
+	}
+
+	testCases := []struct {
+		name          string
+		registry      map[string]Test
+		runFilter     string
+		expectedStats testStats
+	}{
+		{
+			name:          "no test in registry",
+			expectedStats: testStats{},
+		},
+		{
+			name:     "run all tests",
+			registry: registry,
+			expectedStats: testStats{
+				numSuccess: 1,
+				numFailure: 1,
+				numSkipped: 1,
+			},
+		},
+		{
+			name:      "run single test",
+			registry:  registry,
+			runFilter: "success",
+			expectedStats: testStats{
+				numSuccess: 1,
+			},
+		},
+		{
+			name:          "no matching test",
+			registry:      registry,
+			runFilter:     "my-test",
+			expectedStats: testStats{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			overrideTestsRegistry(t, tc.registry)
+			runFilterRegex, err := compileRunFilter(tc.runFilter)
+			require.NoError(t, err)
+			testContext := NewTestContext(nil, nil, "test-cluster", "kube-system", runFilterRegex)
+			stats := testContext.runTests(ctx)
+			assert.Equal(t, tc.expectedStats, stats)
+		})
+	}
+}
