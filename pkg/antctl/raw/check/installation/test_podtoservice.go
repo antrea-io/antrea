8f11dceb0788bf21205f4b02d67aeef7218cffa8
Quan Tian <quan.tian@broadcom.com>
2024-06-06 12:30:49 +0800

    antctl: improve deny-all NetworkPolicy check
    The egress-deny-all-connectivity check takes more than 1m to run even if
    it works as expected. This is because the client Pod can't resolve the
    Service name to IP due to the deny-all policy while DNS resolution
    doesn't have an explicit timeout.
    
    The inability to resolve Service names make testing both SameNodeService
    and OtherNodeService pointless as the client won't really send any
    traffic to them.
    
    The commit changes to test ClusterIPs of target Services directly, to
    check the NetworkPolicy enforcement of both inter-Node and intra-Node
    traffic, and both IPv4 and IPv6 family. It also reduces the code
    redundancy between egress-deny-all and ingress-deny-all tests.
    
    Signed-off-by: Quan Tian <quan.tian@broadcom.com>

--- pkg/antctl/raw/check/installation/test_podtoservice.go
+++ pkg/antctl/raw/check/installation/test_podtoservice.go
@@ -19,48 +19,50 @@ import (
 	"fmt"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 type PodToServiceConnectivityTest struct {
-	service string
-	preRun  func(ctx context.Context, testContext *testContext) error
+	getService func(testContext *testContext) (*corev1.Service, error)
 }
 
 func init() {
 	RegisterTest("pod-to-service-intranode-connectivity", &PodToServiceConnectivityTest{
-		service: echoSameNodeDeploymentName,
+		getService: func(testContext *testContext) (*corev1.Service, error) {
+			return testContext.echoSameNodeService, nil
+		},
 	})
 	RegisterTest("pod-to-service-internode-connectivity", &PodToServiceConnectivityTest{
-		service: echoOtherNodeDeploymentName,
-		preRun: func(ctx context.Context, testContext *testContext) error {
-			if testContext.echoOtherNodePod == nil {
-				return newNotRunnableError("Inter-Node test requires multiple Nodes")
+		getService: func(testContext *testContext) (*corev1.Service, error) {
+			if testContext.echoOtherNodeService == nil {
+				return nil, newNotRunnableError("Inter-Node test requires multiple Nodes")
 			}
-			return nil
+			return testContext.echoOtherNodeService, nil
 		},
 	})
 }
 
 func (t *PodToServiceConnectivityTest) Run(ctx context.Context, testContext *testContext) error {
-	if t.preRun != nil {
-		if err := t.preRun(ctx, testContext); err != nil {
-			return err
-		}
+	service, err := t.getService(testContext)
+	if err != nil {
+		return err
 	}
 	for _, clientPod := range testContext.clientPods {
-		testContext.Log("Validating from Pod %s to Service %s in Namespace %s...", clientPod.Name, t.service, testContext.namespace)
-		// Service is realized asynchronously, retry a few times.
-		if err := wait.PollUntilContextTimeout(ctx, 500*time.Millisecond, 2*time.Second, true, func(ctx context.Context) (bool, error) {
-			if err := testContext.runAgnhostConnect(ctx, clientPod.Name, "", t.service, 80); err != nil {
-				testContext.Log("Client Pod %s was not able to communicate with Service %s: %v, retrying...", clientPod.Name, t.service, err)
-				return false, nil
+		testContext.Log("Validating from Pod %s to Service %s in Namespace %s...", clientPod.Name, service.Name, testContext.namespace)
+		for _, clusterIP := range service.Spec.ClusterIPs {
+			// Service is realized asynchronously, retry a few times.
+			if err := wait.PollUntilContextTimeout(ctx, 500*time.Millisecond, 2*time.Second, true, func(ctx context.Context) (bool, error) {
+				if err := testContext.runAgnhostConnect(ctx, clientPod.Name, "", clusterIP, 80); err != nil {
+					testContext.Log("Client Pod %s was not able to communicate with Service %s (%s): %v, retrying...", clientPod.Name, service, clusterIP, err)
+					return false, nil
+				}
+				testContext.Log("Client Pod %s was able to communicate with Service %s (%s)", clientPod.Name, service.Name, clusterIP)
+				return true, nil
+			}); err != nil {
+				return fmt.Errorf("client Pod %s was not able to communicate with Service %s (%s): %w", clientPod.Name, service.Name, clusterIP, err)
 			}
-			return true, nil
-		}); err != nil {
-			return fmt.Errorf("client Pod %s was not able to communicate with Service %s: %w", clientPod.Name, t.service, err)
 		}
-		testContext.Log("Client Pod %s was able to communicate with Service %s", clientPod.Name, t.service)
 	}
 	return nil
 }
