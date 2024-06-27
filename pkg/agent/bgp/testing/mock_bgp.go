827e2c48a2de2bbbe5d866f5da5e1cf6a0fc13dc
Antonin Bas <antonin.bas@broadcom.com>
2024-06-26 17:12:41 -0700

    Add missing whitespace in generated mock file (#6489)
    PRs #6447 and #6477 got merged around the same time, and the mock file
    for the BGP interface was not generated using the latest mockgen
    version.
    
    Signed-off-by: Antonin Bas <antonin.bas@broadcom.com>

--- pkg/agent/bgp/testing/mock_bgp.go
+++ pkg/agent/bgp/testing/mock_bgp.go
@@ -20,6 +20,7 @@
 //
 //	mockgen -copyright_file hack/boilerplate/license_header.raw.txt -destination pkg/agent/bgp/testing/mock_bgp.go -package testing antrea.io/antrea/pkg/agent/bgp Interface
 //
+
 // Package testing is a generated GoMock package.
 package testing
 
