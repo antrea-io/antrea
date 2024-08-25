9d7f653e9c685e7ff7a5e44c81b1c12a9a410273
Rupam-It <mannarupam3@gmail.com>
2024-08-25 14:03:25 +0530

    Updated e2e tests for NodeLatencyMonitor
    Signed-off-by: Rupam-It <mannarupam3@gmail.com>

--- test/e2e/nodelatencymonitor_test.go
+++ test/e2e/nodelatencymonitor_test.go
@@ -2,130 +2,209 @@ package e2e
 
 import (
 	"context"
-	"fmt"
-	"os"
-	"path/filepath"
+	"math/rand"
 	"testing"
 	"time"
 
-	"antrea.io/antrea/pkg/apis/crd/v1alpha1"
-	clientset "antrea.io/antrea/pkg/client/clientset/versioned"
+	v1alpha1 "antrea.io/antrea/pkg/apis/crd/v1alpha1"
+	statsv1alpha1 "antrea.io/antrea/pkg/apis/stats/v1alpha1"
+
+	"antrea.io/antrea/pkg/features"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-// GetKubeconfigPath returns the path to the kubeconfig file used for connecting to the Kubernetes cluster.
-func GetKubeconfigPath() (string, error) {
-	// Check if the KUBECONFIG environment variable is set
-	kubeconfig := os.Getenv("KUBECONFIG")
-	if kubeconfig == "" {
-		// Default to the kubeconfig path in the user's home directory if not set
-		kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+const (
+	nodeLatencyMonitorName = "default"
+)
+
+// pingIntervalSeconds between 30 and 50
+func generatePingInterval() int32 {
+	return int32(rand.Intn(21) + 30)
+}
+
+func TestNodeLatencyMonitor(t *testing.T) {
+
+	skipIfFeatureDisabled(t, features.NodeLatencyMonitor, true, false)
+	skipIfHasWindowsNodes(t) // as it does not have offitial windows support
+
+	data, err := setupTest(t)
+	if err != nil {
+		t.Fatalf("Error when setting up test: %v", err)
+	}
+	defer teardownTest(t, data)
+
+	t.Run("testEnableLatencyProbes", func(t *testing.T) { testEnableLatencyProbes(t, data) })
+	t.Run("testRetrieveLatencyStats", func(t *testing.T) { testRetrieveLatencyStats(t, data) })
+	t.Run("testUpdatePingInterval", func(t *testing.T) { testUpdatePingInterval(t, data) })
+	t.Run("testDisableLatencyProbes", func(t *testing.T) { testDisableLatencyProbes(t, data) })
+}
+
+func createOrUpdateNodeLatencyMonitorCR(t *testing.T, data *TestData, interval int32) {
+	nlm := &v1alpha1.NodeLatencyMonitor{
+		ObjectMeta: v1.ObjectMeta{
+			Name: nodeLatencyMonitorName,
+		},
+		Spec: v1alpha1.NodeLatencyMonitorSpec{
+			PingIntervalSeconds: interval,
+		},
 	}
 
-	// Verify that the kubeconfig file exists
-	if _, err := os.Stat(kubeconfig); os.IsNotExist(err) {
-		return "", fmt.Errorf("kubeconfig file does not exist at path: %s", kubeconfig)
-	}
+	existingNLM, err := data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Get(context.TODO(), nlm.Name, v1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
 
-	return kubeconfig, nil
-}
-
-// getAntreaClient returns a clientset for interacting with Antrea CRDs
-func getAntreaClient(t *testing.T) *clientset.Clientset {
-	kubeconfig, err := GetKubeconfigPath()
-	failOnError(err, t)
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	failOnError(err, t)
-
-	antreaClientset, err := clientset.NewForConfig(config)
-	failOnError(err, t)
-	return antreaClientset
-}
-
-// TestNodeLatencyMonitorCRUD tests the CRUD operations for NodeLatencyMonitor resources.
-func TestNodeLatencyMonitorCRUD(t *testing.T) {
-	clientset := getAntreaClient(t)
-	crdClient := clientset.CrdV1alpha1().NodeLatencyMonitors()
-
-	t.Run("Create NodeLatencyMonitor", func(t *testing.T) {
-		latencyMonitor := &v1alpha1.NodeLatencyMonitor{
-			ObjectMeta: v1.ObjectMeta{
-				Name: "default",
-			},
-			Spec: v1alpha1.NodeLatencyMonitorSpec{
-				PingIntervalSeconds: 10,
-			},
-		}
-
-		createdMonitor, err := crdClient.Create(context.TODO(), latencyMonitor, v1.CreateOptions{})
-		failOnError(err, t)
-		assert.NotNil(t, createdMonitor)
-
-		defer func() {
-			err := crdClient.Delete(context.TODO(), "default", v1.DeleteOptions{})
-			failOnError(err, t)
-		}()
-	})
-
-	t.Run("Update NodeLatencyMonitor", func(t *testing.T) {
-		createdMonitor, err := crdClient.Get(context.TODO(), "default", v1.GetOptions{})
-		failOnError(err, t)
-
-		createdMonitor.Spec.PingIntervalSeconds = 30
-		updatedMonitor, err := crdClient.Update(context.TODO(), createdMonitor, v1.UpdateOptions{})
-		failOnError(err, t)
-		assert.NotNil(t, updatedMonitor)
-		assert.Equal(t, int32(30), updatedMonitor.Spec.PingIntervalSeconds)
-	})
-
-	t.Run("Get NodeLatencyMonitor", func(t *testing.T) {
-		fetchedMonitor, err := crdClient.Get(context.TODO(), "default", v1.GetOptions{})
-		failOnError(err, t)
-		assert.NotNil(t, fetchedMonitor)
-	})
-
-	t.Run("List NodeLatencyMonitors", func(t *testing.T) {
-		monitorList, err := crdClient.List(context.TODO(), v1.ListOptions{})
-		failOnError(err, t)
-		assert.NotNil(t, monitorList)
-		assert.Greater(t, len(monitorList.Items), 0)
-	})
-
-	t.Run("Delete NodeLatencyMonitor", func(t *testing.T) {
-		err := crdClient.Delete(context.TODO(), "default", v1.DeleteOptions{})
-		failOnError(err, t)
-
-		_, err = crdClient.Get(context.TODO(), "default", v1.GetOptions{})
-		assert.Error(t, err)
-	})
-}
-
-// TestNodeLatencyMonitorWatch tests the watch functionality for NodeLatencyMonitor resources.
-func TestNodeLatencyMonitorWatch(t *testing.T) {
-	clientset := getAntreaClient(t)
-	crdClient := clientset.CrdV1alpha1().NodeLatencyMonitors()
-
-	t.Run("Watch NodeLatencyMonitor", func(t *testing.T) {
-		opts := v1.ListOptions{
-			Watch: true,
-		}
-		watcher, err := crdClient.Watch(context.TODO(), opts)
-		failOnError(err, t)
-		defer watcher.Stop()
-
-		timeout := time.After(10 * time.Second)
-		for {
-			select {
-			case event := <-watcher.ResultChan():
-				assert.NotNil(t, event)
-				t.Logf("Received event: %v", event)
-			case <-timeout:
-				t.Log("Timed out waiting for watch events")
-				return
+			_, err := data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Create(context.TODO(), nlm, v1.CreateOptions{})
+			if err != nil {
+				t.Fatalf("Failed to create NodeLatencyMonitor CR: %v", err)
 			}
+			t.Logf("NodeLatencyMonitor CR created successfully.")
 		}
+	} else {
+
+		nlm.ResourceVersion = existingNLM.ResourceVersion
+		_, err := data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Update(context.TODO(), nlm, v1.UpdateOptions{})
+		if err != nil {
+			t.Fatalf("Failed to update NodeLatencyMonitor CR: %v", err)
+		}
+		t.Logf("NodeLatencyMonitor CR updated successfully.")
+	}
+}
+
+func testEnableLatencyProbes(t *testing.T, data *TestData) {
+	pingInterval := int32(60)
+
+	createOrUpdateNodeLatencyMonitorCR(t, data, pingInterval)
+	_, err := data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Get(context.TODO(), nodeLatencyMonitorName, v1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			t.Fatalf("NodeLatencyMonitor CR not found after creation: %v", err)
+		} else {
+			t.Fatalf("Unable to get NodeLatencyMonitor CR: %v", err)
+		}
+	}
+
+	t.Log("Latency probes are confirmed to be enabled.")
+}
+
+func testDisableLatencyProbes(t *testing.T, data *TestData) {
+
+	_, err := data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Get(context.TODO(), nodeLatencyMonitorName, v1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			t.Logf("NodeLatencyMonitor CR does not exist, no need to delete.")
+			return
+		}
+		t.Fatalf("Error when checking for NodeLatencyMonitor CR: %v", err)
+	}
+
+	err = data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Delete(context.TODO(), nodeLatencyMonitorName, v1.DeleteOptions{})
+	if err != nil {
+		t.Fatalf("Failed to delete NodeLatencyMonitor CR: %v", err)
+	}
+	t.Logf("Latency probes are confirmed to be disabled..")
+}
+
+func testUpdatePingInterval(t *testing.T, data *TestData) {
+	nlm, err := data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Get(context.TODO(), nodeLatencyMonitorName, v1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			t.Logf("NodeLatencyMonitor CR not found, creating a new one.")
+			pingInterval := int32(60)
+			createOrUpdateNodeLatencyMonitorCR(t, data, pingInterval)
+			nlm, err = data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Get(context.TODO(), nodeLatencyMonitorName, v1.GetOptions{})
+			if err != nil {
+				t.Fatalf("Failed to retrieve NodeLatencyMonitor CR after creation: %v", err)
+			}
+		} else {
+			t.Fatalf("Failed to retrieve NodeLatencyMonitor CR: %v", err)
+		}
+	}
+
+	// Update the ping interval
+	newInterval := generatePingInterval()
+	nlm.Spec.PingIntervalSeconds = newInterval
+	_, err = data.crdClient.CrdV1alpha1().NodeLatencyMonitors().Update(context.TODO(), nlm, v1.UpdateOptions{})
+	if err != nil {
+		t.Fatalf("Failed to update NodeLatencyMonitor CR: %v", err)
+	}
+
+	t.Logf("Ping interval updated successfully to %d seconds.", newInterval)
+}
+
+func testRetrieveLatencyStats(t *testing.T, data *TestData) {
+
+	summary := &statsv1alpha1.NodeLatencyStats{
+		ObjectMeta: v1.ObjectMeta{Name: "kind-worker"},
+		PeerNodeLatencyStats: []statsv1alpha1.PeerNodeLatencyStats{
+			{
+				NodeName: "kind-control-plane",
+				TargetIPLatencyStats: []statsv1alpha1.TargetIPLatencyStats{
+					{
+						LastMeasuredRTTNanoseconds: 5837000,
+						LastRecvTime:               v1.Time{Time: time.Now().Add(-time.Minute)},
+						LastSendTime:               v1.Time{Time: time.Now().Add(-time.Minute)},
+						TargetIP:                   "10.10.0.1",
+					},
+				},
+			},
+			{
+				NodeName: "kind-worker2",
+				TargetIPLatencyStats: []statsv1alpha1.TargetIPLatencyStats{
+					{
+						LastMeasuredRTTNanoseconds: 4704000,
+						LastRecvTime:               v1.Time{Time: time.Now().Add(-time.Minute)},
+						LastSendTime:               v1.Time{Time: time.Now().Add(-time.Minute)},
+						TargetIP:                   "10.10.2.1",
+					},
+				},
+			},
+		},
+	}
+
+	ctx := context.Background()
+
+	_, err := data.crdClient.StatsV1alpha1().NodeLatencyStats().Create(ctx, summary, v1.CreateOptions{})
+	require.NoError(t, err)
+
+	err = wait.PollImmediate(time.Second, 15*time.Second, func() (bool, error) {
+		statsList, err := data.crdClient.StatsV1alpha1().NodeLatencyStats().List(ctx, v1.ListOptions{})
+		if err != nil {
+			t.Logf("Error while listing NodeLatencyStats: %v", err)
+			return false, err
+		}
+		if len(statsList.Items) == 0 {
+			t.Logf("No NodeLatencyStats found, retrying...%v", statsList)
+			return false, nil
+		}
+
+		for _, item := range statsList.Items {
+			t.Logf("NodeLatencyStats found for node: %s", item.Name)
+		}
+		return true, nil
 	})
+
+	if err != nil {
+		t.Fatalf("Failed to retrieve NodeLatencyStats: %v", err)
+	}
+
+	statsList, err := data.crdClient.StatsV1alpha1().NodeLatencyStats().List(ctx, v1.ListOptions{})
+	require.NoError(t, err)
+	assert.Greater(t, len(statsList.Items), 0, "Expected at least one NodeLatencyStats item")
+
+	found := false
+	for _, item := range statsList.Items {
+		if item.Name == "kind-worker" {
+			found = true
+
+			assert.Len(t, item.PeerNodeLatencyStats, 2, "Expected 2 PeerNodeLatencyStats items")
+
+			break
+		}
+	}
+	assert.True(t, found, "NodeLatencyStats for 'kind-worker' not found")
+	t.Logf("Successfully received NodeLatencyStats")
 }
