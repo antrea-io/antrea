3465eeaf800d43f9acb6d39ac06da678cc216b04
Rupam-It <mannarupam3@gmail.com>
2024-09-06 23:10:16 +0530

    update copywrite date
    Signed-off-by: Rupam-It <mannarupam3@gmail.com>

--- test/e2e/nodelatencymonitor_test.go
+++ test/e2e/nodelatencymonitor_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Antrea Authors
+// Copyright 2024 Antrea Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 package e2e
 
 import (
-	// Standard library imports
 	"context"
 	"testing"
 	"time"
