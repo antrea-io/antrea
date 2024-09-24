0183504d9d0c404b53bf7b530a258151bb1d381a
Hang Yan <yhang@vmware.com>
2024-09-24 15:48:56 +0800

    fix svc pipeline
    Signed-off-by: Hang Yan <yhang@vmware.com>

--- pkg/agent/openflow/packetcapture.go
+++ pkg/agent/openflow/packetcapture.go
@@ -113,19 +113,39 @@ func (f *featurePacketCapture) genFlows(dataplaneTag uint8,
 				flowBuilder = flowBuilder.MatchDstIP(packet.DestinationIP)
 			}
 		} else {
+			// handle pod -> svc case:
 			// generate flows to endpoints.
 			for _, epPacket := range endpointPackets {
 				tmpFlowBuilder := ConntrackStateTable.ofTable.BuildFlow(priorityHigh).
 					Cookie(cookieID).
 					MatchInPort(ofPort).
 					MatchCTStateTrk(true).
+					Action().LoadRegMark(RewriteMACRegMark).
 					Action().LoadToRegField(PacketCaptureMark, tag).
 					SetHardTimeout(timeout).
-					Action().GotoStage(stagePreRouting)
+					Action().GotoStage(stageEgressSecurity)
 				tmpFlowBuilder.MatchDstIP(epPacket.DestinationIP)
 				flow := matchTransportHeader(packet, tmpFlowBuilder, endpointPackets).Done()
 				flows = append(flows, flow)
 			}
+
+			// capture the first tracked packet for svc.
+			for _, ipProtocol := range f.ipProtocols {
+				tmpFlowBuilder := ConntrackStateTable.ofTable.BuildFlow(priorityHigh).
+					Cookie(cookieID).
+					MatchInPort(ofPort).
+					MatchProtocol(ipProtocol).
+					MatchCTStateNew(true).
+					MatchCTStateTrk(true).
+					Action().LoadRegMark(RewriteMACRegMark).
+					Action().LoadToRegField(PacketCaptureMark, tag).
+					SetHardTimeout(timeout).
+					Action().GotoStage(stagePreRouting)
+				tmpFlowBuilder.MatchDstIP(packet.DestinationIP)
+				tmpFlowBuilder = matchTransportHeader(packet, tmpFlowBuilder, nil)
+				flows = append(flows, tmpFlowBuilder.Done())
+			}
+
 		}
 	} else {
 		flowBuilder = L2ForwardingCalcTable.ofTable.BuildFlow(priorityHigh).
@@ -139,25 +159,6 @@ func (f *featurePacketCapture) genFlows(dataplaneTag uint8,
 			Action().GotoStage(stageIngressSecurity)
 		if packet.SourceIP != nil {
 			flowBuilder = flowBuilder.MatchSrcIP(packet.SourceIP)
-		}
-	}
-
-	// for sender only case, capture the first tracked packet for svc.
-	if senderOnly {
-		for _, ipProtocol := range f.ipProtocols {
-			tmpFlowBuilder := ConntrackStateTable.ofTable.BuildFlow(priorityHigh).
-				Cookie(cookieID).
-				MatchInPort(ofPort).
-				MatchProtocol(ipProtocol).
-				MatchCTStateNew(true).
-				MatchCTStateTrk(true).
-				Action().LoadRegMark(RewriteMACRegMark).
-				Action().LoadToRegField(PacketCaptureMark, tag).
-				SetHardTimeout(timeout).
-				Action().GotoStage(stagePreRouting)
-			tmpFlowBuilder.MatchDstIP(packet.DestinationIP)
-			tmpFlowBuilder = matchTransportHeader(packet, tmpFlowBuilder, nil)
-			flows = append(flows, tmpFlowBuilder.Done())
 		}
 	}
 
