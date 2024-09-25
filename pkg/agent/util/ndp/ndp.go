2dd49d88fd74ec5cf8cceb9963647edae12df50c
Xu Liu <xu.liu@broadcom.com>
2024-09-25 10:07:28 +0000

    Delay the initialization of ARP/NDP responders
    For secondary-network scenarios, the transport interface can be
    changed after the agent is started. The ARP/NDP responders should
    be started after the initialization of secondary-network to bind
    to the transport interface of the new index.
    
    Besides, this change also addresses the following issues:
    - NDP responder may fail to bind to the new interface due to the
    Duplicate Address Detection process.
    - Golang caches the zone index for the interface, which may result
    in NDP responder binding on the stale interface
    
    Fixes: #6623
    
    Signed-off-by: Xu Liu <xu.liu@broadcom.com>

--- pkg/agent/util/ndp/ndp.go
+++ pkg/agent/util/ndp/ndp.go
@@ -17,8 +17,14 @@ package ndp
 import (
 	"fmt"
 	"net"
+	"net/netip"
 
 	"github.com/mdlayher/ndp"
+)
+
+var (
+	// IPv6AllNodes is the IPv6 link-local multicast address for all nodes.
+	IPv6AllNodes = netip.MustParseAddr("ff02::1")
 )
 
 // GratuitousNDPOverIface sends a gratuitous NDP from 'iface' using 'srcIP' as the source IP.
@@ -32,10 +38,10 @@ func GratuitousNDPOverIface(srcIP net.IP, iface *net.Interface) error {
 		return fmt.Errorf("failed to create NDP responder for %q: %s", iface.Name, err)
 	}
 	defer conn.Close()
-
+	target, _ := netip.AddrFromSlice(srcIP)
 	na := &ndp.NeighborAdvertisement{
 		Override:      true,
-		TargetAddress: srcIP,
+		TargetAddress: target,
 		Options: []ndp.Option{
 			&ndp.LinkLayerAddress{
 				Direction: ndp.Target,
@@ -43,5 +49,5 @@ func GratuitousNDPOverIface(srcIP net.IP, iface *net.Interface) error {
 			},
 		},
 	}
-	return conn.WriteTo(na, nil, net.IPv6linklocalallnodes)
+	return conn.WriteTo(na, nil, IPv6AllNodes)
 }
