d36a63a6f7c9dc7655dbe8ed382126937b5b20c8
pulkit_jain <42600268+jainpulkit22@users.noreply.github.com>
2025-02-20 10:50:37 +0530

    Fixed the cleanup failure (#7007)
    If all the clusters are cleaned up then .clusters.swp
    file is not created and it gives an error that file
    not present.
    
    Signed-off-by: Pulkit Jain <pulkit.jain@broadcom.com>

--- ci/kind/kind-setup.sh
+++ ci/kind/kind-setup.sh
@@ -565,6 +565,7 @@ function clean_kind {
       flock -x 200
 
       current_timestamp=$(date +%s)
+      touch ~/.antrea/.clusters.swp
       while IFS=' ' read -r name creationTimestamp; do
           if [[ -z "$name" || -z "$creationTimestamp" ]]; then
               continue
