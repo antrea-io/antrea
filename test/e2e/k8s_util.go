93692050f716f5f2974369f848ac31ca3ad7e266
Peter Tran <peter-pt.tran@broadcom.com>
2025-03-18 10:46:29 -0400

    fix: randomize probing in e2e test setup (#7067)
    * On older machines, the tests `AntreaPolicyExtendedNamespaces` would
      fail but not on CI
      * The failure was due to the 1k+ simultaneous probes from all pods to
        a single pod which could not handle it
      * For that test the number of probes were ~1800:
        * n^2 x m
          * n = 5 namespace x 3 pods
            * squared for the nested for loop validating all pod to pod
    	  connections
          * m = 8 ports
    * By randomizing the probing, the probing against any one pod is spread
      out and allows older laptops to complete the environment setup before
      testing
    
    Signed-off-by: Peter Tran <peter-pt.tran@broadcom.com>

--- test/e2e/k8s_util.go
+++ test/e2e/k8s_util.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math/rand"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -1065,20 +1067,33 @@ func (k *KubernetesUtils) waitForHTTPServers(allPods []Pod) error {
 	return fmt.Errorf("after %d tries, HTTP servers are not ready", maxTries)
 }
 
-func (k *KubernetesUtils) validateOnePort(allPods []Pod, reachability *Reachability, port int32, protocol utils.AntreaPolicyProtocol) {
-	numProbes := len(allPods) * len(allPods)
-	resultsCh := make(chan *probeResult, numProbes)
+// Validate checks the connectivity between all Pods in both directions with a
+// list of ports and a protocol. The connectivity from a Pod to another Pod should
+// be consistent across all provided ports. Otherwise, this connectivity will be
+// treated as Error.
+func (k *KubernetesUtils) Validate(allPods []Pod, reachability *Reachability, ports []int32, protocol utils.AntreaPolicyProtocol) {
+	podCount := len(allPods)
+	numProbes := podCount * podCount * len(ports)
 	// TODO: find better metrics, this is only for POC.
-	oneProbe := func(podFrom, podTo Pod, port int32) {
-		log.Tracef("Probing: %s -> %s", podFrom, podTo)
-		expectedResult := reachability.Expected.Get(podFrom.String(), podTo.String())
-		connectivity, err := k.Probe(podFrom.Namespace(), podFrom.PodName(), podTo.Namespace(), podTo.PodName(), port, protocol, nil, &expectedResult)
-		resultsCh <- &probeResult{podFrom, podTo, connectivity, err}
-	}
-	for _, pod1 := range allPods {
-		for _, pod2 := range allPods {
-			go oneProbe(pod1, pod2, port)
+	resultsCh := make(chan *probeResult, numProbes)
+	oneProbe := func(podFrom Pod, allPods []Pod, port []int32) {
+		allPods = slices.Clone(allPods)
+		rand.Shuffle(len(allPods), func(i, j int) {
+			allPods[i], allPods[j] = allPods[j], allPods[i]
+		})
+
+		for _, podTo := range allPods {
+			for _, port := range ports {
+				log.Tracef("Probing: %s -> %s", podFrom, podTo)
+				expectedResult := reachability.Expected.Get(podFrom.String(), podTo.String())
+				connectivity, err := k.Probe(podFrom.Namespace(), podFrom.PodName(), podTo.Namespace(), podTo.PodName(), port, protocol, nil, &expectedResult)
+				resultsCh <- &probeResult{podFrom, podTo, connectivity, err}
+			}
 		}
+	}
+
+	for _, fromPod := range allPods {
+		go oneProbe(fromPod, allPods, ports)
 	}
 	for i := 0; i < numProbes; i++ {
 		r := <-resultsCh
@@ -1099,22 +1114,6 @@ func (k *KubernetesUtils) validateOnePort(allPods []Pod, reachability *Reachabil
 		} else if prevConn != r.connectivity {
 			reachability.Observe(r.podFrom, r.podTo, Error)
 		}
-	}
-}
-
-// Validate checks the connectivity between all Pods in both directions with a
-// list of ports and a protocol. The connectivity from a Pod to another Pod should
-// be consistent across all provided ports. Otherwise, this connectivity will be
-// treated as Error.
-func (k *KubernetesUtils) Validate(allPods []Pod, reachability *Reachability, ports []int32, protocol utils.AntreaPolicyProtocol) {
-	for _, port := range ports {
-		// we do not run all the probes in parallel as we have experienced that on some
-		// machines, this can cause a fraction of the probes to always fail, despite the
-		// built-in retry (3x) mechanism. Probably because of the large number of probes,
-		// each one being executed in its own goroutine. For example, with 9 Pods and for
-		// ports 80, 81, 8080, 8081, 8082, 8083, 8084 and 8085, we would end up with
-		// potentially 9*9*8 = 648 simultaneous probes.
-		k.validateOnePort(allPods, reachability, port, protocol)
 	}
 }
 
