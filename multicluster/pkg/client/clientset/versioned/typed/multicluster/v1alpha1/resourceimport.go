98bca7b8d57d6d957103097e252efbef5c59392b
Antonin Bas <antonin.bas@broadcom.com>
2025-06-17 16:22:17 -0700

    Regenerate code for multicluster
    Signed-off-by: Antonin Bas <antonin.bas@broadcom.com>

--- multicluster/pkg/client/clientset/versioned/typed/multicluster/v1alpha1/resourceimport.go
+++ multicluster/pkg/client/clientset/versioned/typed/multicluster/v1alpha1/resourceimport.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Antrea Authors
+// Copyright 2025 Antrea Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,9 +17,9 @@
 package v1alpha1
 
 import (
-	"context"
+	context "context"
 
-	v1alpha1 "antrea.io/antrea/multicluster/apis/multicluster/v1alpha1"
+	multiclusterv1alpha1 "antrea.io/antrea/multicluster/apis/multicluster/v1alpha1"
 	scheme "antrea.io/antrea/multicluster/pkg/client/clientset/versioned/scheme"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
@@ -35,33 +35,34 @@ type ResourceImportsGetter interface {
 
 // ResourceImportInterface has methods to work with ResourceImport resources.
 type ResourceImportInterface interface {
-	Create(ctx context.Context, resourceImport *v1alpha1.ResourceImport, opts v1.CreateOptions) (*v1alpha1.ResourceImport, error)
-	Update(ctx context.Context, resourceImport *v1alpha1.ResourceImport, opts v1.UpdateOptions) (*v1alpha1.ResourceImport, error)
+	Create(ctx context.Context, resourceImport *multiclusterv1alpha1.ResourceImport, opts v1.CreateOptions) (*multiclusterv1alpha1.ResourceImport, error)
+	Update(ctx context.Context, resourceImport *multiclusterv1alpha1.ResourceImport, opts v1.UpdateOptions) (*multiclusterv1alpha1.ResourceImport, error)
 	// Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
-	UpdateStatus(ctx context.Context, resourceImport *v1alpha1.ResourceImport, opts v1.UpdateOptions) (*v1alpha1.ResourceImport, error)
+	UpdateStatus(ctx context.Context, resourceImport *multiclusterv1alpha1.ResourceImport, opts v1.UpdateOptions) (*multiclusterv1alpha1.ResourceImport, error)
 	Delete(ctx context.Context, name string, opts v1.DeleteOptions) error
 	DeleteCollection(ctx context.Context, opts v1.DeleteOptions, listOpts v1.ListOptions) error
-	Get(ctx context.Context, name string, opts v1.GetOptions) (*v1alpha1.ResourceImport, error)
-	List(ctx context.Context, opts v1.ListOptions) (*v1alpha1.ResourceImportList, error)
+	Get(ctx context.Context, name string, opts v1.GetOptions) (*multiclusterv1alpha1.ResourceImport, error)
+	List(ctx context.Context, opts v1.ListOptions) (*multiclusterv1alpha1.ResourceImportList, error)
 	Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error)
-	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *v1alpha1.ResourceImport, err error)
+	Patch(ctx context.Context, name string, pt types.PatchType, data []byte, opts v1.PatchOptions, subresources ...string) (result *multiclusterv1alpha1.ResourceImport, err error)
 	ResourceImportExpansion
 }
 
 // resourceImports implements ResourceImportInterface
 type resourceImports struct {
-	*gentype.ClientWithList[*v1alpha1.ResourceImport, *v1alpha1.ResourceImportList]
+	*gentype.ClientWithList[*multiclusterv1alpha1.ResourceImport, *multiclusterv1alpha1.ResourceImportList]
 }
 
 // newResourceImports returns a ResourceImports
 func newResourceImports(c *MulticlusterV1alpha1Client, namespace string) *resourceImports {
 	return &resourceImports{
-		gentype.NewClientWithList[*v1alpha1.ResourceImport, *v1alpha1.ResourceImportList](
+		gentype.NewClientWithList[*multiclusterv1alpha1.ResourceImport, *multiclusterv1alpha1.ResourceImportList](
 			"resourceimports",
 			c.RESTClient(),
 			scheme.ParameterCodec,
 			namespace,
-			func() *v1alpha1.ResourceImport { return &v1alpha1.ResourceImport{} },
-			func() *v1alpha1.ResourceImportList { return &v1alpha1.ResourceImportList{} }),
+			func() *multiclusterv1alpha1.ResourceImport { return &multiclusterv1alpha1.ResourceImport{} },
+			func() *multiclusterv1alpha1.ResourceImportList { return &multiclusterv1alpha1.ResourceImportList{} },
+		),
 	}
 }
