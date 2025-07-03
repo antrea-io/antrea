2a17a1b28f064f3d06f90b7414d03bfdfb8860df
Lan Luo <lan.luo@broadcom.com>
2025-07-03 07:03:55 +0000

    Fix index out of range issue for GetVFLinkNames
    The old version GetVFLinkNames from sriov-cni utils may raise errror
    `runtime error: index out of range [0] with length 0`, so copy the
    latest version to Antrea without importing the package.
    
    Signed-off-by: Lan Luo <lan.luo@broadcom.com>

--- pkg/agent/cniserver/sriov_linux.go
+++ pkg/agent/cniserver/sriov_linux.go
@@ -18,8 +18,16 @@
 package cniserver
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/Mellanox/sriovnet"
-	sriovcniutils "github.com/k8snetworkplumbingwg/sriov-cni/pkg/utils"
+)
+
+var (
+	// SysBusPCI is the /sysfs PCI device directory
+	SysBusPCI = "/sys/bus/pci/devices"
 )
 
 // getVFLinkName returns a VF's network interface name given its PCI address.
@@ -46,9 +54,42 @@ func (n *sriovNet) GetVfRepresentor(uplink string, vfIndex int) (string, error) 
 }
 
 func (n *sriovNet) GetVFLinkNames(pciAddr string) (string, error) {
-	return sriovcniutils.GetVFLinkNames(pciAddr)
+	return GetVFLinkName(pciAddr)
 }
 
 func newSriovNet() *sriovNet {
 	return &sriovNet{}
+}
+
+// Note: The following fuction is coming from https://github.com/k8snetworkplumbingwg/sriov-cni/blob/master/pkg/utils/utils.go
+// The permanent link is https://github.com/k8snetworkplumbingwg/sriov-cni/blob/3d9014b16bd22ed3381f41cd6ad097b8b741cab2/pkg/utils/utils.go#L186-L211
+// We can't directly import the package in go.mod due to the sriov-cni repo has no v2 module yet. it reports:
+// "require github.com/k8snetworkplumbingwg/sriov-cni: version “v2.9.0” invalid: should be v0 or v1, not v2.""
+
+// GetVFLinkName returns VF's network interface name given it's PCI addr
+func GetVFLinkName(pciAddr string) (string, error) {
+	var names []string
+	vfDir := filepath.Join(SysBusPCI, pciAddr, "net")
+	if _, err := os.Lstat(vfDir); err != nil {
+		return "", err
+	}
+
+	fInfos, err := os.ReadDir(vfDir)
+	if err != nil {
+		return "", fmt.Errorf("failed to read net dir of the device %s: %v", pciAddr, err)
+	}
+
+	if len(fInfos) == 0 {
+		return "", fmt.Errorf("VF device %s sysfs path (%s) has no entries", pciAddr, vfDir)
+	}
+
+	names = make([]string, len(fInfos))
+	for idx, f := range fInfos {
+		names[idx] = f.Name()
+	}
+
+	if len(names) < 1 {
+		return "", fmt.Errorf("VF device %s has no entries", pciAddr)
+	}
+	return names[0], nil
 }
