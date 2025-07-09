64a5da34c754474e45ab9e8f41bd2107e5be614a
Jianjun Shen <jianjun.shen@broadcom.com>
2025-07-09 09:41:26 -0700

    Merge connectInterfaceToOVSCommon into connectInterfaceToOVS (#7285)
    connectInterfaceToOVSCommon no longer needs to be a separate func.
    
    Signed-off-by: Jianjun Shen <shenj@vmware.com>

--- pkg/agent/cniserver/pod_configuration_linux.go
+++ pkg/agent/cniserver/pod_configuration_linux.go
@@ -41,17 +41,13 @@ func (pc *podConfigurator) connectInterfaceToOVS(
 	ovsPortName := hostIface.Name
 	containerConfig := buildContainerConfig(ovsPortName, containerID, podName, podNamespace,
 		netNS, containerIface, ips, vlanID)
-	return containerConfig, pc.connectInterfaceToOVSCommon(ovsPortName, netNS, containerConfig)
-}
 
-func (pc *podConfigurator) connectInterfaceToOVSCommon(ovsPortName, netNS string, containerConfig *interfacestore.InterfaceConfig) error {
-	// create OVS Port and add attach container configuration into external_ids
-	containerID := containerConfig.ContainerID
-	klog.V(2).Infof("Adding OVS port %s for container %s", ovsPortName, containerID)
+	// Create an OVS Port and add container configuration into external_ids.
 	ovsAttachInfo := BuildOVSPortExternalIDs(containerConfig)
+	klog.V(2).InfoS("Adding OVS port for container", "port", ovsPortName, "container", containerID)
 	portUUID, err := pc.createOVSPort(ovsPortName, ovsAttachInfo, containerConfig.VLANID)
 	if err != nil {
-		return fmt.Errorf("failed to add OVS port for container %s: %v", containerID, err)
+		return nil, fmt.Errorf("failed to add OVS port for container %s: %v", containerID, err)
 	}
 	// Remove OVS port if any failure occurs in later manipulation.
 	defer func() {
@@ -66,11 +62,11 @@ func (pc *podConfigurator) connectInterfaceToOVSCommon(ovsPortName, netNS string
 		// GetOFPort will wait for up to 1 second for OVSDB to report the OFPort number.
 		ofPort, err = pc.ovsBridgeClient.GetOFPort(ovsPortName, false)
 		if err != nil {
-			return fmt.Errorf("failed to get of_port of OVS port %s: %v", ovsPortName, err)
+			return nil, fmt.Errorf("failed to get of_port of OVS port %s: %v", ovsPortName, err)
 		}
 		klog.V(2).InfoS("Setting up Openflow entries for Pod interface", "container", containerID, "port", ovsPortName)
 		if err = pc.ofClient.InstallPodFlows(ovsPortName, containerConfig.IPs, containerConfig.MAC, uint32(ofPort), containerConfig.VLANID, nil); err != nil {
-			return fmt.Errorf("failed to add Openflow entries for container %s: %v", containerID, err)
+			return nil, fmt.Errorf("failed to add Openflow entries for container %s: %v", containerID, err)
 		}
 	}
 
@@ -90,7 +86,7 @@ func (pc *podConfigurator) connectInterfaceToOVSCommon(ovsPortName, netNS string
 		}
 		pc.podUpdateNotifier.Notify(event)
 	}
-	return nil
+	return containerConfig, nil
 }
 
 func (pc *podConfigurator) configureInterfaces(
