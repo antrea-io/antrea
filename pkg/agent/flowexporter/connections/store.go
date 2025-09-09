97e64330b5ac29b5be01d255957947966274dcb6
Andrew Su <andrew.su@broadcom.com>
2025-09-08 23:54:30 -0400

    Add new connection store implementation
    This updates connection to track the last time it was used.
    
    TODO: Switch back to LastExportTime instead of LastUsedTime
    
    Signed-off-by: Andrew Su <andrew.su@broadcom.com>

---
+++ pkg/agent/flowexporter/connections/store.go
@@ -0,0 +1,195 @@
+package connections
+
+import (
+	"container/heap"
+	"time"
+
+	"antrea.io/antrea/pkg/agent/flowexporter/connection"
+	"antrea.io/antrea/pkg/agent/flowexporter/utils"
+	"k8s.io/klog/v2"
+)
+
+type UpdateMsg struct {
+	Key     []*connection.Connection
+	Deleted bool
+}
+
+type subscriber struct {
+	ch chan UpdateMsg
+}
+
+type ConnStore struct {
+	updateCh chan []connection.Connection // Used to receive new connections
+	addSubCh chan *subscriber
+	delSubCh chan *subscriber
+	subs     map[*subscriber]struct{}
+
+	entries map[connection.ConnectionKey]*connection.Connection
+
+	// TODO Andrew: What is a stale connection?
+	staleConnectionTimeout time.Duration
+	gc                     gcHeap
+}
+
+func NewConnStore(staleConnTimeout time.Duration) *ConnStore {
+	return &ConnStore{
+		updateCh: make(chan []connection.Connection, 10),
+		addSubCh: make(chan *subscriber, 10),
+		delSubCh: make(chan *subscriber, 10),
+		subs:     make(map[*subscriber]struct{}),
+
+		entries:                make(map[connection.ConnectionKey]*connection.Connection, 1000),
+		staleConnectionTimeout: staleConnTimeout,
+	}
+}
+
+func (cs *ConnStore) Run(stopCh <-chan struct{}) {
+	klog.V(5).Info("ConnStore started")
+
+	gcStaleTicker := time.NewTicker(cs.staleConnectionTimeout)
+	defer gcStaleTicker.Stop()
+	for {
+		select {
+		case <-stopCh:
+			return
+		case batch := <-cs.updateCh:
+			cs.updateConnections(batch)
+		case sub := <-cs.addSubCh:
+			cs.subs[sub] = struct{}{}
+		case sub := <-cs.delSubCh:
+			delete(cs.subs, sub)
+		case <-gcStaleTicker.C:
+			cs.removeStaleConnections()
+		}
+	}
+}
+
+func (cs *ConnStore) removeStaleConnections() {
+	now := time.Now().UnixNano()
+	conns := []*connection.Connection{}
+	for len(cs.gc) > 0 {
+		top := cs.gc[0]
+		if top.expiryMs > now {
+			break
+		}
+
+		key := connection.NewConnectionKey(top.conn)
+
+		heap.Pop(&cs.gc)
+		conn, ok := cs.entries[key]
+		if !ok { // Already deleted
+			continue
+		}
+
+		lastUsedTime := conn.LastUsedTime.Load()
+		// TODO Andrew: Do we need to care about "readyToDelete" connections?
+		if lastUsedTime+cs.staleConnectionTimeout.Nanoseconds() <= now {
+			delete(cs.entries, key)
+		}
+
+		conns = append(conns, conn)
+	}
+
+	cs.notify(conns, true)
+}
+
+func (cs *ConnStore) updateConnections(batch []connection.Connection) {
+	updatedConns := make([]*connection.Connection, 0, len(batch))
+	now := time.Now()
+	for i := range batch {
+
+		in := &batch[i] // Copy the connection
+		in.IsPresent = true
+		in.LastUpdateTime = now
+
+		key := connection.NewConnectionKey(in)
+		e, ok := cs.entries[key]
+		if !ok {
+			e = in
+			cs.entries[key] = e
+		} else {
+			e.IsPresent = in.IsPresent
+			// TODO Andrew: Is this check necessary? Maybe the user of this conn should
+			// be the one to determine usage and we focus on storing it.
+			if utils.IsConnectionDying(e) {
+				return
+			}
+
+			// There are some changes since the last update
+			if (in.OriginalStats.Packets > e.OriginalStats.Packets) ||
+				(in.OriginalStats.ReversePackets > e.OriginalStats.ReversePackets) ||
+				(in.TCPState != e.TCPState) {
+				e.LastUpdateTime = in.LastUpdateTime
+			}
+
+			e.OriginalStats = in.OriginalStats
+			e.TCPState = in.TCPState
+		}
+
+		updatedConns = append(updatedConns, e)
+
+		lastUsedTime := e.LastUsedTime.Load()
+		if lastUsedTime == 0 {
+			lastUsedTime = e.UpdateLastUsedTime()
+		}
+
+		heap.Push(&cs.gc, &gcItem{
+			conn:     e,
+			expiryMs: lastUsedTime + cs.staleConnectionTimeout.Nanoseconds(),
+		})
+	}
+
+	cs.notify(updatedConns, false)
+}
+
+func (s *ConnStore) Subscribe() *subscriber {
+	sub := &subscriber{
+		ch: make(chan UpdateMsg, 100),
+	}
+	s.addSubCh <- sub
+	return sub
+}
+
+func (s *ConnStore) Unsubscribe(sub *subscriber) {
+	if sub != nil {
+		s.delSubCh <- sub
+	}
+}
+
+func (cs *ConnStore) notify(conns []*connection.Connection, deleted bool) {
+	if len(conns) == 0 {
+		return
+	}
+
+	msg := UpdateMsg{
+		Key:     conns,
+		Deleted: deleted,
+	}
+
+	for sub := range cs.subs {
+		sub.ch <- msg
+	}
+}
+
+type gcItem struct {
+	conn     *connection.Connection
+	expiryMs int64
+	index    int
+}
+type gcHeap []*gcItem
+
+func (h gcHeap) Len() int           { return len(h) }
+func (h gcHeap) Less(i, j int) bool { return h[i].expiryMs < h[j].expiryMs }
+func (h gcHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].index = i
+	h[j].index = j
+}
+func (h *gcHeap) Push(x any) { *h = append(*h, x.(*gcItem)) }
+func (h *gcHeap) Pop() any {
+	old := *h
+	n := len(old)
+	it := old[n-1]
+	*h = old[:n-1]
+	return it
+}
---
+++ pkg/agent/flowexporter/connections/store_test.go
@@ -0,0 +1,257 @@
+package connections
+
+import (
+	"container/heap"
+	"net/netip"
+	"testing"
+	"time"
+
+	"antrea.io/antrea/pkg/agent/flowexporter/connection"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConnStore_updateConnections(t *testing.T) {
+	conn1 := connection.Connection{
+		FlowKey: tuple1,
+		OriginalStats: connection.Stats{
+			Packets:        10,
+			Bytes:          1000,
+			ReversePackets: 20,
+			ReverseBytes:   2000,
+		},
+	}
+	conn2 := connection.Connection{
+		FlowKey: tuple2,
+		OriginalStats: connection.Stats{
+			Packets:        20,
+			Bytes:          2000,
+			ReversePackets: 30,
+			ReverseBytes:   3000,
+		},
+	}
+	conn3 := connection.Connection{
+		FlowKey: tuple3,
+		OriginalStats: connection.Stats{
+			Packets:        40,
+			Bytes:          4000,
+			ReversePackets: 50,
+			ReverseBytes:   5000,
+		},
+	}
+	conn1Updated := connection.Connection{
+		FlowKey: tuple1,
+		OriginalStats: connection.Stats{
+			Packets:        11,
+			Bytes:          1100,
+			ReversePackets: 22,
+			ReverseBytes:   2200,
+		},
+	}
+	conn2Updated := connection.Connection{
+		FlowKey: tuple2,
+		OriginalStats: connection.Stats{
+			Packets:        22,
+			Bytes:          2200,
+			ReversePackets: 33,
+			ReverseBytes:   3300,
+		},
+	}
+	conn3Updated := connection.Connection{
+		FlowKey: tuple3,
+		OriginalStats: connection.Stats{
+			Packets:        44,
+			Bytes:          4400,
+			ReversePackets: 55,
+			ReverseBytes:   5500,
+		},
+	}
+
+	tests := []struct {
+		name                string
+		incomingConnections []connection.Connection
+		subs                []subscriber
+		existingEntries     []connection.Connection
+		expectedEntries     []connection.Connection
+		expectedUpdates     int
+	}{
+		{
+			name:                "No connections",
+			incomingConnections: []connection.Connection{},
+			existingEntries:     []connection.Connection{},
+		}, {
+			name:                "New connections",
+			incomingConnections: []connection.Connection{conn1, conn2, conn3},
+			expectedEntries:     []connection.Connection{conn1, conn2, conn3},
+			expectedUpdates:     3,
+		}, {
+			name:                "Updated connections",
+			incomingConnections: []connection.Connection{conn1Updated, conn2Updated, conn3Updated},
+			existingEntries:     []connection.Connection{conn1, conn2, conn3},
+			expectedEntries:     []connection.Connection{conn1Updated, conn2Updated, conn3Updated},
+			expectedUpdates:     3,
+		}, {
+			name:                "New connection - notify subscriber",
+			incomingConnections: []connection.Connection{conn1, conn2},
+			subs: []subscriber{{
+				ch: make(chan UpdateMsg, 1),
+			}},
+			expectedEntries: []connection.Connection{conn1, conn2},
+			expectedUpdates: 2,
+		}, {
+			name:                "Update connections - notify subscriber",
+			incomingConnections: []connection.Connection{conn1Updated},
+			subs: []subscriber{{
+				ch: make(chan UpdateMsg, 1),
+			}},
+			existingEntries: []connection.Connection{conn1},
+			expectedEntries: []connection.Connection{conn1Updated},
+			expectedUpdates: 1,
+		},
+		// TODO Andrew: Add test case for multiple existing entries
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &ConnStore{
+				subs:    make(map[*subscriber]struct{}),
+				entries: make(map[connection.ConnectionKey]*connection.Connection, 1000),
+			}
+
+			for _, sub := range tt.subs {
+				cs.subs[&sub] = struct{}{}
+			}
+
+			for _, entry := range tt.existingEntries {
+				cs.entries[entry.FlowKey] = &entry
+			}
+
+			now := time.Now()
+			cs.updateConnections(tt.incomingConnections)
+
+			for _, want := range tt.expectedEntries {
+				want.LastUpdateTime = now
+				got, ok := cs.entries[want.FlowKey]
+				require.True(t, ok)
+				assert.True(t, got.IsPresent)
+				if !cmp.Equal(got, &want,
+					cmpopts.IgnoreUnexported(netip.Addr{}),
+					cmpopts.EquateApproxTime(1*time.Second),
+					cmpopts.IgnoreFields(connection.Connection{}, "IsPresent", "LastUsedTime")) {
+					t.Errorf("(-want, +got): %s", cmp.Diff(&want, got,
+						cmpopts.IgnoreUnexported(netip.Addr{}),
+						cmpopts.EquateApproxTime(1*time.Second),
+						cmpopts.IgnoreFields(connection.Connection{}, "IsPresent", "LastUsedTime"),
+					))
+				}
+			}
+
+			timeout := time.NewTicker(5 * time.Second)
+			defer timeout.Stop()
+			for _, sub := range tt.subs {
+				select {
+				case <-timeout.C:
+					t.Fatal("timedout waiting for subscriber to receive update")
+				case msg := <-sub.ch:
+					assert.Len(t, msg.Key, tt.expectedUpdates)
+					assert.False(t, msg.Deleted)
+					// TODO: Check the actual returned item.
+				}
+			}
+		})
+	}
+}
+
+func TestConnStore_removeStaleConnections(t *testing.T) {
+	conn1 := connection.Connection{
+		FlowKey: tuple1,
+		OriginalStats: connection.Stats{
+			Packets:        10,
+			Bytes:          1000,
+			ReversePackets: 20,
+			ReverseBytes:   2000,
+		},
+	}
+	conn1.LastUsedTime.Store((20 * time.Second).Nanoseconds())
+
+	staleConn1 := connection.Connection{
+		FlowKey: tuple2,
+		OriginalStats: connection.Stats{
+			Packets:        20,
+			Bytes:          2000,
+			ReversePackets: 30,
+			ReverseBytes:   3000,
+		},
+	}
+	staleConn1.LastUsedTime.Store((-20 * time.Second).Nanoseconds())
+
+	staleConn2 := connection.Connection{
+		FlowKey: tuple3,
+		OriginalStats: connection.Stats{
+			Packets:        40,
+			Bytes:          4000,
+			ReversePackets: 50,
+			ReverseBytes:   5000,
+		},
+	}
+	staleConn2.LastUsedTime.Store((-30 * time.Second).Nanoseconds())
+
+	tests := []struct {
+		name                   string
+		staleConnectionTimeout time.Duration
+		subs                   []subscriber
+		existingEntries        []connection.Connection
+		numConnectionRemoved   int
+	}{
+		{
+			name:                   "No connections",
+			staleConnectionTimeout: 5 * time.Second,
+			numConnectionRemoved:   0,
+		}, {
+			name:                 "multiple expired connections",
+			existingEntries:      []connection.Connection{conn1, staleConn1, staleConn2},
+			numConnectionRemoved: 2,
+		}, {
+			name: "multiple expired connections - notify subs",
+			subs: []subscriber{{
+				ch: make(chan UpdateMsg, 1),
+			}},
+			existingEntries:      []connection.Connection{conn1, staleConn1, staleConn2},
+			numConnectionRemoved: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &ConnStore{
+				subs:    make(map[*subscriber]struct{}),
+				entries: make(map[connection.ConnectionKey]*connection.Connection, 1000),
+			}
+			for _, sub := range tt.subs {
+				cs.subs[&sub] = struct{}{}
+			}
+			now := time.Now()
+			for _, entry := range tt.existingEntries {
+				cs.entries[entry.FlowKey] = &entry
+				heap.Push(&cs.gc, &gcItem{
+					conn:     &entry,
+					expiryMs: now.UnixNano() + entry.LastUsedTime.Load(),
+				})
+			}
+			cs.removeStaleConnections()
+			assert.Len(t, cs.entries, len(tt.existingEntries)-tt.numConnectionRemoved)
+		})
+
+		timeout := time.NewTicker(5 * time.Second)
+		defer timeout.Stop()
+		for _, sub := range tt.subs {
+			select {
+			case <-timeout.C:
+				t.Fatal("timedout waiting for subscriber to receive update")
+			case msg := <-sub.ch:
+				assert.Len(t, msg.Key, tt.numConnectionRemoved)
+				assert.True(t, msg.Deleted)
+			}
+		}
+	}
+}
