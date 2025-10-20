cdde570f5c53c40a35104deb457d272f47c848dd
Antonin Bas <antonin.bas@broadcom.com>
2025-10-20 15:27:30 -0700

    Remove unrelated change
    Signed-off-by: Antonin Bas <antonin.bas@broadcom.com>

--- pkg/agent/flowexporter/connections/conntrack_ovs.go
+++ pkg/agent/flowexporter/connections/conntrack_ovs.go
@@ -242,9 +242,6 @@ func flowStringToAntreaConnection(flow string, zoneFilter uint16) (*connection.C
 			if err != nil {
 				return nil, fmt.Errorf("conversion of mark '%s' to int failed: %v", fields[len(fields)-1], err)
 			}
-			if uint32(val)&connAllowedCTMarkMask == 0 {
-				break
-			}
 			conn.Mark = uint32(val)
 		case strings.Contains(fs, "timeout"):
 			fields := strings.Split(fs, "=")
