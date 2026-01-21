e74a0c4e8372cb95bcbe4dad5de92435941e226a
harshakumar25 <jhah1225@gmail.com>
2026-01-21 14:42:00 +0530

    Consolidate unit tests for MustParseMAC and GetLocalBroadcastIP into ip_test.go
    Addressed reviewer feedback to move new unit tests from ip_extra_test.go
    into the existing ip_test.go file for better consistency and maintainability.
    
    Changes:
    - Moved TestMustParseMAC tests to ip_test.go
    - Moved TestGetLocalBroadcastIP tests to ip_test.go
    - Removed ip_extra_test.go file
    
    All tests pass successfully with 86.3% coverage on pkg/util/ip package.
    
    Signed-off-by: harshakumar25 <jhah1225@gmail.com>

--- pkg/util/ip/ip_test.go
+++ pkg/util/ip/ip_test.go
@@ -412,3 +412,67 @@ func TestGetStartAndEndOfPrefix(t *testing.T) {
 		})
 	}
 }
+
+func TestMustParseMAC(t *testing.T) {
+	tests := []struct {
+		name        string
+		mac         string
+		expectedMAC string
+		shouldPanic bool
+	}{
+		{
+			name:        "valid MAC",
+			mac:         "aa:bb:cc:dd:ee:ff",
+			expectedMAC: "aa:bb:cc:dd:ee:ff",
+			shouldPanic: false,
+		},
+		{
+			name:        "invalid MAC",
+			mac:         "invalid-mac",
+			shouldPanic: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.shouldPanic {
+				assert.Panics(t, func() { MustParseMAC(tt.mac) })
+			} else {
+				mac := MustParseMAC(tt.mac)
+				assert.Equal(t, tt.expectedMAC, mac.String())
+			}
+		})
+	}
+}
+
+func TestGetLocalBroadcastIP(t *testing.T) {
+	tests := []struct {
+		name       string
+		cidr       string
+		expectedIP string
+	}{
+		{
+			name:       "IPv4 /24",
+			cidr:       "192.168.1.0/24",
+			expectedIP: "192.168.1.255",
+		},
+		{
+			name:       "IPv4 /16",
+			cidr:       "10.0.0.0/16",
+			expectedIP: "10.0.255.255",
+		},
+		{
+			name:       "IPv4 /30",
+			cidr:       "10.10.10.0/30",
+			expectedIP: "10.10.10.3",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, ipNet, _ := net.ParseCIDR(tt.cidr)
+			broadcastIP := GetLocalBroadcastIP(ipNet)
+			assert.Equal(t, tt.expectedIP, broadcastIP.String())
+		})
+	}
+}
