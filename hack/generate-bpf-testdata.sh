3b2ec8070dcc4b6b29b35fcfed945f00191af3f2
SharanRP <z8903830@gmail.com>
2026-04-07 23:35:27 +0530

    Consolidate BPF tests and improve test data generation
    Signed-off-by: SharanRP <z8903830@gmail.com>

---
+++ hack/generate-bpf-testdata.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Copyright 2026 Antrea Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "${THIS_DIR}/.." && pwd)"
+
+pushd "${ROOT_DIR}" > /dev/null
+
+echo "Generating BPF test data..."
+go test ./pkg/agent/packetcapture/capture -tags update_bpf_testdata -run TestUpdateBPFTestdata -count=1 -v -update
+
+popd > /dev/null
+
+echo "Update successful!"
