75b412bed0f67da6e21eb3b5c60f24f32cae48d4
kairav mittal <kairavmittal@gmail.com>
2026-06-23 01:59:22 +0530

    MC leader cache changes
    Signed-off-by: kairav mittal <kairavmittal@gmail.com>

--- multicluster/cmd/multicluster-controller/leader.go
+++ multicluster/cmd/multicluster-controller/leader.go
@@ -123,7 +123,6 @@ func runLeader(o *Options) error {
 	if err = staleController.SetupWithManager(mgr); err != nil {
 		return fmt.Errorf("error creating StaleResCleanupController: %v", err)
 	}
-	go staleController.Run(stopCh)
 
 	klog.InfoS("Leader MC Controller Starting Manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
